739b6cea1d44fb33b1e28d86067d4bb07c6bd711
RoryBecker <RoryBecker@241145c3-2f4c-0410-ae16-21396c40807f>
2008-05-07 12:49:03 +0000

    Added items remotely
    Refactor_CreateStubForHandler

---
+++ Refactor_CreateStubForHandler/VS2008/Refactor_CreateStubForHandler_Tests/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports DevExpress.CodeRush.Common
+
+<Assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "Refactor_CreateStubForHandler_Tests", PlugInLoadType.Demand)>
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Refactor_CreateStubForHandler_Tests")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Refactor_CreateStubForHandler_Tests")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8111bce9-3ca2-4ab8-b1aa-e941b8fce778")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
