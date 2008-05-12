1272a43510cd5857f8e0facef591452efa76bb01
RoryBecker <RoryBecker@241145c3-2f4c-0410-ae16-21396c40807f>
2008-05-12 15:39:06 +0000

    CreateStubForHandler - Test Project - Removed Redundant test Code and commended out experimental code

--- Refactor_CreateStubForHandler/VS2008/Refactor_CreateStubForHandler_Tests/MiscDX.vb
+++ Refactor_CreateStubForHandler/VS2008/Refactor_CreateStubForHandler_Tests/MiscDX.vb
@@ -1,75 +1,56 @@
 ﻿Imports DevExpress.CodeRush.StructuralParser
 
 Public Module MiscDX
-    Private Sub FillVarsForClass(ByVal theClass As [Class], Optional ByVal isParent As Boolean = False)
+    'Private Sub FillVarsForClass(ByVal theClass As [Class], Optional ByVal isParent As Boolean = False)
 
-        Dim ienum As IEnumerator = theClass.AllVariables.GetEnumerator
-        While ienum.MoveNext
-            Dim var As Variable = DirectCast(ienum.Current, Variable)
-            If var.Visibility = MemberVisibility.Local Then
-                Continue While
-            End If
-            If var.Visibility = MemberVisibility.Param Then
-                Continue While
-            End If
-            If var.Visibility = MemberVisibility.Private Then
-                Continue While
-            End If
-            If isParent Then
-                Continue While
-            End If
-            If var.GetClass Is Nothing Then
-                Continue While
-            End If
-            If Not var.InsideStruct OrElse var.GetStruct.Name = _parentClassOrStructName Then
-                CheckAndAddVar(var, False)
-            End If
-        End While
-        If Not theClass.PrimaryAncestorType Is Nothing Then
-            If TypeOf theClass.PrimaryAncestorType.GetDeclaration Is [Class] Then
-                Dim parentclass As [Class] = DirectCast(theClass.PrimaryAncestorType.GetDeclaration, [Class])
-                FillVarsForClass(parentclass, True)
-            End If
-        End If
-    End Sub
-    Private Sub FillVarsForClass(ByVal theClass As [Class], Optional ByVal isParent As Boolean = False)
-        Dim ienum As IEnumerator = theClass.AllVariables.GetEnumerator
-        While ienum.MoveNext
-            Dim var As Variable = DirectCast(ienum.Current, Variable)
-            If (Not var.Visibility = MemberVisibility.Local AndAlso Not var.Visibility = MemberVisibility.Param) Then
-                If Not isParent OrElse Not var.Visibility = MemberVisibility.Private Then
-                    If Not var.GetClass Is Nothing AndAlso ((var.InsideStruct AndAlso var.GetStruct.Name = _parentClassOrStructName) OrElse Not var.InsideStruct) Then
-                        CheckAndAddVar(var, False)
-                    End If
-                End If
+    '    Dim ienum As IEnumerator = theClass.AllVariables.GetEnumerator
+    '    While ienum.MoveNext
+    '        Dim var As Variable = DirectCast(ienum.Current, Variable)
+    '        If var.Visibility = MemberVisibility.Local Then
+    '            Continue While
+    '        End If
+    '        If var.Visibility = MemberVisibility.Param Then
+    '            Continue While
+    '        End If
+    '        If var.Visibility = MemberVisibility.Private Then
+    '            Continue While
+    '        End If
+    '        If isParent Then
+    '            Continue While
+    '        End If
+    '        If var.GetClass Is Nothing Then
+    '            Continue While
+    '        End If
+    '        If Not var.InsideStruct OrElse var.GetStruct.Name = _parentClassOrStructName Then
+    '            CheckAndAddVar(var, False)
+    '        End If
+    '    End While
+    '    If Not theClass.PrimaryAncestorType Is Nothing Then
+    '        If TypeOf theClass.PrimaryAncestorType.GetDeclaration Is [Class] Then
+    '            Dim parentclass As [Class] = DirectCast(theClass.PrimaryAncestorType.GetDeclaration, [Class])
+    '            FillVarsForClass(parentclass, True)
+    '        End If
+    '    End If
+    'End Sub
+    'Private Sub FillVarsForClass(ByVal theClass As [Class], Optional ByVal isParent As Boolean = False)
+    '    Dim ienum As IEnumerator = theClass.AllVariables.GetEnumerator
+    '    While ienum.MoveNext
+    '        Dim var As Variable = DirectCast(ienum.Current, Variable)
+    '        If (Not var.Visibility = MemberVisibility.Local AndAlso Not var.Visibility = MemberVisibility.Param) Then
+    '            If Not isParent OrElse Not var.Visibility = MemberVisibility.Private Then
+    '                If Not var.GetClass Is Nothing AndAlso ((var.InsideStruct AndAlso var.GetStruct.Name = _parentClassOrStructName) OrElse Not var.InsideStruct) Then
+    '                    CheckAndAddVar(var, False)
+    '                End If
+    '            End If
 
-            End If
-            Dim tst As String
-        End While
-        If Not theClass.PrimaryAncestorType Is Nothing Then
-            If TypeOf theClass.PrimaryAncestorType.GetDeclaration Is [Class] Then
-                Dim parentclass As [Class] = DirectCast(theClass.PrimaryAncestorType.GetDeclaration, [Class])
-                FillVarsForClass(parentclass, True)
-            End If
-        End If
-    End Sub
-
-    Enum Colors
-        Red
-        Blue
-        Green
-        Yellow
-    End Enum
-    Private Sub MethodName()
-        Dim Color As Colors
-        Select Case Color
-            Case Colors.Red
-            Case Colors.Blue
-            Case Colors.Green
-            Case Else
-                Throw New Exception("Bad Color")
-        End Select
-
-    End Sub
-
+    '        End If
+    '        Dim tst As String
+    '    End While
+    '    If Not theClass.PrimaryAncestorType Is Nothing Then
+    '        If TypeOf theClass.PrimaryAncestorType.GetDeclaration Is [Class] Then
+    '            Dim parentclass As [Class] = DirectCast(theClass.PrimaryAncestorType.GetDeclaration, [Class])
+    '            FillVarsForClass(parentclass, True)
+    '        End If
+    '    End If
+    'End Sub
 End Module
