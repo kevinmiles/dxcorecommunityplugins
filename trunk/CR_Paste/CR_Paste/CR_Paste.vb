f9ef9eff65073eeb97033b2fe5b0ca621271f18e
RoryBecker <RoryBecker@241145c3-2f4c-0410-ae16-21396c40807f>
2008-05-12 10:59:17 +0000

    CR_Plugin: Added SmartPaste menu item to 'Paste' context menu (bad naming) CR_Plugin: ...detects the PastTo Language based on your current document. CR_Plugin: ...detects the PastFrom Language by comparing a Parse-Generate Cycle with the original source.

--- trunk/CR_Paste/CR_Paste/CR_Paste.vb
+++ trunk/CR_Paste/CR_Paste/CR_Paste.vb
@@ -25,12 +25,18 @@ Public Class CR_Paste
 #End Region
 
     Private Sub PasteSmartTag_GetSmartTagItems(ByVal sender As Object, ByVal ea As GetSmartTagItemsEventArgs) Handles PasteSmartTag.GetSmartTagItems
+        Dim mnuSmartPaste As New SmartTagItem("Smart Paste")
         Dim mnuCSharpToVBNet As New SmartTagItem("CSharp -> VBNet")
         Dim mnuVBNetToCSharp As New SmartTagItem("VBNet -> CSharp")
+        ea.Add(mnuSmartPaste)
         ea.Add(mnuCSharpToVBNet)
         ea.Add(mnuVBNetToCSharp)
+        AddHandler mnuSmartPaste.Execute, AddressOf PasteCSharpAsVBNet
         AddHandler mnuCSharpToVBNet.Execute, AddressOf PasteCSharpAsVBNet
         AddHandler mnuVBNetToCSharp.Execute, AddressOf PasteVBNetAsCSharp
+    End Sub
+    Private Sub SmartPaste(ByVal sender As Object, ByVal e As EventArgs)
+        Call New GenericDXTranslator(CodeRush.Documents.ActiveLanguage).Paste()
     End Sub
     Private Sub PasteCSharpAsVBNet(ByVal sender As Object, ByVal e As EventArgs)
         Call New GenericDXTranslator("Basic", "CSharp").Paste()
