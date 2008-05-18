2b83963de5edb7ad4e4a237a09b06faeef4370db
RoryBecker <RoryBecker@241145c3-2f4c-0410-ae16-21396c40807f>
2008-05-18 23:22:37 +0000

    HighlightCurrentLineInEditor: Added Options page at "Editor\Painting\CurrentLine" to allow for adjusting the inner and outer color of the highlight

---
+++ trunk/HighlightCurrentLineInEditor/HighlightCurrentLineInEditor/HighlightCurrentLineOptions.Designer.vb
@@ -0,0 +1,102 @@
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HighlightCurrentLineOptions
+	Inherits DevExpress.CodeRush.Core.OptionsPage
+
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Public Sub New()
+		MyBase.New()
+
+		'This call is required by the Component Designer.
+		InitializeComponent()
+
+	End Sub
+
+	'OptionsPage overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing AndAlso components IsNot Nothing Then
+			components.Dispose()
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.InnerColor = New Global.HighlightCurrentLineInEditor.Controls.ColorPicker
+        Me.OuterColor = New Global.HighlightCurrentLineInEditor.Controls.ColorPicker
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'InnerColor
+        '
+        Me.InnerColor.AllowOpacity = True
+        Me.InnerColor.ColorBase = System.Drawing.Color.Blue
+        Me.InnerColor.LabelWidth = 104
+        Me.InnerColor.Location = New System.Drawing.Point(17, 30)
+        Me.InnerColor.Name = "InnerColor"
+        Me.InnerColor.Opacity = 70
+        Me.InnerColor.Size = New System.Drawing.Size(264, 24)
+        Me.InnerColor.TabIndex = 0
+        Me.InnerColor.Text = "Inner Color"
+        '
+        'OuterColor
+        '
+        Me.OuterColor.AllowOpacity = False
+        Me.OuterColor.ColorBase = System.Drawing.Color.Blue
+        Me.OuterColor.LabelWidth = 104
+        Me.OuterColor.Location = New System.Drawing.Point(17, 60)
+        Me.OuterColor.Name = "OuterColor"
+        Me.OuterColor.Opacity = 255
+        Me.OuterColor.Size = New System.Drawing.Size(264, 24)
+        Me.OuterColor.TabIndex = 0
+        Me.OuterColor.Text = "Outer Color"
+        '
+        'HighlightCurrentLineOptions
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.OuterColor)
+        Me.Controls.Add(Me.InnerColor)
+        Me.Name = "HighlightCurrentLineOptions"
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Public Shared ReadOnly Property Storage() As DevExpress.CodeRush.Core.DecoupledStorage
+        Get
+            Return DevExpress.CodeRush.Core.CodeRush.Options.GetStorage(GetCategory(), GetPageName())
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property Category() As String
+        Get
+            Return HighlightCurrentLineOptions.GetCategory()
+        End Get
+    End Property
+
+    Public Overrides ReadOnly Property PageName() As String
+        Get
+            Return HighlightCurrentLineOptions.GetPageName()
+        End Get
+    End Property
+
+    Public Shared Shadows Sub Show()
+        DevExpress.CodeRush.Core.CodeRush.Command.Execute("Options", FullPath)
+    End Sub
+    Friend WithEvents InnerColor As Global.HighlightCurrentLineInEditor.Controls.ColorPicker
+    Friend WithEvents OuterColor As Global.HighlightCurrentLineInEditor.Controls.ColorPicker
+
+	Public Shared ReadOnly Property FullPath() As String
+		Get
+			Return GetCategory() + "\" + GetPageName()
+		End Get
+	End Property
+
+End Class
