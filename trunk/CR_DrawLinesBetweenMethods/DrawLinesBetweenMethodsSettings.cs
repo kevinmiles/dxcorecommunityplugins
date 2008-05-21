cf06a4273114181fd2dcdb7887b0b65ee85b5ece
duncan.smart <duncan.smart@241145c3-2f4c-0410-ae16-21396c40807f>
2008-05-21 15:11:06 +0000

    * Added colour picker button to options page * Added ".Designer" file for options page and moved designer junk there (using http://tinyurl.com/4wh4he) * Removed unnecessary CR_DrawLinesBetweenMethods subdirectory * Made default line colour lighter.

---
+++ trunk/CR_DrawLinesBetweenMethods/DrawLinesBetweenMethodsSettings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+using DevExpress.CodeRush.Core;
+
+namespace CR_DrawLinesBetweenMethods
+{
+	class DrawLinesBetweenMethodsSettings
+	{
+		public DrawLinesBetweenMethodsSettings()
+		{
+			FullWidth = false;
+			LineDashStyle = DashStyle.Solid;
+			LineWidth = 1;
+			DrawLineAtEndOfMethod = false;
+			DrawShadow = true;
+		}
+
+		private void LoadFromStorage(DecoupledStorage storage)
+		{
+			
+		}
+
+		public bool FullWidth { get; set; }
+		public DashStyle LineDashStyle { get; set; }
+		public int LineWidth { get; set; }
+		public Color LineColor { get; set; }
+		public bool DrawLineAtEndOfMethod { get; set; }
+		public bool DrawShadow { get; set; }
+
+	}
+}
