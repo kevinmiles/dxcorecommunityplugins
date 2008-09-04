38c698407e1a45ca19767986b44da376810bce88
RoryBecker@Gmail.com <RoryBecker@Gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2008-09-04 10:01:00 +0000

    Added CR_Execute

---
+++ trunk/CR_Execute/CR_Execute/PlugIn1.vb
@@ -0,0 +1,28 @@
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.CodeRush.Core
+Imports DevExpress.CodeRush.PlugInCore
+Imports DevExpress.CodeRush.StructuralParser
+
+Public Class PlugIn1
+
+	'DXCore-generated code...
+#Region " InitializePlugIn "
+	Public Overrides Sub InitializePlugIn()
+		MyBase.InitializePlugIn()
+
+		'TODO: Add your initialization code here.
+	End Sub
+#End Region
+#Region " FinalizePlugIn "
+	Public Overrides Sub FinalizePlugIn()
+		'TODO: Add your finalization code here.
+
+		MyBase.FinalizePlugIn()
+	End Sub
+#End Region
+    Private Sub Execute_Execute(ByVal ea As DevExpress.CodeRush.Core.ExecuteEventArgs) Handles Execute.Execute
+        CodeRush.Command.Execute(ea.Action.Parameters.GetString("CommandName"))
+    End Sub
+End Class
