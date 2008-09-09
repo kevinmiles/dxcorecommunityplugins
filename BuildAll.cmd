2813e4a55f07c391d449133aa16d6980f8e92399
duncan.smart <duncan.smart@241145c3-2f4c-0410-ae16-21396c40807f>
2008-09-09 15:01:24 +0000

    * Added BuildAll.cmd that, er... builds everything, and creates Setup.exe. Still quite a few projects to fix up though (e.g. looks like there's a new DevExpress.Refactor.Testing DLL that isn't publicly released yet).
    * DXCoreCommunityPluginsSuiteSetup.iss: InnoSetup script (http://www.jrsoftware.org/isinfo.php) to build setup.exe. 
      Needs a bit more work:     
        - not all things added
        - (re)categorise things

---
+++ BuildAll.cmd
@@ -0,0 +1,37 @@
+@echo off
+
+set MSBUILD=%SystemRoot%\Microsoft.NET\Framework\v3.5\msbuild.exe
+set ISCC="%ProgramFiles%\Inno Setup\ISCC.exe"
+if not exist %ISCC% set ISCC="%ProgramFiles(x86)%\Inno Setup\ISCC.exe"
+
+:: Build each SLN
+for /r %%F in (*.sln) do call :DOBUILD "%%F"
+
+:: Create Setup EXE
+if exist %ISCC% (
+	call :COMPILE_SETUP 
+) else (
+	echo InnoSetup compiler not found: install from http://www.jrsoftware.org/
+)
+
+goto :EOF
+
+:DOBUILD
+	echo * %~n1
+	%MSBUILD% /verbosity:quiet /target:Build /property:Configuration=Release;OutDir=%~dp0bin\ /nologo %1
+	if %ERRORLEVEL% NEQ 0 (
+		echo ================================================================
+	)
+	goto :EOF
+
+
+:COMPILE_SETUP
+	"%ISCC%" /Q DXCoreCommunityPluginsSuiteSetup.iss
+	if %ERRORLEVEL% NEQ 0 (
+		echo.
+		echo ================================================================
+		echo COMPILE SETUP FAIL
+		echo ================================================================
+		echo.
+	) 
+	goto :EOF
