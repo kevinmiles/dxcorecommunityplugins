c96f57452502667044c0bb39a41b84ee38a455d2
tenHoleHarp <tenHoleHarp@241145c3-2f4c-0410-ae16-21396c40807f>
2008-09-10 15:17:47 +0000

    Removed an unused action Added RunTests action to the context menu.

--- RedGreen/RedGreen/PlugIn1.Designer.cs
+++ RedGreen/RedGreen/PlugIn1.Designer.cs
@@ -39,33 +39,24 @@ namespace RedGreen
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlugIn1));
             this.actRunTest = new DevExpress.CodeRush.Core.Action(this.components);
-            this.actNextError = new DevExpress.CodeRush.Core.Action(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.actRunTest)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.actNextError)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
             // actRunTest
             // 
             this.actRunTest.ActionName = "Run Test(s)";
-            this.actRunTest.CommonMenu = DevExpress.CodeRush.Menus.VsCommonBar.None;
+            this.actRunTest.ButtonText = "Run Test(s)";
+            this.actRunTest.CommonMenu = DevExpress.CodeRush.Menus.VsCommonBar.EditorContext;
             this.actRunTest.Description = "Runs the currently selected unit tests";
             this.actRunTest.Image = ((System.Drawing.Bitmap)(resources.GetObject("actRunTest.Image")));
             this.actRunTest.ImageBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(254)))), ((int)(((byte)(0)))));
             this.actRunTest.Execute += new DevExpress.CodeRush.Core.CommandExecuteEventHandler(this.actRunTest_Execute);
-            // 
-            // actNextError
-            // 
-            this.actNextError.CommonMenu = DevExpress.CodeRush.Menus.VsCommonBar.None;
-            this.actNextError.Image = ((System.Drawing.Bitmap)(resources.GetObject("actNextError.Image")));
-            this.actNextError.ImageBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(254)))), ((int)(((byte)(0)))));
-            this.actNextError.Execute += new DevExpress.CodeRush.Core.CommandExecuteEventHandler(this.actNextError_Execute);
             // 
             // PlugIn1
             // 
             this.SolutionOpened += new DevExpress.CodeRush.Core.DefaultHandler(this.PlugIn1_SolutionOpened);
             this.EditorPaintLanguageElement += new DevExpress.CodeRush.Core.EditorPaintLanguageElementEventHandler(this.PlugIn1_EditorPaintLanguageElement);
             ((System.ComponentModel.ISupportInitialize)(this.actRunTest)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.actNextError)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
         }
@@ -73,6 +64,5 @@ namespace RedGreen
         #endregion
 
         private DevExpress.CodeRush.Core.Action actRunTest;
-        private DevExpress.CodeRush.Core.Action actNextError;
     }
 }
