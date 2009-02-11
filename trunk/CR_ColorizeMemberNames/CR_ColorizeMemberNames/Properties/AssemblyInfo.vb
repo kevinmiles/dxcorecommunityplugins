862e0e267445bfec4bd96fb68f6144011c4adff6
RoryBecker@Gmail.com <RoryBecker@Gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-02-11 18:16:57 +0000

    Adding ColorizeMemberNames

---
+++ trunk/CR_ColorizeMemberNames/CR_ColorizeMemberNames/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports DevExpress.CodeRush.Common
+
+<Assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "ColorizeMemberNames", PlugInLoadType.Demand, LoadAbilityType.LoadEnabled)>
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ColorizeMemberNames")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("ColorizeMemberNames")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("720ea6d3-8706-4518-8185-9776a385ee0e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
