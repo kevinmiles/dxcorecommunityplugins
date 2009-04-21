2a3e906453f0b20d50ea70da5913990bc6840e49
duncan.smart <duncan.smart@241145c3-2f4c-0410-ae16-21396c40807f>
2009-04-21 10:56:16 +0000

    * Added changes submitted by Ben Alabaster (via Rory).    "Additional functionality:     - Ability to have lines before and after members     - Ability to turn lines on/off for user chosen members: class, property, method     - Ability to set leading/trailing space after line depending if the line is displayed before or after the member     - Ability to set the shadow height from the previously preset height of 5 pixels
       Fixes issues:
        - Line painting when code is collapsed to definitions"
    
    (Thanks Ben!)
    
    * Tweaked the above so that default initial settings actually draw lines
    * Added support for painting lines before Enums
    * Bit of a code tidy up. Converted tabs to spaces (it's the VS default; I've decided not to fight it any more:-)).

--- CR_DrawLinesBetweenMethods/DrawLinesBetweenMethodsPlugIn.cs
+++ CR_DrawLinesBetweenMethods/DrawLinesBetweenMethodsPlugIn.cs
@@ -1,8 +1,6 @@
 using System;
-using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Windows.Forms;
 using DevExpress.CodeRush.Core;
 using DevExpress.CodeRush.PlugInCore;
 using DevExpress.CodeRush.StructuralParser;
@@ -10,154 +8,172 @@ using System.Diagnostics;
 
 namespace CR_DrawLinesBetweenMethods
 {
-	/// <summary>
-	/// Summary description for DrawLinesBetweenMethodsPlugIn.
-	/// </summary>
-	public class DrawLinesBetweenMethodsPlugIn: StandardPlugIn
-	{
-		#region private fields...
-		//private System.ComponentModel.Container components = null;
-		#endregion
+    /// <summary>
+    /// Summary description for DrawLinesBetweenMethodsPlugIn.
+    /// </summary>
+    public class DrawLinesBetweenMethodsPlugIn : StandardPlugIn
+    {
+        #region Component Designer generated code
+        public DrawLinesBetweenMethodsPlugIn()
+        {
+            InitializeComponent();
+        }
 
-		// constructor...
-		#region DrawLinesBetweenMethodsPlugIn
-		public DrawLinesBetweenMethodsPlugIn()
-		{
-			/// <summary>
-			/// Required for Windows.Forms Class Composition Designer support
-			/// </summary>
-			InitializeComponent();
-		}
-		#endregion
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            // 
+            // DrawLinesBetweenMethodsPlugIn
+            // 
+            this.OptionsChanged += new DevExpress.CodeRush.Core.OptionsChangedEventHandler(this.DrawLinesBetweenMethodsPlugIn_OptionsChanged);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
 
-		// CodeRush-generated code
-		#region InitializePlugIn
-		public override void InitializePlugIn()
-		{
-			base.InitializePlugIn();
+        }
+        #endregion
 
-			loadSettings();
-		}
-		#endregion
-		#region FinalizePlugIn
-		public override void FinalizePlugIn()
-		{
-			//
-			// TODO: Add your finalization code here.
-			//
-              
-			base.FinalizePlugIn();
-		}
-		#endregion
+        // CodeRush-generated code
+        #region InitializePlugIn
+        public override void InitializePlugIn()
+        {
+            base.InitializePlugIn();
 
-		#region Component Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
-			// 
-			// DrawLinesBetweenMethodsPlugIn
-			// 
-			this.OptionsChanged += new DevExpress.CodeRush.Core.OptionsChangedEventHandler(this.DrawLinesBetweenMethodsPlugIn_OptionsChanged);
-			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            loadSettings();
+        }
+        #endregion
+        
+        #region FinalizePlugIn
+        public override void FinalizePlugIn()
+        {
+            //
+            // TODO: Add your finalization code here.
+            //
 
-		}
-		#endregion
+            base.FinalizePlugIn();
+        }
+        #endregion
 
-		private void DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement(DevExpress.CodeRush.Core.EditorPaintLanguageElementEventArgs ea)
-		{
-			if (ea.LanguageElement is Method || ea.LanguageElement is Property || ea.LanguageElement is Class)
-			{		
-				int line;                                        
-				if(_drawLineAtEndOfMethod)
-					line = ea.LanguageElement.Range.End.Line + 1;
-				else
-				{
-					line = ea.LanguageElement.Range.Start.Line;
+        private void DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement(EditorPaintLanguageElementEventArgs ea)
+        {
+            if ((ea.LanguageElement is Class) && _enableOnClass
+                || (ea.LanguageElement is Property) && _enableOnProperty
+                || (ea.LanguageElement is Method) && _enableOnMethod
+                || (ea.LanguageElement is Enumeration) && _enableOnEnum)
+            {
+                decorate(ea);
+            }
+        }
 
-					// do we need to negotiate attributes or comments?
-					LanguageElement prev = ea.LanguageElement.PreviousSibling;
-					while(prev != null && 
-						(prev.ElementType == LanguageElementType.Comment 
-						|| prev.ElementType == LanguageElementType.XmlDocComment 
-						|| prev.ElementType == LanguageElementType.AttributeSection ))
-					{
-						//Skip: Comment, AttributeSection, XmlDocComment
-						//Debug.WriteLine("Skipping " + prev.ElementType + "  \"" + prev.ToString() + "\"");
-						line = prev.Range.Start.Line;
+        void decorate(EditorPaintLanguageElementEventArgs ea)
+        {
+            //Debug.WriteLine("Decorating " + ea.LanguageElement.ElementType + ": " + ea.LanguageElement);
 
-						prev = prev.PreviousSibling;
-					}
-				}
-				
-				int leftPos;                                                        
-				if(_fullWidth)
-					leftPos = 0;
-				else if (_drawLineAtEndOfMethod)
-					leftPos = ea.PaintArgs.GetPoint(ea.LanguageElement.Range.End).X;
-				else
-					leftPos = ea.PaintArgs.GetPoint(ea.LanguageElement.Range.Start).X;
-				
-				Point linePos = ea.PaintArgs.GetPoint(line, 1);
-				int yPos = linePos.Y;
-				Point leftPt = new Point(leftPos, yPos);
-				Point rightPt = new Point(ea.PaintArgs.TextView.Width, yPos);
+            //Top of code block
+            int leftPos = _fullWidth ? 0 : ea.PaintArgs.GetPoint(ea.LanguageElement.Range.Start).X;
 
-				Rectangle rect = new Rectangle(leftPos, yPos, ea.PaintArgs.TextView.Width, 6);
-				
-				if (_drawShadow)
-				{
-					Color shadowColor = Color.FromArgb(0x33, _lineColor);
-					using (LinearGradientBrush brush = new LinearGradientBrush(rect, shadowColor, Color.Transparent, 90, true))
-					{
-						ea.PaintArgs.Graphics.FillRectangle(brush, rect);
-					}
-				}
-				
-				using (Pen pen = new Pen(_lineColor, _lineWidth))
-				{
-					pen.DashStyle = _lineDashStyle;
-					ea.PaintArgs.Graphics.DrawLine(pen, leftPt, rightPt);
-				}
-				
-			}
-		}
+            //Separate out start and end of method for independent control...
+            if (_drawLineAtStartOfMethod)
+            {
+                int line = ea.LanguageElement.Range.Start.Line;
 
+                // Skip up over Comment, AttributeSection, XmlDocComment
+                LanguageElement prev = ea.LanguageElement.PreviousSibling;
+                while (prev != null &&
+                    (prev.ElementType == LanguageElementType.Comment
+                    || prev.ElementType == LanguageElementType.XmlDocComment
+                    || prev.ElementType == LanguageElementType.AttributeSection))
+                {
+                    //Debug.WriteLine("Skipping " + prev.ElementType + "  \"" + prev.ToString() + "\"");
+                    line = prev.Range.Start.Line;
+                    prev = prev.PreviousSibling;
+                }
 
-		bool _fullWidth = false;
-		DashStyle _lineDashStyle = DashStyle.Solid;
-		int _lineWidth = 1;
-		Color _lineColor = Color.Silver;
-		bool _drawLineAtEndOfMethod = false;
-		bool _drawShadow = true;
-		bool _enabled = true;
+                Point leftPoint = new Point(leftPos, ea.PaintArgs.GetPoint(line, 1).Y - _lineSpacer);
+                Point rightPoint = new Point(ea.PaintArgs.TextView.Width, leftPoint.Y);
+                drawLine(ea.PaintArgs.Graphics, _lineColor, leftPoint, rightPoint, _lineWidth, _lineDashStyle, _drawShadow, _shadowHeight);
+            }
+
+            if (_drawLineAtEndOfMethod)
+            {
+                int line = ea.LanguageElement.Range.End.Line + 1;
+                Point leftPoint = new Point(leftPos, ea.PaintArgs.GetPoint(line, 1).Y + _lineSpacer);
+                Point rightPoint = new Point(ea.PaintArgs.TextView.Width, leftPoint.Y);
+                drawLine(ea.PaintArgs.Graphics, _lineColor, leftPoint, rightPoint, _lineWidth, _lineDashStyle, _drawShadow, _shadowHeight);
+            }
+        }
+
+        void drawLine(Graphics graphics, Color lineColor, Point startPoint, Point endPoint, int lineWidth, DashStyle lineStyle, bool shadow, int shadowHeight)
+        {
+            if (shadow)
+            {
+                Point bottomRight = endPoint;
+                bottomRight.Y = endPoint.Y + shadowHeight;
+                drawShadow(graphics, lineColor, startPoint, bottomRight);
+            }
+
+            using (Pen pen = new Pen(lineColor, lineWidth) { DashStyle = lineStyle })
+            {
+                graphics.DrawLine(pen, startPoint, endPoint);
+            }
+        }
+
+        void drawShadow(Graphics graphics, Color lineColor, Point topLeft, Point bottomRight)
+        {
+            Color shadowColor = Color.FromArgb(0x33, lineColor);
+            Rectangle rect = new Rectangle(topLeft.X, topLeft.Y, bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y);
+            using (LinearGradientBrush brush = new LinearGradientBrush(rect, shadowColor, Color.Transparent, 90, true))
+            {
+                graphics.FillRectangle(brush, rect);
+            }
+        }
 
 
-		private void DrawLinesBetweenMethodsPlugIn_OptionsChanged(DevExpress.CodeRush.Core.OptionsChangedEventArgs ea)
-		{
-			loadSettings();
-		}
+        bool _fullWidth;
+        DashStyle _lineDashStyle = DashStyle.Solid;
+        int _lineWidth = 1;
+        Color _lineColor = Color.Silver;
+        bool _drawLineAtStartOfMethod = true;
+        bool _drawLineAtEndOfMethod;
+        bool _drawShadow = true;
+        bool _enabled = true;
+        bool _enableOnClass = true;
+        bool _enableOnProperty = true;
+        bool _enableOnMethod = true;
+        bool _enableOnEnum = true;
+        int _lineSpacer;
+        int _shadowHeight = 5;
 
-		void loadSettings()
-		{
-			using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
-			{
-				_fullWidth = storage.ReadBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
-				_lineDashStyle = (DashStyle)storage.ReadEnum("DrawLinesBetweenMethods", "LineDashStyle", typeof(DashStyle), _lineDashStyle);
-				_lineWidth = storage.ReadInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
-				_lineColor = storage.ReadColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
-				_drawLineAtEndOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
-				_drawShadow = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
-				_enabled = storage.ReadBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
+        private void DrawLinesBetweenMethodsPlugIn_OptionsChanged(OptionsChangedEventArgs ea)
+        {
+            loadSettings();
+        }
 
-				this.EditorPaintLanguageElement -= DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement;
-				if (_enabled)
-					this.EditorPaintLanguageElement += DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement;
-			}
-		}
+        void loadSettings()
+        {
+            using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
+            {
+                _fullWidth = storage.ReadBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
+                _lineDashStyle = (DashStyle)storage.ReadEnum("DrawLinesBetweenMethods", "LineDashStyle", typeof(DashStyle), _lineDashStyle);
+                _lineWidth = storage.ReadInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
+                _lineColor = storage.ReadColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
+                _drawLineAtStartOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtStartOfMethod", _drawLineAtStartOfMethod);
+                _drawLineAtEndOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
+                _drawShadow = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
+                _enableOnClass = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnClass", _enableOnClass);
+                _enableOnProperty = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnProperty", _enableOnProperty);
+                _enableOnMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnMethod", _enableOnMethod);
+                _enableOnEnum = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnEnum", _enableOnEnum);
+                _enabled = storage.ReadBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
+                _lineSpacer = storage.ReadInt32("DrawLinesBetweenMethods", "LineSpacer", _lineSpacer);
+                _shadowHeight = storage.ReadInt32("DrawLinesBetweenMethods", "ShadowHeight", _shadowHeight);
 
-	}
+                EditorPaintLanguageElement -= DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement;
+                if (_enabled)
+                    EditorPaintLanguageElement += DrawLinesBetweenMethodsPlugIn_EditorPaintLanguageElement;
+            }
+        }
+    }
 }
--- CR_DrawLinesBetweenMethods/OptDrawLinesBetweenMethods.cs
+++ CR_DrawLinesBetweenMethods/OptDrawLinesBetweenMethods.cs
@@ -1,5 +1,4 @@
 using System;
-using System.ComponentModel;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Windows.Forms;
@@ -7,112 +6,150 @@ using DevExpress.CodeRush.Core;
 
 namespace CR_DrawLinesBetweenMethods
 {
-	/// <summary>
-	/// Summary description for OptDrawLinesBetweenMethods.
-	/// </summary>
-	public partial class OptDrawLinesBetweenMethods : OptionsPage
-	{
-		public OptDrawLinesBetweenMethods() : base()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Summary description for OptDrawLinesBetweenMethods.
+    /// </summary>
+    public partial class OptDrawLinesBetweenMethods : OptionsPage
+    {
+        public OptDrawLinesBetweenMethods()
+        {
+            InitializeComponent();
+        }
 
-		private void OptDrawLinesBetweenMethods_PreparePage(object sender, OptionsPageStorageEventArgs ea)
-		{
-			loadSettings();
-		}
+        private void OptDrawLinesBetweenMethods_PreparePage(object sender, OptionsPageStorageEventArgs ea)
+        {
+            loadSettings();
+        }
 
-		private void OptDrawLinesBetweenMethods_CommitChanges(object sender, OptionsPageStorageEventArgs ea)
-		{
-			saveSettings();
-		}
+        private void OptDrawLinesBetweenMethods_CommitChanges(object sender, CommitChangesEventArgs ea)
+        {
+            saveSettings();
+        }
+
+        bool _fullWidth;
+        DashStyle _lineDashStyle = DashStyle.Solid;
+        int _lineWidth = 1;
+        Color _lineColor = Color.Silver;
+        bool _drawLineAtStartOfMethod = true;
+        bool _drawLineAtEndOfMethod;
+        bool _drawShadow = true;
+        bool _enabled = true;
+        bool _enableOnClass = true;
+        bool _enableOnProperty = true;
+        bool _enableOnMethod = true;
+        bool _enableOnEnum = true;
+        int _lineSpacer;
+        int _shadowHeight = 5;
+
+        void loadSettings()
+        {
+            try
+            {
+                using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
+                {
+                    _fullWidth = storage.ReadBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
+                    _lineDashStyle = (DashStyle)storage.ReadEnum("DrawLinesBetweenMethods", "LineDashStyle", typeof(DashStyle), _lineDashStyle);
+                    _lineWidth = storage.ReadInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
+                    _lineColor = storage.ReadColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
+                    _drawLineAtStartOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtStartOfMethod", _drawLineAtStartOfMethod);
+                    _drawLineAtEndOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
+                    _drawShadow = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
+                    _enableOnClass = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnClass", _enableOnClass);
+                    _enableOnProperty = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnProperty", _enableOnProperty);
+                    _enableOnMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnMethod", _enableOnMethod);
+                    _enableOnEnum = storage.ReadBoolean("DrawLinesBetweenMethods", "EnableOnEnum", _enableOnEnum);
+                    _lineSpacer = storage.ReadInt32("DrawLinesBetweenMethods", "LineSpacer", _lineSpacer);
+                    _shadowHeight = storage.ReadInt32("DrawLinesBetweenMethods", "ShadowHeight", _shadowHeight);
+                    _enabled = storage.ReadBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
+                }
+
+                _fullWidthChk.Checked = _fullWidth;
+                _lineStyleLst.Text = _lineDashStyle.ToString();
+                _lineWidthLst.Text = _lineWidth.ToString();
+                _lineColorBtn.BackColor = _lineColor;
+                _drawLineAtStartChk.Checked = _drawLineAtStartOfMethod;
+                _drawLineAtEndChk.Checked = _drawLineAtEndOfMethod;
+                _drawShadowChk.Checked = _drawShadow;
+                _enableOnMemberCheckList.SetItemChecked(0, _enableOnClass);
+                _enableOnMemberCheckList.SetItemChecked(1, _enableOnProperty);
+                _enableOnMemberCheckList.SetItemChecked(2, _enableOnMethod);
+                _enableOnMemberCheckList.SetItemChecked(3, _enableOnEnum);
+                _lineSpaceNUD.Value = _lineSpacer;
+                _shadowHeightNUD.Value = _shadowHeight;
+                _enabledChk.Checked = _enabled;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        void saveSettings()
+        {
+            try
+            {
+
+                _fullWidth = _fullWidthChk.Checked;
+                _lineDashStyle = (DashStyle)Enum.Parse(typeof(DashStyle), _lineStyleLst.Text);
+                _lineWidth = int.Parse(_lineWidthLst.Text);
+                _lineColor = _lineColorBtn.BackColor;
+                _drawLineAtStartOfMethod = _drawLineAtStartChk.Checked;
+                _drawLineAtEndOfMethod = _drawLineAtEndChk.Checked;
+                _drawShadow = _drawShadowChk.Checked;
+                _enableOnClass = _enableOnMemberCheckList.GetItemChecked(0);
+                _enableOnProperty = _enableOnMemberCheckList.GetItemChecked(1);
+                _enableOnMethod = _enableOnMemberCheckList.GetItemChecked(2);
+                _enableOnEnum = _enableOnMemberCheckList.GetItemChecked(3);
+                _enabled = _enabledChk.Checked;
+                _lineSpacer = (int)_lineSpaceNUD.Value;
+                _shadowHeight = (int)_shadowHeightNUD.Value;
+
+                using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
+                {
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
+                    storage.WriteEnum("DrawLinesBetweenMethods", "LineDashStyle", _lineDashStyle);
+                    storage.WriteInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
+                    storage.WriteColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "DrawLineAtStartOfMethod", _drawLineAtStartOfMethod);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "EnableOnClass", _enableOnClass);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "EnableOnProperty", _enableOnProperty);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "EnableOnMethod", _enableOnMethod);
+                    storage.WriteBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
+                    storage.WriteInt32("DrawLinesBetweenMethods", "LineSpacer", _lineSpacer);
+                    storage.WriteInt32("DrawLinesBetweenMethods", "ShadowHeight", _shadowHeight);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
 
 
-		bool _fullWidth = false;
-		DashStyle _lineDashStyle = DashStyle.Solid;
-		int _lineWidth = 1;
-		Color _lineColor = Color.Silver;
-		bool _drawLineAtEndOfMethod = false;
-		bool _drawShadow = true;
-		bool _enabled = true;
+        private void _lineColorBtn_Click(object sender, EventArgs e)
+        {
+            using (ColorDialog dlg = new ColorDialog { AnyColor = true, FullOpen = true, Color = _lineColorBtn.BackColor })
+            {
+                if (dlg.ShowDialog() == DialogResult.OK)
+                    _lineColorBtn.BackColor = dlg.Color;
+            }
+        }
 
-		void loadSettings()
-		{
-			try
-			{
+        private void OptDrawLinesBetweenMethods_Load(object sender, EventArgs e)
+        {
+            // Enable controls with Enabled checkbox
+            _mainPanel.DataBindings.Add("Enabled", _enabledChk, "Checked");
+        }
 
-				using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
-				{
-					_fullWidth = storage.ReadBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
-					_lineDashStyle = (DashStyle)storage.ReadEnum("DrawLinesBetweenMethods", "LineDashStyle", typeof(DashStyle), _lineDashStyle);
-					_lineWidth = storage.ReadInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
-					_lineColor = storage.ReadColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
-					_drawLineAtEndOfMethod = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
-					_drawShadow = storage.ReadBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
-					_enabled = storage.ReadBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
-				}
+        private void _drawShadowChk_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox cbx = (CheckBox)sender;
+            _shadowHeightNUD.Enabled = cbx.Checked;
+        }
 
-				_fullWidthChk.Checked = _fullWidth;
-				_lineStyleLst.Text = _lineDashStyle.ToString();
-				_lineWidthLst.Text = _lineWidth.ToString();
-				_lineColorBtn.BackColor = _lineColor;
-				_drawLineAtEndChk.Checked = _drawLineAtEndOfMethod;
-				_drawShadowChk.Checked = _drawShadow;
-				_enabledChk.Checked = _enabled;
-
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.ToString());
-			}
-
-		}
-
-		void saveSettings()
-		{
-			try
-			{
-				_fullWidth = _fullWidthChk.Checked;
-				_lineDashStyle = (DashStyle)Enum.Parse(typeof(DashStyle), _lineStyleLst.Text);
-				_lineWidth = int.Parse(_lineWidthLst.Text);
-				_lineColor = _lineColorBtn.BackColor;
-				_drawLineAtEndOfMethod = _drawLineAtEndChk.Checked;
-				_drawShadow = _drawShadowChk.Checked;
-				_enabled = _enabledChk.Checked;
-
-				using (DecoupledStorage storage = OptDrawLinesBetweenMethods.Storage)
-				{
-					storage.WriteBoolean("DrawLinesBetweenMethods", "FullWidth", _fullWidth);
-					storage.WriteEnum("DrawLinesBetweenMethods", "LineDashStyle", _lineDashStyle);
-					storage.WriteInt32("DrawLinesBetweenMethods", "LineWidth", _lineWidth);
-					storage.WriteColor("DrawLinesBetweenMethods", "LineColor", _lineColor);
-					storage.WriteBoolean("DrawLinesBetweenMethods", "DrawLineAtEndOfMethod", _drawLineAtEndOfMethod);
-					storage.WriteBoolean("DrawLinesBetweenMethods", "DrawShadow", _drawShadow);
-					storage.WriteBoolean("DrawLinesBetweenMethods", "Enabled", _enabled);
-				}
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(ex.ToString());
-			}
-		}
-
-		private void _lineColorBtn_Click(object sender, EventArgs e)
-		{
-			ColorDialog dlg = new ColorDialog();
-			dlg.AnyColor = true;
-			dlg.FullOpen = true;
-			dlg.Color = _lineColorBtn.BackColor;
-
-			if (dlg.ShowDialog() == DialogResult.OK)
-				_lineColorBtn.BackColor = dlg.Color;
-		}
-
-		private void OptDrawLinesBetweenMethods_Load(object sender, EventArgs e)
-		{
-			// Enable controls with Enabled checkbox
-			_mainPanel.DataBindings.Add("Enabled", _enabledChk, "Checked");
-		}
-
-	}
+    }
 }
