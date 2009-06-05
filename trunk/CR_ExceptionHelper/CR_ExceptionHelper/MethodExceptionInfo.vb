f55073297233b2e15e4db08bc33237ea44c45585
RoryBecker@Gmail.com <RoryBecker@Gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-06-05 20:21:38 +0000

    Adding CR_ExceptionHelper

---
+++ trunk/CR_ExceptionHelper/CR_ExceptionHelper/MethodExceptionInfo.vb
@@ -0,0 +1,326 @@
+Option Infer On
+Imports System.Xml.XPath
+Imports DevExpress.CodeRush.Core
+Imports DevExpress.CodeRush.PlugInCore
+Imports DevExpress.CodeRush.StructuralParser
+Imports System
+Imports System.Xml.Linq
+
+
+''' <summary>
+''' Stores information about one method.
+''' </summary>
+''' <remarks></remarks>
+Friend Class MethodExceptionInfo
+
+#Region " Private Members "
+
+    Private ReadOnly m_MethodDescriptor As String
+    Private m_NameRanges As SourceRangeCollection
+    Private m_ExceptionInfos As ChainedList(Of ExceptionInfo)
+
+#End Region
+
+#Region " Shared Members "
+
+    'TODO: add the ability to refresh the cache, or remove it again (which is possible).
+    Private Shared m_DiscoveredMethods As New Dictionary(Of String, MethodExceptionInfo)
+
+#End Region
+
+#Region " Constructors "
+
+    ''' <summary>
+    ''' Standard Constructor
+    ''' </summary>
+    ''' <param name="methodDescriptor">The method call, in the format of Namespace.Type.Method(Params)</param>
+    ''' <param name="assemblyReference">A DXCore assembly reference.</param>
+    ''' <param name="nameRange">A SourceRange indicating the location of the method call reference in the code.</param>
+    ''' <remarks></remarks>
+    Public Sub New(ByVal methodDescriptor As String, ByVal assemblyReference As AssemblyReference, ByVal nameRange As SourceRange)
+        'Store field values
+        m_MethodDescriptor = methodDescriptor
+        NameRanges.Add(nameRange)
+        FindExceptions(assemblyReference)
+    End Sub
+
+    Public Sub New(ByVal methodDescriptor As String, ByVal docComment As XmlDocComment, ByVal nameRange As SourceRange)
+        m_MethodDescriptor = methodDescriptor
+        NameRanges.Add(nameRange)
+        FindExceptions(docComment)
+    End Sub
+
+#End Region
+
+
+#Region " Shared Factory Methods "
+
+    ''' <summary>
+    ''' Clears the cache of previously discovered methodsexceptioninfos
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Shared Sub Clear()
+        m_DiscoveredMethods.Clear()
+    End Sub
+
+    ''' <summary>
+    ''' Generates a MethodInfo instance based on the provided language element
+    ''' </summary>
+    ''' <param name="element">A language element which possibly represents a method call</param>
+    ''' <returns>A MethodExceptionInfo instance, or null</returns>
+    ''' <remarks></remarks>
+    Public Shared Function Create(ByVal element As LanguageElement) As MethodExceptionInfo
+
+        If Not TypeOf element Is IMethodCallExpression _
+                AndAlso Not TypeOf element Is IMethodCallStatement Then
+            Return Nothing
+        End If
+
+        Dim methodInfo As MethodExceptionInfo = Nothing
+        Dim methodDeclaration As IMethodElement
+        Dim strMethodDescriptor As String
+
+        methodDeclaration = TryCast(ParserServices.SourceModelService.GetDeclaration(element), IMethodElement)
+
+        If methodDeclaration Is Nothing Then
+            ' Call is to sourcecode rather than a library.
+            Return Nothing
+        End If
+
+        strMethodDescriptor = GetMethodDescriptor(methodDeclaration)
+        ' Of Form "System.IO.Directory.Delete(System.String)"
+
+        If strMethodDescriptor = Nothing Then
+            ' Couldn't build Method Descriptor
+            Return Nothing
+        End If
+
+        If m_DiscoveredMethods.TryGetValue(strMethodDescriptor, methodInfo) Then
+            methodInfo.NameRanges.Add(element.NameRange)    ' Store this location for later when we paint references contributing to this exception.
+        Else
+            'locate the assemblyreference
+            Dim assemblyModel As MetaDataAssemblyModel = TryCast(methodDeclaration.AssemblyModel, MetaDataAssemblyModel)
+            Dim assemblyRef As AssemblyReference = Nothing
+            If assemblyModel IsNot Nothing Then
+                assemblyRef = assemblyModel.Assembly
+            End If
+
+            If assemblyRef IsNot Nothing Then
+                methodInfo = New MethodExceptionInfo(strMethodDescriptor, assemblyRef, element.NameRange)
+                m_DiscoveredMethods.Add(methodInfo.MethodDescriptor, methodInfo)
+            Else
+                Dim codeEl As CodeElement = TryCast(methodDeclaration, CodeElement)
+                If codeEl IsNot Nothing Then
+                    methodInfo = New MethodExceptionInfo(strMethodDescriptor, codeEl.DocComment, element.NameRange)
+                End If
+            End If
+
+        End If
+
+        Return methodInfo
+
+    End Function
+
+
+    ''' <summary>
+    ''' Generates the methodDescriptor string for a method
+    ''' </summary>
+    ''' <param name="methodDeclaration">The method to generated the descriptor for</param>
+    ''' <returns>The methodDescriptor string</returns>
+    ''' <remarks></remarks>
+    Private Shared Function GetMethodDescriptor(ByVal methodDeclaration As IMethodElement) As String
+
+        Dim sb As New System.Text.StringBuilder(64)
+        Dim parameter As IParameterElement
+        Dim iCount As Int32 = 0
+        Dim blnIsGeneric As Boolean = False
+        Dim genericParameterNames() As String = Nothing
+
+        If Not methodDeclaration Is Nothing Then
+
+            Dim genericElem As IMethodElement = TryCast(methodDeclaration.GenericTemplate, IMethodElement)
+            If genericElem IsNot Nothing Then
+                methodDeclaration = genericElem
+                blnIsGeneric = True
+                ReDim genericParameterNames(methodDeclaration.TypeParameters.Count - 1)
+                For i As Int32 = 0 To methodDeclaration.TypeParameters.Count - 1
+                    genericParameterNames(i) = methodDeclaration.TypeParameters(i).Name
+                Next
+            End If
+
+            sb.Append(methodDeclaration.FullName)
+            If blnIsGeneric Then sb.Replace("`", "``")
+
+            If methodDeclaration.Parameters.Count > 0 Then
+                sb.Append("(")
+
+                For Each parameter In methodDeclaration.Parameters
+
+                    Dim paramName As String
+
+                    If Not parameter.Type Is Nothing Then
+                        If iCount > 0 Then
+                            sb.Append(",")
+                        End If
+
+                        If parameter.Type.IsArrayType Then
+                            paramName = ParserServices.LanguageService.GetFullTypeName(parameter.Type.BaseType.Name)
+                            If blnIsGeneric Then paramName = "``" & Array.IndexOf(genericParameterNames, paramName)
+                            sb.Append(paramName & "[]")
+                        Else
+                            paramName = ParserServices.LanguageService.GetFullTypeName(parameter.Type.Name)
+                            If blnIsGeneric Then paramName = "``" & Array.IndexOf(genericParameterNames, paramName)
+                            sb.Append(paramName)
+                        End If
+
+                        iCount += 1
+                    End If
+                Next
+
+                sb.Append(")")
+
+            End If
+
+        End If
+
+        Return sb.ToString
+
+    End Function
+
+#End Region
+
+#Region " Public Properties "
+
+    ''' <summary>
+    ''' Method name string accessor
+    ''' </summary>
+    ''' <returns>The method name string</returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property MethodDescriptor() As String
+        Get
+            Return m_MethodDescriptor
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Returns a SourceRange indicating the location of the method call reference in the code.
+    ''' </summary>
+    Public ReadOnly Property NameRanges() As SourceRangeCollection
+        Get
+            If m_NameRanges Is Nothing Then
+                m_NameRanges = New SourceRangeCollection()
+            End If
+            Return m_NameRanges
+        End Get
+    End Property
+
+    ''' <summary>
+    ''' Exception list accessor
+    ''' </summary>
+    ''' <returns>The set of exceptions that this method can throw</returns>
+    ''' <remarks></remarks>
+    Public ReadOnly Property Exceptions() As ReadOnlyChainedList(Of ExceptionInfo)
+        Get
+            Return m_ExceptionInfos
+        End Get
+    End Property
+
+#End Region
+
+#Region " Private Methods "
+
+    ''' <summary>
+    ''' Analyses the exception entries in the assembly's xml file
+    ''' </summary>
+    ''' <param name="assemblyReference">The assembly to analyse</param>
+    ''' <remarks></remarks>
+    Private Sub FindExceptions(ByVal assemblyReference As AssemblyReference)
+
+        If assemblyReference Is Nothing Then Return
+
+        m_ExceptionInfos = New ChainedList(Of ExceptionInfo)()
+
+        Dim fileinfo As IO.FileInfo = Nothing
+
+        If assemblyReference.FilePath.StartsWith("file:") Then
+            fileinfo = New IO.FileInfo((New Uri(assemblyReference.FilePath)).AbsolutePath)
+        Else
+            fileinfo = New IO.FileInfo(assemblyReference.FilePath)
+        End If
+
+        fileinfo = New IO.FileInfo(fileinfo.DirectoryName & "\en\" & fileinfo.Name.Substring(0, fileinfo.Name.Length - fileinfo.Extension.Length) & ".xml")
+
+        If Not fileinfo.Exists Then Return
+
+        Dim XMLDoc = XElement.Parse(My.Computer.FileSystem.ReadAllText(fileinfo.FullName))
+        Dim Member = (From item In XMLDoc.<members>.<member> _
+                                 Where item.@name = "M:" & m_MethodDescriptor _
+                                 Select item).FirstOrDefault
+        Dim Exceptions = Member.<exception>
+        For Each Ex As XElement In Exceptions
+            m_ExceptionInfos.Add(New ExceptionInfo(Ex.@cref.Substring(2), Ex.Value))
+        Next
+    End Sub
+
+    ''' <summary>
+    ''' Analyses the exception entries in the provided xml comment block 
+    ''' </summary>
+    ''' <param name="comment">The comment to analyse</param>
+    ''' <remarks></remarks>
+    Private Sub FindExceptions(ByVal comment As XmlDocComment)
+
+        m_ExceptionInfos = New ChainedList(Of ExceptionInfo)()
+
+        If comment Is Nothing Then Return
+
+        Dim strComment As String = comment.ToString
+
+        Dim lines() As String = strComment.Split(New String() {vbCrLf, vbCr, vbLf}, StringSplitOptions.RemoveEmptyEntries)
+
+        For i As Int32 = 0 To lines.Length - 1
+            lines(i) = lines(i).TrimStart(New Char() {ChrW(9), ChrW(32), "'"c})
+        Next
+
+        strComment = "<member name=""M:" & m_MethodDescriptor & """>" & String.Join(vbCrLf, lines) & "</member>"
+
+        Using reader As Xml.XmlReader = New Xml.XmlTextReader(strComment, Xml.XmlNodeType.Element, Nothing)
+            If reader.ReadToDescendant("exception") Then
+                Do
+                    Dim exName As String = reader.GetAttribute("cref")
+                    Dim content As String
+                    If exName.StartsWith("T:") Then
+                        exName = exName.Substring(2)
+                    Else
+                        exName = CodeRush.Source.GetDeclaration(comment, exName).FullName
+                    End If
+                    content = reader.ReadInnerXml
+                    m_ExceptionInfos.Add(New ExceptionInfo(exName, content))
+                Loop While reader.ReadToNextSibling("exception")
+            End If
+        End Using
+
+    End Sub
+
+
+    Private Sub FindExceptions(ByVal reader As Xml.XmlReader)
+        reader.ReadToFollowing("member")
+        Dim strName As String = "M:" & m_MethodDescriptor
+        While reader.GetAttribute("name") <> strName
+            reader.Skip()
+            reader.Read()
+        End While
+        If reader.ReadToDescendant("exception") Then
+            Do
+                Dim exName As String = reader.GetAttribute("cref")
+                Dim content As String
+                If exName.StartsWith("T:") Then exName = exName.Substring(2)
+                content = reader.ReadInnerXml
+                m_ExceptionInfos.Add(New ExceptionInfo(exName, content))
+            Loop While reader.ReadToNextSibling("exception")
+        End If
+    End Sub
+
+
+#End Region
+
+End Class
---
+++ trunk/CR_ExceptionHelper/CR_ExceptionHelper/TryCatchGenerator.vb
@@ -0,0 +1,288 @@
+Option Strict On
+Option Explicit On
+
+Imports System
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.CodeRush.Core
+Imports DevExpress.CodeRush.PlugInCore
+Imports DevExpress.CodeRush.StructuralParser
+Imports DevExpress.CodeRush.Interop.OLE.Helpers
+Imports System.Collections
+
+<DesignerCategory("")> <ToolboxItem(True)> _
+Public Class TryCatchGenerator
+	Implements IComponent
+
+
+#Region "Constructors"
+	''' <summary>
+	''' called by the component design surface
+	''' </summary>
+	''' <param name="container">contianer for disposal linking</param>
+	''' <remarks></remarks>
+	Public Sub New(ByVal container As System.ComponentModel.IContainer)
+		MyClass.New()
+		If container IsNot Nothing Then container.Add(Me)
+	End Sub
+
+	Public Sub New()
+		MyBase.New()
+	End Sub
+
+#End Region
+
+
+#Region "Try Catch wrapping methods"
+
+
+	''' <summary>
+	''' Inserts a Try Catch block around the textDocument active Selection 
+	''' </summary>
+	''' <param name="handlers">set of exceptions to provide catch blocks for.</param>
+	''' <remarks></remarks>
+	Friend Sub WrapSelectionInTryCatch(ByVal handlers As IEnumerable(Of ExceptionHandler))
+
+		Dim doc As TextDocument = CodeRush.Documents.ActiveTextDocument
+
+		Dim selectionStartLine As Integer = CodeRush.TextViews.Active.Selection.Range.Top.Line
+		Dim startLine As String = doc.GetLine(selectionStartLine)
+		Dim leadingWhiteSpace As String = CodeRush.StrUtil.GetLeadingWhiteSpace(startLine)
+
+		Dim code As String = WrapInTryCatch(CodeRush.TextViews.Active.Selection.Text, leadingWhiteSpace, handlers)
+
+        Dim sr As SourceRange = doc.Selection.Range
+		If Not sr.StartPrecedesEnd Then 'swap the start and end to ensure correct replace
+			sr = New SourceRange(sr.End, sr.Start)
+		End If
+
+		Dim lParentUnit As ParentUndoUnit = CodeRush.UndoStack.OpenParentUnit("Add Exception Handlers", True)
+		Try
+			doc.DeleteText(sr)
+			Dim insertedTextRange As SourceRange = doc.ExpandText(sr.Start, code)	 ' Using ExpandText to expand "«Caret»" and "«Marker»", added by WrapInTryCatch - Mark.
+			doc.Format(insertedTextRange)
+		Finally
+			CodeRush.UndoStack.CommitParentUnit(lParentUnit)
+		End Try
+
+		' The Queued edits do not format the code, and in C# this is necessary, so the lines below
+		' have been replaced with the lines above.
+
+		'doc.QueueReplace(sr, code)
+		'doc.apApplyQueuedEdits("Add Exception Handler")
+	End Sub
+
+
+    ''' <summary>
+    ''' Wraps the given text in a Try Catch block
+    ''' </summary>
+    ''' <param name="selectionText">the text to wrap in the Try Catch clock</param>
+    ''' <param name="handlers">set of exceptions to provide Catch blocks for</param>
+    ''' <returns>the text wrapped in the Try Catch</returns>
+    ''' <remarks></remarks>
+	Friend Function WrapInTryCatch(ByVal selectionText As String, ByVal leadingWhiteSpace As String, ByVal handlers As IEnumerable(Of ExceptionHandler)) As String
+        Dim listOfHandlers As List(Of ExceptionHandler) = SortExceptions(handlers)
+        Dim lang As LanguageServices = CodeRush.Language
+        Dim eb As ElementBuilder = lang.GetActiveElementBuilder
+        Dim tryElem As LanguageElement = eb.AddTry(Nothing)
+
+        eb.AddSnippetCodeElement(tryElem, selectionText)
+        For Each item As ExceptionHandler In listOfHandlers
+            Dim ce As [Catch] = eb.AddCatch(Nothing, item.ExceptionName, "ex")
+            Dim throwersAlreadyDocumented As Hashtable = New Hashtable()     ' Make sure we document multiple callers only once.
+            For Each exMethInfo As ExceptionInfo In item.ExceptionMethodInfos
+                Dim exceptionThrower As String = exMethInfo.TypeName
+                If Not throwersAlreadyDocumented.ContainsKey(exceptionThrower) Then
+                    throwersAlreadyDocumented.Add(exceptionThrower, Nothing)
+
+                    'AddComment(ce, String.Format("Thrown from {0}.", exceptionThrower))
+                    ' TODO: Convert <cref> and other XML doc tags in exMethInfo.Description to plain text. -- Suggested by Mark.
+                    'AddComment(ce, String.Format("Message: {0}.", exMethInfo.Description))
+
+                    ' Use the technique above to add comments -- Mark
+
+                    '				eb.AddStatement(ce, lang.GetComment(" thrown by : " & exMethInfo.TypeName).TrimEnd)
+                    '				eb.AddStatement(ce, lang.GetComment(" description :" & exMethInfo.Description).TrimEnd)
+                End If
+            Next
+
+            ' To add an empty line, use this code (the call to AddStatement with an empty string generated lines consisting of semi-colons in C# -- Mark.
+            eb.AddSnippetCodeElement(ce, leadingWhiteSpace & "«Marker»" & Environment.NewLine)
+
+            ce.AddNode(New [Throw])
+            ' Use the code above (instead of the code below) to add a new throw statement. We'll add an AddThrow method to ElementBuilder as well -- Mark.
+            'eb.AddStatement(ce, lang.CreateLanguageElement(LanguageElementType.Throw))
+        Next
+
+		Return eb.GenerateCode()
+
+	End Function
+    Private Sub AddComment(ByVal parentElement As LanguageElement, ByVal commentText As String)
+        Dim eb As New ElementBuilder
+        Dim CommentLines As List(Of String) = ConvertToMaxWidthParagraph(commentText, 80)
+        For Each line As String In CommentLines
+            ' eb.BuildComment(line, CommentType.SingleLine)
+            parentElement.AddNode(New Comment() With {.Name = " " & line})
+        Next
+    End Sub
+    Private Function ConvertToMaxWidthParagraph(ByVal Comment As String, ByVal MaxWidth As Integer) As List(Of String)
+        Dim Segment As String
+        Comment &= " "
+        If Comment.Length <= MaxWidth AndAlso Not Comment.Contains(Chr(10)) Then
+            Return New String() {Comment}.ToList
+        End If
+        Dim List As New List(Of String)
+        For Each InnerComment As String In Comment.Split(Chr(10))
+            If InnerComment.Trim <> String.Empty AndAlso InnerComment.Trim <> "." Then
+                Do
+                    Segment = InnerComment.Substring(0, Math.Min(MaxWidth, InnerComment.Length))
+                    Segment = InnerComment.Substring(0, Segment.LastIndexOf(" "))
+
+                    List.Add(Segment)
+                    ' Reduce SourceComment 
+                    InnerComment = InnerComment.Substring(Segment.Length)
+                Loop Until InnerComment.Length < 10 OrElse Segment = String.Empty
+            End If
+
+        Next
+        Return List
+    End Function
+
+
+#End Region
+
+
+#Region "utility methods"
+
+
+	''' <summary>
+	''' Creates an ExceptionHandlerCollection based on a list of MethodExceptionInfos
+	''' </summary>
+	''' <param name="methods">The MethodExceptionInfos to combine</param>
+	''' <returns>An ExceptionHandlerCollection</returns>
+	''' <remarks></remarks>
+	Friend Shared Function GetExceptionHandlerCollection(ByVal methods As IEnumerable(Of MethodExceptionInfo)) As ExceptionHandlerCollection
+		Dim handlers As New ExceptionHandlerCollection
+		For Each item As MethodExceptionInfo In methods
+			For Each ex As ExceptionInfo In item.Exceptions
+				handlers.Add(ex.TypeName, item.MethodDescriptor, item.NameRanges, ex.Description)
+			Next
+		Next
+		Return handlers
+	End Function
+
+
+#End Region
+
+
+#Region "(private) sorting of exceptions"
+
+	'Private m_Exceptions As New Dictionary(Of String, Int32)
+
+	Private Function SortExceptions(ByVal handlers As IEnumerable(Of ExceptionHandler)) As List(Of ExceptionHandler)
+		Dim listOfHandlers As New List(Of ExceptionHandler)
+		For Each item As ExceptionHandler In handlers
+			listOfHandlers.Add(item)
+		Next
+		listOfHandlers.Sort(New Comparison(Of ExceptionHandler)(AddressOf HandlerComparer))
+		' only cache exception depth per call per thread
+		Me.ExceptionDepths.Clear()
+		Return listOfHandlers
+	End Function
+
+
+	Private Function HandlerComparer(ByVal x As ExceptionHandler, ByVal y As ExceptionHandler) As Int32
+		Return GetExceptionDepth(y).CompareTo(GetExceptionDepth(x))
+	End Function
+
+
+	Private Function GetExceptionDepth(ByVal ex As ExceptionHandler) As Int32
+		Dim value As Int32 = 0
+		If Me.ExceptionDepths.TryGetValue(ex.ExceptionName, value) Then Return value
+
+		Dim typ As Type = Type.GetType(ex.ExceptionName, False, False)
+
+		If typ Is Nothing Then
+			For Each ass As AssemblyReference In CodeRush.Documents.Active.ProjectElement.AssemblyReferences
+				Dim assembly As Reflection.Assembly = Reflection.Assembly.ReflectionOnlyLoadFrom(ass.FilePath)
+				typ = assembly.GetType(ex.ExceptionName, False, False)
+				If typ IsNot Nothing Then Exit For
+			Next
+		End If
+
+		If typ Is Nothing Then
+			' must be a live code declaration
+			Dim el As ITypeElement = TryCast(CodeRush.Documents.Active.ProjectElement.FindElementByFullName(ex.ExceptionName, True, True), ITypeElement)
+			While el IsNot Nothing
+				el = el.GetBaseType
+				value += 1
+			End While
+
+		Else
+			Do While typ IsNot Nothing
+				typ = typ.BaseType
+				value += 1
+			Loop
+		End If
+
+		Me.ExceptionDepths.Add(ex.ExceptionName, value)
+
+		Return value
+	End Function
+
+
+	' thread safe storage of exceptions per call per thread
+	Private m_ExceptionDepths As New My.MyProject.ThreadSafeObjectProvider(Of Dictionary(Of String, Int32))
+
+	Private ReadOnly Property ExceptionDepths() As Dictionary(Of String, Int32)
+		Get
+			Return m_ExceptionDepths.GetInstance
+		End Get
+	End Property
+
+
+#End Region
+
+
+#Region "IComponent Interface"
+	<System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId:="0#")> <System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1725:ParameterNamesShouldMatchBaseDeclaration", MessageId:="0#")> _
+	Public Event Disposed As EventHandler Implements System.ComponentModel.IComponent.Disposed
+
+	Private m_Site As ISite
+
+	Private Property Site() As System.ComponentModel.ISite Implements System.ComponentModel.IComponent.Site
+		Get
+			Return Me.m_Site
+		End Get
+		Set(ByVal value As System.ComponentModel.ISite)
+			Me.m_Site = value
+		End Set
+	End Property
+
+	Private disposedValue As Boolean		' To detect redundant calls
+
+	' IDisposable
+	Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+		If Not Me.disposedValue Then
+			If disposing Then
+				' free unmanaged resources when explicitly called
+			End If
+
+			' free shared unmanaged resources
+		End If
+		Me.disposedValue = True
+		RaiseEvent Disposed(Me, EventArgs.Empty)
+	End Sub
+
+#End Region
+
+#Region " IDisposable Support "
+
+	Public Sub Dispose() Implements IDisposable.Dispose
+		Dispose(True)
+		GC.SuppressFinalize(Me)
+	End Sub
+#End Region
+
+End Class
