731e1dc1bef2c6d9738b82b5974f84b959a3fe9f
RoryBecker@Gmail.com <RoryBecker@Gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-06-10 19:15:24 +0000

    New Minor plugin ClearAllMarkers

---
+++ trunk/CR_ClearAllMarkers/CR_ClearAllMarkers/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports DevExpress.CodeRush.Common
+
+<Assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "CR_ClearAllMarkers", PlugInLoadType.Demand, LoadAbilityType.LoadEnabled)>
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CR_ClearAllMarkers")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("CR_ClearAllMarkers")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2af38d42-49ba-4efa-8a60-35a7e615eb34")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
