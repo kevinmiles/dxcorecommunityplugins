ece705dc810856069789456438ce0853a209d103
rorybecker <RoryBecker@Gmail.com>
2009-06-10 19:15:24 +0000

    New Minor plugin ClearAllMarkers

---
+++ CR_ClearAllMarkers/CR_ClearAllMarkers/PlugIn1.vb
@@ -0,0 +1,29 @@
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Windows.Forms
+Imports DevExpress.CodeRush.Core
+Imports DevExpress.CodeRush.PlugInCore
+Imports DevExpress.CodeRush.StructuralParser
+
+Public Class PlugIn1
+
+	'DXCore-generated code...
+#Region " InitializePlugIn "
+	Public Overrides Sub InitializePlugIn()
+		MyBase.InitializePlugIn()
+
+		'TODO: Add your initialization code here.
+	End Sub
+#End Region
+#Region " FinalizePlugIn "
+	Public Overrides Sub FinalizePlugIn()
+		'TODO: Add your finalization code here.
+
+		MyBase.FinalizePlugIn()
+	End Sub
+#End Region
+
+    Private Sub ClearAllMarkers_Execute(ByVal ea As DevExpress.CodeRush.Core.ExecuteEventArgs) Handles ClearAllMarkers.Execute
+        CodeRush.Markers.Clear()
+    End Sub
+End Class
