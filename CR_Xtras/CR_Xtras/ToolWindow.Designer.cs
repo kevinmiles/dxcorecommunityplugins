a42bf5e532649204ca7f4b87f959cacc5c78c962
dangilkerson <dangilkerson@241145c3-2f4c-0410-ae16-21396c40807f>
2009-07-24 01:26:17 +0000

    Basic form to list plugins.

--- CR_Xtras/CR_Xtras/ToolWindow.Designer.cs
+++ CR_Xtras/CR_Xtras/ToolWindow.Designer.cs
@@ -42,10 +42,51 @@ namespace CR_Xtras
 		{
 			this.components = new System.ComponentModel.Container();
 			this.events = new DevExpress.DXCore.PlugInCore.DXCoreEvents(this.components);
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
 			((System.ComponentModel.ISupportInitialize)(this.events)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+			this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.listView1.Location = new System.Drawing.Point(0, 0);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(301, 310);
+			this.listView1.TabIndex = 1;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			this.listView1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Name";
+			this.columnHeader1.Width = 114;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Installed";
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Version";
+			this.columnHeader3.Width = 108;
+			// 
+			// ToolWindow
+			// 
+			this.Controls.Add(this.listView1);
+			this.Name = "ToolWindow";
+			this.Size = new System.Drawing.Size(301, 310);
 			((System.ComponentModel.ISupportInitialize)(this.events)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
@@ -68,5 +109,10 @@ namespace CR_Xtras
 			return DevExpress.CodeRush.Core.CodeRush.ToolWindows.Hide(typeof(ToolWindow).GUID);
 		}
 		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
 	}
 }
