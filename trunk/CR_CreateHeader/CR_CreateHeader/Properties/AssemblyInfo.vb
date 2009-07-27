97b1ea4c29f6d46a9ec32284c4f758c064cc268d
RoryBecker@gmail.com <RoryBecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-07-27 22:44:21 +0000

    Adding CR_CreateHeader Plugin

---
+++ trunk/CR_CreateHeader/CR_CreateHeader/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports DevExpress.CodeRush.Common
+
+<Assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "CR_CreateHeader", PlugInLoadType.Demand, LoadAbilityType.LoadDisabled)>
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("CR_CreateHeader")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("CR_CreateHeader")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("e1ae4fb1-f5c7-44c6-97af-e1a26b4fdaa8")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
