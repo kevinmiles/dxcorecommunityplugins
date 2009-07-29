f3d9626d2546750f49aa5c5c31de0f047c65de08
rorybecker <RoryBecker@Gmail.com>
2009-07-29 08:26:23 +0000

    CR_MultiFileTemplates: Undo Enhancements

--- CR_MultiFileTemplates/CR_MultiFileTemplates/PlugIn1.vb
+++ CR_MultiFileTemplates/CR_MultiFileTemplates/PlugIn1.vb
@@ -24,23 +24,30 @@ Public Class PlugIn1
     End Sub
 #End Region
 
-
     Private Sub ExpandTemplateToFile_Execute(ByVal ea As DevExpress.CodeRush.Core.ExecuteTextCommandEventArgs) Handles ExpandTemplateToFile.Execute
         ' Template Name and Category
         Dim FullTemplateNameAndPath = ea.GetParameterValue(0)
         Dim TemplateName = FullTemplateNameAndPath.Split("\"c).Last
         Dim TemplateCategory = FullTemplateNameAndPath.Substring(0, FullTemplateNameAndPath.Length - TemplateName.Length - 1)
-
+        Dim StartFile As String = CodeRush.Documents.ActiveFileName
         Dim FileName = ea.GetParameterValue(1)
         Dim Language = CodeRush.Documents.ActiveLanguage
         Dim Template = CodeRush.Templates.FindTemplate(TemplateName, TemplateCategory, Language)
         Dim FinalText = CodeRush.Strings.Expand(Template.FirstItemInContext.Expansion)
         Dim FilePath As String = GetActiveFolder() & "\" & FileName
+
         Call My.Computer.FileSystem.WriteAllText(FilePath, FinalText, False)
+        CodeRush.UndoStack.Add(New CreatedFileUndoUnit(FilePath, FinalText))
+        Dim projectName = CodeRush.Project.Active.Name
         CodeRush.Project.Active.AddFile(FilePath)
-        If CBool(ea.GetParameterValue(2)) Then
+        CodeRush.Solution.AddFileToProject(projectName, FilePath)
+        CodeRush.UndoStack.Add(New AddedProjectFileUndoUnit(projectName, FilePath))
+
+        If CBool(ea.Parameters.Length > 2) And CBool(ea.GetParameterValue(2)) Then
+            CodeRush.UndoStack.Add(New FileActivateUndoUnit(StartFile))
             CodeRush.File.Activate(FilePath)
         End If
+
 
     End Sub
     Private Function GetActiveFolder() As String
