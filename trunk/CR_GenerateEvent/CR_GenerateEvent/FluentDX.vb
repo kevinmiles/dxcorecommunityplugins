f5869034fe06a839759f63451b42c30bf9957794
RoryBecker@Gmail.com <RoryBecker@Gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-07-29 08:06:38 +0000

    Minor refactoring of CR_GenerateEvent

--- trunk/CR_GenerateEvent/CR_GenerateEvent/FluentDX.vb
+++ trunk/CR_GenerateEvent/CR_GenerateEvent/FluentDX.vb
@@ -1,3 +1,4 @@
+Option Infer On
 Imports System.ComponentModel
 Imports System.Drawing
 Imports System.Windows.Forms
@@ -18,7 +19,9 @@ Public Module FluentDX
         Return TryCast(LE, IEventElement)
     End Function
     <Extension()> _
-    Public Function GenerateCode(ByVal LE As LanguageElement) As String
-        Return CodeRush.CodeMod.GenerateCode(LE)
+    Public Function GenerateCode(ByVal LE As LanguageElement, Optional ByVal SurroundWithCRLF As Boolean = False) As String
+        Return String.Format("{0}{1}{0}", _
+                             If(SurroundWithCRLF, System.Environment.NewLine, String.Empty), _
+                             CodeRush.CodeMod.GenerateCode(LE))
     End Function
 End Module
