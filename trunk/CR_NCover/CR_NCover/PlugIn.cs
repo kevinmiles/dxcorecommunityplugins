b2330c83d154a8a82c624db411287a9f3a0e9234
dangilkerson <dangilkerson@241145c3-2f4c-0410-ae16-21396c40807f>
2009-08-26 05:20:48 +0000

    Hides coverage overlay when text changes.

--- trunk/CR_NCover/CR_NCover/PlugIn.cs
+++ trunk/CR_NCover/CR_NCover/PlugIn.cs
@@ -6,7 +6,8 @@ namespace CR_NCover
 	public partial class PlugIn : StandardPlugIn
 	{
 		private readonly CoverageOverlay overlay = new CoverageOverlay();
-		private readonly Service service = new Service();
+        private bool showingOverlay = false;
+        private readonly Service service = new Service();
 
 		public override void InitializePlugIn()
 		{
@@ -23,18 +24,22 @@ namespace CR_NCover
 
 		public void WhenResultsUpdated()
 		{
+            showingOverlay = true;
+            
 			if (VisualStudio.HasAnActiveTextView)
-				overlay.Show(service.GetCoverageResultsForActiveDocument());
+                overlay.Show(service.GetCoverageResultsForActiveDocument());
 		}
 
 		public void WhenPaintingBackground(EditorPaintEventArgs ea)
 		{
-			overlay.Update(service.GetCoverageResultsForActiveDocument());
+            if (showingOverlay)
+			    overlay.Update(service.GetCoverageResultsForActiveDocument());
 		}
 
 		public void WhenTextChanges(TextChangedEventArgs ea)
 		{
-			overlay.Hide();
+            overlay.Hide();
+            showingOverlay = false;  
 		}
 
 		private void StartResultWatcher()
