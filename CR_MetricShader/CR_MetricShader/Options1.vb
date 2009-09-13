c9974d93c49d27b262a7230c84fa107267b20602
rorybecker <RoryBecker@Gmail.com>
2009-09-13 14:27:38 +0000

    Updates to CR_MetricShader. These include updates to increase performance by... ...Only processing Members which appear on screen ...Not painting anything if the member falls within the optimal quarter rather than actively painting Color.White.

--- CR_MetricShader/CR_MetricShader/Options1.vb
+++ CR_MetricShader/CR_MetricShader/Options1.vb
@@ -6,6 +6,8 @@ Imports System.Runtime.CompilerServices
 
 <UserLevel(UserLevel.NewUser)> _
 Public Class Options1
+    Friend Const STR_MetricName As String = "MetricName"
+    Friend Const STR_MetricShader As String = "MetricShader"
 
     'DXCore-generated code...
 #Region " Initialize "
@@ -30,25 +32,25 @@ Public Class Options1
     Friend Shared Function Providers() As ICodeMetricProvider()
         Return CodeRush.Source.GetAllCodeMetricProviders.Where(Function(f) f.MetricGoal <> MetricGoal.Types).ToArray
     End Function
-    Private Sub Options1_RestoreDefaults(ByVal sender As Object, ByVal ea As DevExpress.CodeRush.Core.OptionsPageEventArgs) Handles Me.RestoreDefaults
-        ' Load default values
+    Private Sub RefreshProviderList()
         cbxMetric.Items.Clear()
         For Each Provider In Providers()
             cbxMetric.Items.Add(Provider.DisplayName)
         Next
+    End Sub
+    Private Sub Options1_RestoreDefaults(ByVal sender As Object, ByVal ea As OptionsPageEventArgs) Handles Me.RestoreDefaults
+        ' Load default values
+        Call RefreshProviderList()
         cbxMetric.SelectedIndex = 0
     End Sub
 
-    Private Sub Options1_PreparePage(ByVal sender As Object, ByVal ea As DevExpress.CodeRush.Core.OptionsPageStorageEventArgs) Handles Me.PreparePage
-        cbxMetric.Items.Clear()
-        For Each Provider In Providers()
-            cbxMetric.Items.Add(Provider.DisplayName)
-        Next
-        cbxMetric.SelectedIndex = ea.Storage.ReadInt32("MetricShader", "MetricName", 0)
+    Private Sub Options1_PreparePage(ByVal sender As Object, ByVal ea As OptionsPageStorageEventArgs) Handles Me.PreparePage
+        Call RefreshProviderList()
+        cbxMetric.SelectedIndex = ea.Storage.ReadInt32(STR_MetricShader, STR_MetricName, 0)
     End Sub
 
-    Private Sub Options1_CommitChanges(ByVal sender As Object, ByVal ea As DevExpress.CodeRush.Core.CommitChangesEventArgs) Handles Me.CommitChanges
-        ea.Storage.WriteInt32("MetricShader", "MetricName", cbxMetric.SelectedIndex)
+    Private Sub Options1_CommitChanges(ByVal sender As Object, ByVal ea As CommitChangesEventArgs) Handles Me.CommitChanges
+        ea.Storage.WriteInt32(STR_MetricShader, STR_MetricName, cbxMetric.SelectedIndex)
     End Sub
 
     Private Sub cbxMetric_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cbxMetric.SelectedIndexChanged
--- CR_MetricShader/CR_MetricShader/PlugIn1.vb
+++ CR_MetricShader/CR_MetricShader/PlugIn1.vb
@@ -1,13 +1,15 @@
-
+Option Strict On
+Option Explicit On
+Option Infer On
 Imports System.ComponentModel
 Imports System.Drawing
 Imports System.Windows.Forms
 Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
+Imports System.Runtime.CompilerServices
 
 Public Class PlugIn1
-
 
     'DXCore-generated code...
 #Region " InitializePlugIn "
@@ -29,38 +31,42 @@ Public Class PlugIn1
 
     Private mMetric As ICodeMetricProvider
     Private Sub LoadSettings()
-        mMetric = Options1.Providers(Options1.Storage.ReadInt32("MetricShader", "MetricName", 0))
+        mMetric = Options1.Providers(Options1.Storage.ReadInt32(Options1.STR_MetricShader, Options1.STR_MetricName, 0))
     End Sub
     Private Sub PlugIn1_EditorPaintBackground(ByVal ea As EditorPaintEventArgs) Handles Me.EditorPaintBackground
         If CodeRush.Source.ActiveClass Is Nothing Then
             Exit Sub
         End If
-        For Each Member In CodeRush.Source.ActiveClass.AllMembers.OfType(Of Member)()
-            Dim View = CodeRush.Documents.ActiveTextView
-            Dim Rect As Rectangle = View.GetRectangleFromRange(Member.GetFullBlockCutRange)
-            View.HighlightCode(Member.GetFullBlockCutRange, GetColor(Member), GetColor(Member), Color.White)
+        Dim View = CodeRush.Documents.ActiveTextView
+        Dim Members = CodeRush.Source.ActiveClass.AllMembers.OfType(Of Member)()
+        Dim ViewRange As SourceRange = View.GetRangeFromRectangle(View.Bounds)
+        For Each Member In Members.Where(Function(m) ViewRange.Intersects(m.GetFullBlockCutRange))
+            Dim PaintColor = GetColor(Member)
+            If PaintColor.HasValue Then
+                View.HighlightCode(Member.GetFullBlockCutRange, PaintColor.Value, PaintColor.Value, Color.White)
+            End If
         Next
     End Sub
-    Private Function GetColor(ByVal Member As Member) As Color
+    Private Function GetColor(ByVal Member As Member) As Nullable(Of Color)
         Select Case mMetric.GetValue(Member)
-            Case Is < 0.25 * mMetric.WarningValue
-                Return Color.FromArgb(0, Color.White)
-            Case Is < 0.5 * mMetric.WarningValue
+            Case Is < CInt(0.25 * mMetric.WarningValue)
+                Return Nothing
+            Case Is < CInt(0.5 * mMetric.WarningValue)
                 Return Color.FromArgb(30, Color.Green)
-            Case Is < 0.75 * mMetric.WarningValue
+            Case Is < CInt(0.75 * mMetric.WarningValue)
                 Return Color.FromArgb(50, Color.Orange)
             Case Else
                 Return Color.FromArgb(75, Color.Red)
         End Select
     End Function
 
-    Private Sub LineCountDemoMetric_GetMetricValue(ByVal sender As System.Object, ByVal e As DevExpress.CodeRush.Extensions.GetMetricValueEventArgs) Handles LineCountDemoMetric.GetMetricValue
-        e.Value = e.LanguageElement.Range.Height
-    End Sub
-
-    Private Sub PlugIn1_OptionsChanged(ByVal ea As DevExpress.CodeRush.Core.OptionsChangedEventArgs) Handles Me.OptionsChanged
+    Private Sub PlugIn1_OptionsChanged(ByVal ea As OptionsChangedEventArgs) Handles Me.OptionsChanged
         If ea.OptionsPages.Contains(GetType(Options1)) Then
             LoadSettings()
         End If
+    End Sub
+
+    Private Sub LineCountDemoMetric_GetMetricValue(ByVal sender As System.Object, ByVal e As DevExpress.CodeRush.Extensions.GetMetricValueEventArgs) Handles LineCountDemoMetric.GetMetricValue
+        e.Value = e.LanguageElement.Range.Height
     End Sub
 End Class
