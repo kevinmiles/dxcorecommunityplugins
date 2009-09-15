9d318e4d508b0f645406740eae61e937a03d8f28
RoryBecker@gmail.com <RoryBecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-09-15 21:51:37 +0000

    Changes to CR_MetricShader to Add a Toggle Button to the DXCore Visualize Toolbar.

--- trunk/CR_MetricShader/CR_MetricShader/PlugIn1.vb
+++ trunk/CR_MetricShader/CR_MetricShader/PlugIn1.vb
@@ -8,6 +8,9 @@ Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
 Imports System.Runtime.CompilerServices
+Imports System.Reflection
+Imports DevExpress.CodeRush.Menus
+Imports DevExpress.CodeRush.Extensions
 
 Public Class PlugIn1
 
@@ -18,6 +21,7 @@ Public Class PlugIn1
 
         'TODO: Add your initialization code here.
         Call LoadSettings()
+        Call CreateVisualizeButton()
 
     End Sub
 #End Region
@@ -30,22 +34,36 @@ Public Class PlugIn1
 #End Region
 
     'Improvements
-    ' "Enabled" option.
+    ' Done - "Enabled" option
+    ' Done - DXCore visualize Toolbar
     ' Configure %ages
     ' Configure Colors
     ' Configure Opacity
     ' Configure Widths
 
-
+#Region "Fields"
     Private mEnabled As Boolean = True ' Default to On
     Private mMetric As ICodeMetricProvider
+    Private mVisualizeButton As IMenuButton
+#End Region
+#Region "Resources"
+    Public Function GetBitmapByName(ByVal BitmapName As String) As Bitmap
+        Dim Asm As Assembly = Assembly.GetAssembly(Me.GetType)
+        Dim stream As IO.Stream = Asm.GetManifestResourceStream(String.Format("CR_MetricShader.{0}", BitmapName))
+        Return CType(Bitmap.FromStream(stream), Bitmap)
+    End Function
+#End Region
+#Region "Settings"
     Private Sub LoadSettings()
         mMetric = Options1.Providers(Options1.Storage.ReadInt32(Options1.STR_MetricShader, Options1.STR_MetricName, 0))
         mEnabled = Options1.Storage.ReadBoolean(Options1.STR_MetricShader, Options1.STR_ShaderEnabled, True)
-        For Each TextView In CodeRush.TextViews.Cast(Of TextView)()
-            Call TextView.Invalidate()
-        Next
+        Call CodeRush.TextViews.Refresh()
+        If (mVisualizeButton IsNot Nothing) Then
+            mVisualizeButton.IsDown = mEnabled
+        End If
     End Sub
+#End Region
+#Region "Editor Events"
     Private Sub PlugIn1_EditorPaintBackground(ByVal ea As EditorPaintEventArgs) Handles Me.EditorPaintBackground
         If CodeRush.Source.ActiveClass Is Nothing Then
             Exit Sub
@@ -63,6 +81,45 @@ Public Class PlugIn1
             End If
         Next
     End Sub
+
+    Private Sub PlugIn1_OptionsChanged(ByVal ea As OptionsChangedEventArgs) Handles Me.OptionsChanged
+        If ea.OptionsPages.Contains(GetType(Options1)) Then
+            LoadSettings()
+        End If
+    End Sub
+#End Region
+#Region "Visualize Button"
+    Private Sub CreateVisualizeButton()
+        If (CodeRush.Menus Is Nothing OrElse CodeRush.Menus.VisualizeToolBar Is Nothing) Then
+            Return
+        End If
+        Dim Image = GetBitmapByName("CR_MetricShader.ico")
+        mVisualizeButton = CodeRush.Menus.VisualizeToolBar.AddButton(Options1.GetPageName(), Image)
+        If (mVisualizeButton Is Nothing) Then
+            Return
+        End If
+        mVisualizeButton.Style = ButtonStyle.Icon
+        mVisualizeButton.SetFace(Image, Image)
+        mVisualizeButton.IsDown = mEnabled
+        AddHandler mVisualizeButton.Click, AddressOf VisualizeButton_Click
+    End Sub
+
+    Private Sub VisualizeButton_Click(ByVal sender As Object, ByVal e As MenuButtonClickEventArgs)
+        ' Toggle Feature
+        mEnabled = Not mEnabled
+        Using Storage = Options1.Storage
+            Storage.WriteBoolean(Options1.STR_MetricShader, Options1.STR_ShaderEnabled, mEnabled)
+        End Using
+        Call LoadSettings()
+    End Sub
+#End Region
+
+#Region "Demo Metric"
+    Private Sub LineCountDemoMetric_GetMetricValue(ByVal sender As System.Object, ByVal e As GetMetricValueEventArgs) Handles LineCountDemoMetric.GetMetricValue
+        e.Value = e.LanguageElement.Range.Height
+    End Sub
+#End Region
+
     Private Function GetColor(ByVal Member As Member) As Nullable(Of Color)
         Select Case mMetric.GetValue(Member)
             Case Is < CInt(0.25 * mMetric.WarningValue)
@@ -76,13 +133,4 @@ Public Class PlugIn1
         End Select
     End Function
 
-    Private Sub PlugIn1_OptionsChanged(ByVal ea As OptionsChangedEventArgs) Handles Me.OptionsChanged
-        If ea.OptionsPages.Contains(GetType(Options1)) Then
-            LoadSettings()
-        End If
-    End Sub
-
-    Private Sub LineCountDemoMetric_GetMetricValue(ByVal sender As System.Object, ByVal e As DevExpress.CodeRush.Extensions.GetMetricValueEventArgs) Handles LineCountDemoMetric.GetMetricValue
-        e.Value = e.LanguageElement.Range.Height
-    End Sub
 End Class
