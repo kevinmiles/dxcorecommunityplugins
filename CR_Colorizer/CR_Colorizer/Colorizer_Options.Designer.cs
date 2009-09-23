f41112ccd40e0866c4355c01e3ff06c5c6b13d0d
casey.kramer <casey.kramer@gmail.com>
2009-09-23 19:33:38 +0000

    A New plugin which provides for specialized colorization of specific types of language elements such as fields, public/private properties and methods, method parameters, and local variables.

---
+++ CR_Colorizer/CR_Colorizer/Colorizer_Options.Designer.cs
@@ -0,0 +1,132 @@
+using System;
+using DevExpress.CodeRush.Core;
+
+namespace CR_Colorizer
+{
+    partial class Colorizer_Options
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Colorizer_Options()
+        {
+            /// <summary>
+            /// Required for Windows.Forms Class Composition Designer support
+            /// </summary>
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(6, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(65, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Enabled";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // Colorizer_Options
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Colorizer_Options";
+            this.PreparePage += new DevExpress.CodeRush.Core.OptionsPage.PreparePageEventHandler(this.Colorizer_Options_PreparePage);
+            this.CommitChanges += new DevExpress.CodeRush.Core.OptionsPage.CommitChangesEventHandler(this.Colorizer_Options_CommitChanges);
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        ///
+        /// Gets a DecoupledStorage instance for this options page.
+        ///
+        public static DecoupledStorage Storage
+        {
+            get
+            {
+                return DevExpress.CodeRush.Core.CodeRush.Options.GetStorage(GetCategory(), GetPageName());
+            }
+        }
+        ///
+        /// Returns the category of this options page.
+        ///
+        public override string Category
+        {
+            get
+            {
+                return Colorizer_Options.GetCategory();
+            }
+        }
+        ///
+        /// Returns the page name of this options page.
+        ///
+        public override string PageName
+        {
+            get
+            {
+                return Colorizer_Options.GetPageName();
+            }
+        }
+        ///
+        /// Returns the full path (Category + PageName) of this options page.
+        ///
+        public static string FullPath
+        {
+            get
+            {
+                return GetCategory() + "\\" + GetPageName();
+            }
+        }
+
+        ///
+        /// Displays the DXCore options dialog and selects this page.
+        ///
+        public new static void Show()
+        {
+            DevExpress.CodeRush.Core.CodeRush.Command.Execute("Options", FullPath);
+        }
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        
+        private System.Windows.Forms.Label label1;
+    }
+}
