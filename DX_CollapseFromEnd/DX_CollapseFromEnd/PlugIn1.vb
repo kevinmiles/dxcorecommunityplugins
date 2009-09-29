b4d08d948e7f672a6b8d513b9ed305dbdf413337
rorybecker <RoryBecker@Gmail.com>
2009-09-29 21:03:32 +0000

    Added Graphic for DX_CollapseFromEnd

--- DX_CollapseFromEnd/DX_CollapseFromEnd/PlugIn1.vb
+++ DX_CollapseFromEnd/DX_CollapseFromEnd/PlugIn1.vb
@@ -4,6 +4,7 @@ Imports System.Windows.Forms
 Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
+Imports System.Reflection
 
 Public Class PlugIn1
 
@@ -23,15 +24,25 @@ Public Class PlugIn1
     End Sub
 #End Region
 
+    Public Function GetBitmapByName(ByVal BitmapName As String) As Bitmap
+        Dim Asm As Assembly = Assembly.GetAssembly(Me.GetType)
+        Dim stream As IO.Stream = Asm.GetManifestResourceStream(String.Format("DX_CollapseFromEnd.{0}", BitmapName))
+        Return CType(Bitmap.FromStream(stream), Bitmap)
+    End Function
+
     Private Sub PlugIn1_EditorPaintLanguageElement(ByVal ea As DevExpress.CodeRush.Core.EditorPaintLanguageElementEventArgs) Handles Me.EditorPaintLanguageElement
         Dim View = CodeRush.Documents.ActiveTextView
+        If View Is Nothing Then
+            Exit Sub
+        End If
         If ea.LanguageElement.IsCollapsible _
         AndAlso View.IsOnScreen(ea.LanguageElement.CollapsibleRange.End.Line) Then
             ' Add Tile
             Dim EndPoint = ea.LanguageElement.CollapsibleRange.End
-            Dim Rect = View.GetRectangleFromRange(New SourceRange(EndPoint.Line, 1, EndPoint.Line, 2))
-            Rect.X = Rect.X - Rect.Width
-            View.Graphics.FillRectangle(Brushes.Black, Rect)
+            Dim Rect = View.GetRectangleFromRange(New SourceRange(EndPoint, EndPoint.OffsetPoint(0, 1)))
+            '            Rect.X = Rect.X + Rect.Width
+            View.Graphics.DrawImage(GetBitmapByName("Collapse.bmp"), New PointF(Rect.Left + 3, Rect.Top + 4))
+            'View.Graphics.FillRectangle(Brushes.Black, Rect)
             View.AddTile(NewTile(Rect, ea.LanguageElement))
         End If
     End Sub
@@ -39,6 +50,13 @@ Public Class PlugIn1
     Private Sub PlugIn1_TileMouseDown(ByVal sender As Object, ByVal ea As DevExpress.CodeRush.Core.TileMouseEventArgs) Handles Me.TileMouseDown
         If Me.TileIsOurs(ea.Tile) Then
             Call (CType(ea.Tile.Object, LanguageElement)).Collapse()
+        End If
+    End Sub
+
+    Private Sub PlugIn1_TileSetCursor(ByVal sender As Object, ByVal ea As DevExpress.CodeRush.Core.TileSetCursorEventArgs) Handles Me.TileSetCursor
+        If Me.TileIsOurs(ea.Tile) Then
+            Cursor.Current = Cursors.Hand
+            ea.SetCursorArgs.Cancel = True
         End If
     End Sub
 End Class
