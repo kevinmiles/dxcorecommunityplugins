f1ceda9c64b12cd50659fc03832a58fa2500655c
rorybecker <RoryBecker@Gmail.com>
2009-10-07 18:57:38 +0000

    Minor refactoring of DX_CustomCollapse

--- DX_CustomCollapse/DX_CustomCollapse/PlugIn1.vb
+++ DX_CustomCollapse/DX_CustomCollapse/PlugIn1.vb
@@ -5,6 +5,7 @@ Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
 Imports System.Runtime.CompilerServices
+Imports CR = DevExpress.CodeRush.Core.CodeRush
 
 
 Public Class PlugIn1
@@ -27,36 +28,16 @@ Public Class PlugIn1
 
     Private Sub actCustomCollapse_Execute(ByVal ea As DevExpress.CodeRush.Core.ExecuteEventArgs) Handles actCustomCollapse.Execute
         Dim Exclusions As LanguageElementType() = {LanguageElementType.Namespace, LanguageElementType.Class, LanguageElementType.Struct, LanguageElementType.Region}
-        For Each Node In GetElements(CodeRush.Source.ActiveFileNode, Function(e) Not e.ElementType.In(Exclusions) AndAlso e.IsCollapsible)
+        For Each Node In GetElements(CR.Source.ActiveFileNode, Function(e) e.IsCollapsible _
+                                                                     AndAlso Not e.ElementType.In(Exclusions))
             Node.Collapse()
         Next
     End Sub
-    Private Function GetElements(ByVal Scope As LanguageElement, Optional ByVal Proc As System.Func(Of LanguageElement, Boolean) = Nothing) As IEnumerable(Of LanguageElement)
-        Dim Enumerator = New ElementEnumerable(Scope, True).OfType(Of LanguageElement)()
-        If Proc Is Nothing Then
-            Return Enumerator
-        End If
-        Return Enumerator.Where(Proc)
-    End Function
-    Private Function GetElements(ByVal Scope As LanguageElement, ByVal ElementType() As LanguageElementType, Optional ByVal Proc As System.Func(Of LanguageElement, Boolean) = Nothing) As IEnumerable(Of LanguageElement)
-        Dim Enumerator = New ElementEnumerable(Scope, New ElementTypeFilter(ElementType), True).OfType(Of LanguageElement)()
-        If Proc Is Nothing Then
-            Return Enumerator
-        End If
-        Return Enumerator.Where(Proc)
-    End Function
-
-
 End Class
 
 Public Module GeneralExt
     <Extension()> _
     Public Function [In](ByVal Source As LanguageElementType, ByVal List As IEnumerable(Of LanguageElementType)) As Boolean
-        For Each Item In List
-            If Item = Source Then
-                Return True
-            End If
-        Next
-        Return False
+        Return List.Any(Function(e) e = Source)
     End Function
 End Module
