7b1032b07382a081bff565dc53e5cc59cc7d18de
RoryBecker@gmail.com <RoryBecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2009-12-11 23:52:18 +0000

    DX_MoveCode: Adjusted MoveCodeUp and MoveCodeDown to cater to Methods as well as statements.

--- trunk/DX_MoveCode/DX_MoveCode/Extentions.vb
+++ trunk/DX_MoveCode/DX_MoveCode/Extentions.vb
@@ -34,4 +34,18 @@ Public Module Extentions
     Public Function MoveABS(ByVal Source As SourceRange, ByVal Point As SourcePoint) As SourceRange
         Return New SourceRange(Point.Line, 1, Point.Line + Source.Height, 1)
     End Function
+    <Extension()> _
+    Public Function NextCodeSiblingWhichIsNot(ByVal Source As LanguageElement, ByVal ParamArray Types() As LanguageElementType) As LanguageElement
+        NextCodeSiblingWhichIsNot = Source
+        Do
+            NextCodeSiblingWhichIsNot = NextCodeSiblingWhichIsNot.NextCodeSibling
+        Loop Until NextCodeSiblingWhichIsNot Is Nothing OrElse Not Types.Contains(NextCodeSiblingWhichIsNot.ElementType)
+    End Function
+    <Extension()> _
+    Public Function PreviousCodeSiblingWhichIsNot(ByVal Source As LanguageElement, ByVal ParamArray Types() As LanguageElementType) As LanguageElement
+        PreviousCodeSiblingWhichIsNot = Source
+        Do
+            PreviousCodeSiblingWhichIsNot = PreviousCodeSiblingWhichIsNot.PreviousCodeSibling
+        Loop Until PreviousCodeSiblingWhichIsNot Is Nothing OrElse Not Types.Contains(PreviousCodeSiblingWhichIsNot.ElementType)
+    End Function
 End Module
