7a7065ca2bccc6b140749aea5f0ad60c8ae0c0df
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2010-01-22 02:17:49 +0000

    First draft - only one issue reported, so there is long way to go

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/ICodeIssue.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using Microsoft.StyleCop;
+
+    internal interface ICodeIssue
+    {
+        void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation);
+    }
+}
