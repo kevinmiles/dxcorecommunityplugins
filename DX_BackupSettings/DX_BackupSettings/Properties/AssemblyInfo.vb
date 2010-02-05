e5cddf834c7a9e9571e862ced4d820c198e2d0c8
rorybecker <RoryBecker@Gmail.com>
2010-02-05 15:58:47 +0000

    Added BackupSettings plugin... Used to backup DX Settings to a zip file.

---
+++ DX_BackupSettings/DX_BackupSettings/Properties/AssemblyInfo.vb
@@ -0,0 +1,38 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports DevExpress.CodeRush.Common
+
+<Assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "DX_BackupSettings", PlugInLoadType.Demand, LoadAbilityType.LoadDisabled)>
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("DX_BackupSettings")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("DX_BackupSettings")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1ad1f222-2840-4c08-a3f6-67c91e23e94d")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
