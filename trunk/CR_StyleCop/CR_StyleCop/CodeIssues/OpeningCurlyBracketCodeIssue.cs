b96aabdc68e0f5b6da67dfc223907d8225704fca
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2010-02-07 03:08:33 +0000

    Added few layout issues

---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/OpeningCurlyBracketCodeIssue.cs
@@ -0,0 +1,29 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using Microsoft.StyleCop;
+
+    internal class OpeningCurlyBracketCodeIssue : ICodeIssue
+    {
+        public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
+        {
+            string message = String.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
+            if (violation.Element == null)
+            {
+                ea.AddSmell(new SourceRange(violation.Line, 1, violation.Line, document.LengthOfLine(violation.Line) + 1), message, 10);
+                return;
+            }
+            foreach (var token in violation.Element.ElementTokens)
+            {
+                if (violation.Line == token.Location.StartPoint.LineNumber && token.Text == "{")
+                {
+                    SourceRange sourceRange = new SourceRange(token.Location.StartPoint.LineNumber, 1, token.Location.EndPoint.LineNumber, token.Location.EndPoint.IndexOnLine + 2);
+                    ea.AddSmell(sourceRange, message, 10);
+                    return;
+                }
+            }
+        }
+    }
+}
---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1506CodeIssue.cs
@@ -0,0 +1,32 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using Microsoft.StyleCop;
+
+    internal class SA1506CodeIssue : ICodeIssue
+    {
+        public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
+        {
+            string message = String.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
+            if (violation.Element == null)
+            {
+                ea.AddSmell(new SourceRange(violation.Line, 1, violation.Line, document.LengthOfLine(violation.Line) + 1), message, 10);
+                return;
+            }
+            for (int lineNumber = violation.Line; lineNumber < document.LineCount; lineNumber++)
+            {
+                string nextLine = document.GetText(lineNumber + 1).Trim();
+                if (string.IsNullOrEmpty(nextLine))
+                {
+                    string lineText = document.GetText(lineNumber);
+                    string textToUnderline = lineText.TrimStart();
+                    SourceRange sourceRange = new SourceRange(lineNumber, lineText.Length - textToUnderline.Length, lineNumber, document.LengthOfLine(lineNumber) + 1);
+                    ea.AddSmell(sourceRange, message, 10);
+                    return;
+                }
+            }
+        }
+    }
+}
