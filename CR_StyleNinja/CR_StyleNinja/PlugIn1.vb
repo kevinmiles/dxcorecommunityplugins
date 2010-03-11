942447a146765870bade5b0367550505eb8920b0
rorybecker <RoryBecker@Gmail.com>
2010-03-11 18:07:12 +0000

    Adjustments to CR_StyleNinja and DX_GuidGen

--- CR_StyleNinja/CR_StyleNinja/PlugIn1.vb
+++ CR_StyleNinja/CR_StyleNinja/PlugIn1.vb
@@ -4,6 +4,8 @@ Imports System.Windows.Forms
 Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
+Imports System.Runtime.CompilerServices
+
 Public Class PlugIn1
 
     'DXCore-generated code...
@@ -31,6 +33,8 @@ Public Class PlugIn1
         AddHandler NewIssue.CheckCodeIssues, AddressOf New CheckIssuesDelegateCaller(CheckIssues).Invoke
         CType(NewIssue, System.ComponentModel.ISupportInitialize).EndInit()
     End Sub
+#End Region
+#Region "Enumerators"
     Private Function Locals(ByVal Scope As LanguageElement) As IEnumerable(Of Variable)
         Return Variables(Scope).Where(Function(v) v.IsLocal)
     End Function
@@ -49,10 +53,9 @@ Public Class PlugIn1
     Private Function Interfaces(ByVal Scope As LanguageElement) As IEnumerable(Of [Interface])
         Return New ElementEnumerable(Scope, GetType([Interface]), True).OfType(Of [Interface])()
     End Function
-    Public Function NamespacesClassesEnumsSructsDelegatesEventsMethodsProperties(ByVal Scope As LanguageElement) As IEnumerable(Of LanguageElement)
+    Public Function MainElements(ByVal Scope As LanguageElement) As IEnumerable(Of LanguageElement)
         Return New ElementEnumerable(Scope, NamespacesClassesEnumsSructsDelegatesEventsMethodsPropertiesTypes, True).OfType(Of LanguageElement)()
     End Function
-
 #End Region
     Public Sub RegisterIssues()
         ' SA1302
@@ -68,14 +71,12 @@ Public Class PlugIn1
         If CodeRush.CodeStyle.PrefixLocal = String.Empty Then
             Exit Sub
         End If
-        Dim Scope = TryCast(ea.Scope, LanguageElement)
-        If Scope Is Nothing Then
-            Exit Sub
+        If ea.Scope.ToLE IsNot Nothing Then
+            Dim Finder = Locals(ea.Scope.ToLE).WhereNotNameStarts(CodeRush.CodeStyle.PrefixLocal)
+            For Each FoundItem In Finder
+                ea.AddHint(FoundItem.NameRange, String.Format("Local {0} should start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixLocal))
+            Next
         End If
-        Dim Finder = Locals(Scope).Where(Function(l) Not l.Name.StartsWith(CodeRush.CodeStyle.PrefixLocal))
-        For Each FoundItem In Finder
-            ea.AddHint(FoundItem.NameRange, String.Format("Local {0} should start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixLocal))
-        Next
     End Sub
 #End Region
 #Region "FieldsStartWith..."
@@ -83,14 +84,12 @@ Public Class PlugIn1
         If CodeRush.CodeStyle.PrefixField = String.Empty Then
             Exit Sub
         End If
-        Dim Scope = TryCast(ea.Scope, LanguageElement)
-        If Scope Is Nothing Then
-            Exit Sub
+        If ea.Scope.ToLE IsNot Nothing Then
+            Dim Finder = Fields(ea.Scope.ToLE).WhereNotNameStarts(CodeRush.CodeStyle.PrefixField)
+            For Each FoundItem As Variable In Finder
+                ea.AddHint(FoundItem.NameRange, String.Format("Field {0} should start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixField))
+            Next
         End If
-        Dim Finder = Fields(Scope).Where(Function(f) Not f.Name.StartsWith(CodeRush.CodeStyle.PrefixField))
-        For Each FoundItem As Variable In Finder
-            ea.AddHint(FoundItem.NameRange, String.Format("Field {0} should start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixField))
-        Next
     End Sub
 #End Region
 #Region "ParametersStartWithParamPrefix"
@@ -98,40 +97,32 @@ Public Class PlugIn1
         If CodeRush.CodeStyle.PrefixParam = String.Empty Then
             Exit Sub
         End If
-        Dim Scope = TryCast(ea.Scope, LanguageElement)
-        If Scope Is Nothing Then
-            Exit Sub
+        If ea.Scope.ToLE IsNot Nothing Then
+            Dim Finder = Params(ea.Scope.ToLE).WhereNotNameStarts(CodeRush.CodeStyle.PrefixParam)
+            For Each FoundItem As Param In Finder
+                ea.AddHint(FoundItem.NameRange, String.Format("Parameter '{0}' does not start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixParam))
+            Next
         End If
-        Dim Finder = Params(Scope).Where(Function(p) Not p.Name.StartsWith(CodeRush.CodeStyle.PrefixParam))
-        For Each FoundItem As Param In Finder
-            ea.AddHint(FoundItem.NameRange, String.Format("Parameter '{0}' does not start with '{1}'", FoundItem.Name, CodeRush.CodeStyle.PrefixParam))
-        Next
     End Sub
 #End Region
-
 #Region "InterfacesStartWith..."
     Public Sub InterfacesStartWithI_CheckCodeIssues(ByVal sender As Object, ByVal ea As CheckCodeIssuesEventArgs)
-        Dim Scope = TryCast(ea.Scope, LanguageElement)
-        If Scope Is Nothing Then
-            Exit Sub
+        If ea.Scope.ToLE IsNot Nothing Then
+            Dim Finder = Interfaces(ea.Scope.ToLE).WhereNotNameStarts("I")
+            For Each FoundItem As [Interface] In Finder
+                ea.AddHint(FoundItem.NameRange, String.Format("Interface {0} should start with 'I'", FoundItem))
+            Next
         End If
-        Dim Finder = Interfaces(Scope).Where(Function(I) Not I.Name.StartsWith("I"))
-        For Each FoundItem As [Interface] In Finder
-            ea.AddHint(FoundItem.NameRange, String.Format("Interface {0} should start with 'I'", FoundItem))
-        Next
     End Sub
 #End Region
 #Region "ElementsStartWithUpperCase"
     Public Sub ElementsStartWithUpperCase_CheckCodeIssues(ByVal sender As Object, ByVal ea As CheckCodeIssuesEventArgs)
-        Dim Scope = TryCast(ea.Scope, LanguageElement)
-        If Scope Is Nothing Then
-            Exit Sub
+        If ea.Scope.ToLE IsNot Nothing Then
+            Dim Finder = MainElements(ea.Scope.ToLE).WhereNameStartsLower()
+            For Each FoundItem As Method In Finder
+                ea.AddHint(FoundItem.NameRange, String.Format("Element '{0}' does not start with an upper case char.", FoundItem.Name))
+            Next
         End If
-        Dim Finder = NamespacesClassesEnumsSructsDelegatesEventsMethodsProperties(Scope).Where(Function(e) Char.IsLower(e.Name.First))
-        For Each FoundItem As Method In Finder
-            ea.AddHint(FoundItem.NameRange, String.Format("Element '{0} does not start with an upper case char.", FoundItem.Name))
-
-        Next
     End Sub
 #End Region
 
