df0b3380ea6e3a846169866b83ce003a2b630357
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2010-03-12 11:21:20 +0000

    CR_StyleNinja: SA1307 bug fixed. Previously required Lowercase rather than Uppercase.

--- trunk/CR_StyleNinja/CR_StyleNinja/CheckIssuesDelegateCaller.vb
+++ trunk/CR_StyleNinja/CR_StyleNinja/CheckIssuesDelegateCaller.vb
@@ -4,9 +4,12 @@ Imports System.Windows.Forms
 Imports DevExpress.CodeRush.Core
 Imports DevExpress.CodeRush.PlugInCore
 Imports DevExpress.CodeRush.StructuralParser
+Imports DevExpress.Refactor.Core
 
 
 Public Delegate Sub CheckCodeIssuesDelegate(ByVal sender As Object, ByVal ea As CheckCodeIssuesEventArgs)
+Public Delegate Sub ApplyDelegate(ByVal sender As Object, ByVal ea As ApplyContentEventArgs)
+Public Delegate Sub CheckAvailabilityDelegate(ByVal sender As Object, ByVal ea As CheckAvailabilityEventArgs)
 Public Class CheckIssuesDelegateCaller
     Private mCheckIssues As CheckCodeIssuesDelegate
     Public Sub New(ByVal CheckIssues As CheckCodeIssuesDelegate)
@@ -15,5 +18,23 @@ Public Class CheckIssuesDelegateCaller
     End Sub
     Public Sub Invoke(ByVal sender As Object, ByVal ea As CheckCodeIssuesEventArgs)
         mCheckIssues.Invoke(sender, ea)
+    End Sub
+End Class
+Public Class CheckAvailabilityDelegateCaller
+    Private mCheckAvailability As CheckAvailabilityDelegate
+    Public Sub New(ByVal CheckAvailability As CheckAvailabilityDelegate)
+        mCheckAvailability = CheckAvailability
+    End Sub
+    Public Sub Invoke(ByVal sender As Object, ByVal ea As CheckAvailabilityEventArgs)
+        mCheckAvailability.Invoke(sender, ea)
+    End Sub
+End Class
+Public Class ApplyDelegateCaller
+    Private mApply As ApplyDelegate
+    Public Sub New(ByVal Apply As ApplyDelegate)
+        mApply = Apply
+    End Sub
+    Public Sub Invoke(ByVal sender As Object, ByVal ea As ApplyContentEventArgs)
+        mApply.Invoke(sender, ea)
     End Sub
 End Class
