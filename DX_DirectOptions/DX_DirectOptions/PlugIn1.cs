11547527bbb9dbe9f5af0ff8e7cf16d32361d175
rorybecker <RoryBecker@Gmail.com>
2010-03-24 19:03:33 +0000

    DX_DirectOptions: Update to position menu items correctly.

--- DX_DirectOptions/DX_DirectOptions/PlugIn1.cs
+++ DX_DirectOptions/DX_DirectOptions/PlugIn1.cs
@@ -6,6 +6,9 @@ using System.Windows.Forms;
 using DevExpress.CodeRush.Core;
 using DevExpress.CodeRush.PlugInCore;
 using DevExpress.CodeRush.StructuralParser;
+using System.Threading;
+using System.Collections.Generic;
+using DevExpress.CodeRush.Menus;
 
 namespace DX_DirectOptions
 {
@@ -19,21 +22,64 @@ namespace DX_DirectOptions
             base.InitializePlugIn();
             createShowShortcutOptions();
             createShowTemplateOptions();
-            //
-            // TODO: Add your initialization code here.
-            //
+            EventNexus.DXCoreLoaded += new DXCoreLoadedEventHandler(EventNexus_DXCoreLoaded);
+
         }
         #endregion
         #region FinalizePlugIn
         public override void FinalizePlugIn()
         {
-            //
-            // TODO: Add your finalization code here.
-            //
-
+            EventNexus.DXCoreLoaded -= new DXCoreLoadedEventHandler(EventNexus_DXCoreLoaded);
             base.FinalizePlugIn();
         }
         #endregion
+
+        #region Magic by 'The Great Wizard Skorkin'
+        void EventNexus_DXCoreLoaded(DXCoreLoadedEventArgs ea)
+        {
+            RearrangeMenuItemsAsync();
+        }
+
+        private void RearrangeMenuItemsAsync()
+        {
+            SendOrPostCallback callback = delegate(object state) { this.AsyncCallBack(); };
+            SynchronizationContext sync = SynchronizationContext.Current;
+            if (sync != null)
+                sync.Post(callback, null);
+            else
+                callback(null);
+        }
+
+        void AsyncCallBack()
+        {
+            IEnumerator<IMenuControl> e = CodeRush.Menus.DXCore.GetEnumerator();
+            while (e.MoveNext())
+            {
+                IMenuControl current = e.Current;
+                if (current.Caption == "&Options...")
+                {
+                    SetNextControlBeginGroup(current, false);
+                }
+                else if (current.Caption == "&Templates...")
+                {
+                    SetNextControlBeginGroup(current, false);
+                }
+                else if (current.Caption == "&Shortcuts...")
+                {
+                    SetNextControlBeginGroup(current, true);
+                    break;
+                }
+            }
+        }
+
+        void SetNextControlBeginGroup(IMenuControl current, bool value)
+        {
+            IMenuControl nextControl = current.Parent[current.Index + 1];
+            if (nextControl != null)
+                nextControl.BeginGroup = value;
+        }
+        #endregion
+
         public void createShowTemplateOptions()
         {
             if (!CodeRush.Options.IsRegistered("Editor\\Templates"))
@@ -43,7 +89,7 @@ namespace DX_DirectOptions
             ShowTemplateOptions.ActionName = "ShowTemplateOptions";
             ShowTemplateOptions.ButtonText = "&Templates..."; // Used if button is placed on a menu.
             ShowTemplateOptions.CommonMenu = DevExpress.CodeRush.Menus.VsCommonBar.DevExpress;
-            ShowTemplateOptions.Position = LastPosition;
+            ShowTemplateOptions.Position = 3;
             ShowTemplateOptions.RegisterInCR = true;
             ShowTemplateOptions.Execute += ShowTemplateOptions_Execute;
             ((System.ComponentModel.ISupportInitialize)(ShowTemplateOptions)).EndInit();
@@ -59,7 +105,7 @@ namespace DX_DirectOptions
             ShowShortcutOptions.ActionName = "ShowShortcutOptions";
             ShowShortcutOptions.ButtonText = "&Shortcuts..."; // Used if button is placed on a menu.
             ShowShortcutOptions.CommonMenu = DevExpress.CodeRush.Menus.VsCommonBar.DevExpress;
-            ShowShortcutOptions.Position = LastPosition;
+            ShowShortcutOptions.Position = 4;
             ShowShortcutOptions.RegisterInCR = true;
             ShowShortcutOptions.Execute += ShowShortcutOptions_Execute;
             ((System.ComponentModel.ISupportInitialize)(ShowShortcutOptions)).EndInit();
@@ -68,5 +114,6 @@ namespace DX_DirectOptions
         {
             CodeRush.Options.Show("IDE", "Shortcuts");
         }
+
     }
 }
