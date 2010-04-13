d099b3add71b06ff1e2477a5b585398c76305756
travis.illig <travis.illig@241145c3-2f4c-0410-ae16-21396c40807f>
2010-04-13 20:10:13 +0000

    Committed first version of DX_FormatOnSave code.

---
+++ DX_FormatOnSave/trunk/DX_FormatOnSave/Properties/AssemblyInfo.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using DevExpress.CodeRush.Common;
+
+[assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "DX_FormatOnSave", PlugInLoadType.StartUp, LoadAbilityType.LoadEnabled)]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DX_FormatOnSave")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Paraesthesia")]
+[assembly: AssemblyProduct("DXCore for Visual Studio .NET")]
+[assembly: AssemblyCopyright("Copyright © 2010 Travis Illig")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("97150a1d-b4ad-4403-98f2-2e6a4aa6f721")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
