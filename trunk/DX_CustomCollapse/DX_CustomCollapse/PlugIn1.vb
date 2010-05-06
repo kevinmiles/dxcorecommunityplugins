4bbddc2e08c3fc61f00f471f91284b1aa92c73db
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2010-05-06 10:57:35 +0000

    DX_CustomCollapse: Changed plugin to take params. Can now pass comma seperated list of LanguageElementTypes to collapse

--- trunk/DX_CustomCollapse/DX_CustomCollapse/PlugIn1.vb
+++ trunk/DX_CustomCollapse/DX_CustomCollapse/PlugIn1.vb
@@ -27,17 +27,28 @@ Public Class PlugIn1
 #End Region
 
     Private Sub actCustomCollapse_Execute(ByVal ea As DevExpress.CodeRush.Core.ExecuteEventArgs) Handles actCustomCollapse.Execute
+        Dim InclusionStrings = CStr(ea.VarIn).Split(","c).Select(Function(s) Normalise(s)).ToList
         Dim Exclusions As LanguageElementType() = {LanguageElementType.Namespace, LanguageElementType.Class, LanguageElementType.Struct, LanguageElementType.Region}
-        For Each Node In GetElements(CR.Source.ActiveFileNode, Function(e) e.IsCollapsible _
-                                                                     AndAlso Not e.ElementType.In(Exclusions))
+
+        For Each Node In GetElements(CR.Source.ActiveFileNode, _
+                                     Function(e) e.IsCollapsible _
+                                     AndAlso InclusionStrings.Contains(Normalise(e.ElementType.ToString)))
+
             Node.Collapse()
         Next
     End Sub
+    Private Function Normalise(ByVal S As String) As String
+        Return S.LastPart("."c).Trim.ToLower
+    End Function
 End Class
 
 Public Module GeneralExt
     <Extension()> _
     Public Function [In](ByVal Source As LanguageElementType, ByVal List As IEnumerable(Of LanguageElementType)) As Boolean
         Return List.Any(Function(e) e = Source)
+    End Function
+    <Extension()> _
+    Public Function LastPart(ByVal Source As String, ByVal PartSpecifier As String) As String
+        Return Source.Substring(Source.LastIndexOf(PartSpecifier) + 1)
     End Function
 End Module
