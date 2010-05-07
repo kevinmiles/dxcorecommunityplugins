b914a6bff504db63f694ddaa05a6b8638c40f8de
RoryBecker@gmail.com <RoryBecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2010-05-07 16:18:59 +0000

    CR_StyleNinja  - Refactored for Issues centred around lines rather than elements.  - Added additional Issues (1500 series)

--- trunk/CR_StyleNinja/CR_StyleNinja/Utility/Components.vb
+++ trunk/CR_StyleNinja/CR_StyleNinja/Utility/Components.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.ComponentModel
-Imports CR_StyleNinja.Checker
+Imports CR_StyleNinja.ElementChecker
+Imports CR_StyleNinja.LineChecker
 Imports DevExpress.CodeRush.Core
 Imports System.Runtime.CompilerServices
 
@@ -8,16 +9,22 @@ Friend Module Components
     <Extension()> _
     Public Function CreateIssue(ByVal Components As IContainer, _
                                 ByVal IssueMessage As String, _
-                                ByVal Qualifies As QualifiesDelegate, _
-                                Optional ByVal SourceType As SourceTypeEnum = SourceTypeEnum.Unknown) As IssueProvider
-        Return CreateIssue(Components, IssueMessage, AddressOf New Checker(SourceType, Qualifies, IssueMessage).CheckCodeIssues)
+                                ByVal Qualifies As LineQualifiesDelegate) As IssueProvider
+        Return CreateIssue(Components, IssueMessage, AddressOf New LineChecker(Qualifies, IssueMessage).CheckCodeIssues)
     End Function
     <Extension()> _
     Public Function CreateIssue(ByVal Components As IContainer, _
                                 ByVal IssueMessage As String, _
-                                ByVal Qualifies As QualifiesDelegate, _
+                                ByVal Qualifies As ElementQualifiesDelegate, _
+                                Optional ByVal SourceType As SourceTypeEnum = SourceTypeEnum.Unknown) As IssueProvider
+        Return CreateIssue(Components, IssueMessage, AddressOf New ElementChecker(SourceType, Qualifies, IssueMessage).CheckCodeIssues)
+    End Function
+    <Extension()> _
+    Public Function CreateIssue(ByVal Components As IContainer, _
+                                ByVal IssueMessage As String, _
+                                ByVal Qualifies As ElementQualifiesDelegate, _
                                 ByVal Type As Type) As IssueProvider
-        Return CreateIssue(Components, IssueMessage, AddressOf New Checker(Type, Qualifies, IssueMessage).CheckCodeIssues)
+        Return CreateIssue(Components, IssueMessage, AddressOf New ElementChecker(Type, Qualifies, IssueMessage).CheckCodeIssues)
     End Function
     <Extension()> _
     Public Function CreateIssue(ByVal Components As IContainer,
@@ -31,7 +38,7 @@ Friend Module Components
         Return NewIssue
     End Function
     <Extension()> _
-    Public Function CreateRefactoring(ByVal Components As IContainer, ByVal IssueName As String, ByVal DisplayName As String, ByVal Apply As ApplyDelegate, ByVal CheckContentAvailability As CheckContentAvailabilityDelegate) As DevExpress.Refactor.Core.RefactoringProvider
+    Public Function CreateRefactoring(ByVal Components As IContainer, ByVal IssueName As String, ByVal DisplayName As String, ByVal CheckContentAvailability As CheckContentAvailabilityDelegate, ByVal Apply As ApplyDelegate) As DevExpress.Refactor.Core.RefactoringProvider
         Dim NewRefactoring As New DevExpress.Refactor.Core.RefactoringProvider(Components)
         CType(NewRefactoring, System.ComponentModel.ISupportInitialize).BeginInit()
         NewRefactoring.ProviderName = IssueName ' Should be Unique
@@ -43,7 +50,7 @@ Friend Module Components
         Return NewRefactoring
     End Function
     <Extension()> _
-    Public Function CreateCodeProvider(ByVal Components As IContainer, ByVal IssueName As String, ByVal DisplayName As String, ByVal Apply As ApplyDelegate, ByVal CheckContentAvailability As CheckContentAvailabilityDelegate) As CodeProvider
+    Public Function CreateCodeProvider(ByVal Components As IContainer, ByVal IssueName As String, ByVal DisplayName As String, ByVal CheckContentAvailability As CheckContentAvailabilityDelegate, ByVal Apply As ApplyDelegate) As CodeProvider
         Dim NewCodeProvider As New CodeProvider(Components)
         CType(NewCodeProvider, System.ComponentModel.ISupportInitialize).BeginInit()
         NewCodeProvider.ProviderName = IssueName ' Should be Unique
