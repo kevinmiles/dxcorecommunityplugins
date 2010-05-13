37214bba90689b8c128ad1bca5417fa2bd7554a9
RoryBecker@gmail.com <RoryBecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2010-05-13 20:08:39 +0000

    DX_StackNav: v1 commit

---
+++ trunk/DX_StackNav/DX_StackNav/StackNavEntry.vb
@@ -0,0 +1,53 @@
+﻿Public Class StackNavEntry
+#Region "Fields"
+    Private mPath As String
+    Private mFilename As String
+    Private mLine As Integer
+    Private mDescription As String
+#End Region
+#Region "Constructors"
+    Public Sub New(ByVal PathAndFile As String, ByVal Line As Integer, ByVal Description As String)
+        MyClass.New(PathAndFile.Substring(0, PathAndFile.LastIndexOf("\"c)), _
+                    PathAndFile.Substring(PathAndFile.LastIndexOf("\"c) + 1), Line, Description)
+    End Sub
+    Public Sub New(ByVal Path As String, ByVal Filename As String, ByVal Line As Integer, ByVal Description As String)
+        Me.mPath = Path
+        Me.mFilename = Filename
+        Me.mLine = Line
+        Me.mDescription = Description
+    End Sub
+#End Region
+#Region "Simple properties"
+    Private ReadOnly Property Filename() As String
+        Get
+            Return mFilename
+        End Get
+    End Property
+    Private ReadOnly Property Path() As String
+        Get
+            Return mPath
+        End Get
+    End Property
+    Public ReadOnly Property Line() As Integer
+        Get
+            Return mLine
+        End Get
+    End Property
+    Private ReadOnly Property Description() As String
+        Get
+            Return mDescription
+        End Get
+    End Property
+#End Region
+    Private ReadOnly Property Location As String
+        Get
+            Return String.Format("{0} - Line {1}", mFilename, mLine)
+        End Get
+    End Property
+    Public ReadOnly Property PathAndFile As String
+        Get
+            Return String.Format("{0}\{1}", mPath, mFilename)
+        End Get
+    End Property
+
+End Class
---
+++ trunk/DX_StackNav/DX_StackNav/ToolWindow1.vb
@@ -0,0 +1,64 @@
+Imports System.ComponentModel
+Imports System.Drawing
+Imports System.Runtime.InteropServices
+Imports System.Windows.Forms
+Imports DevExpress.CodeRush.Core
+Imports DevExpress.CodeRush.PlugInCore
+Imports DevExpress.CodeRush.StructuralParser
+Imports System.Text.RegularExpressions
+
+<Title("Stack Nav")> _
+Public Class ToolWindow1
+#Region " InitializePlugIn "
+    Public Overrides Sub InitializePlugIn()
+        MyBase.InitializePlugIn()
+
+        'TODO: Add your initialization code here.
+    End Sub
+#End Region
+#Region " FinalizePlugIn "
+    Public Overrides Sub FinalizePlugIn()
+        'TODO: Add your finalization code here.
+
+        MyBase.FinalizePlugIn()
+    End Sub
+#End Region
+
+#Region "Guts"
+    Private Regex = New Regex("\s+at\s(?<Method>.+)\(\)\sin\s(?<File>.+)\:line\s(?<LineNumber>\d+)")
+    Public Function GetStackFrames(ByVal StackText As String) As List(Of StackNavEntry)
+        Dim Frames As New List(Of StackNavEntry)
+        For Each Match As Match In Regex.Matches(StackText)
+            Frames.Add(New StackNavEntry(Match.Groups("File").Value, Match.Groups("LineNumber").Value, StackText))
+        Next
+        Return Frames
+    End Function
+    Private Sub JumpToFile(ByVal Filename As String, ByVal Line As Integer)
+        Dim Document = CodeRush.File.Activate(Filename)
+        If Document IsNot Nothing Then
+            CodeRush.TextViews.Active.Caret.MoveTo(Line, 1)
+        End If
+    End Sub
+    Private Sub Refresh()
+        Dim Frames = GetStackFrames(TextBox1.Text)
+        Grid.DataSource = Frames
+        Grid.Refresh()
+    End Sub
+#End Region
+#Region "UI Events "
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        Call Refresh()
+    End Sub
+
+    Private Sub cmdRefresh_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdRefresh.Click
+        Refresh()
+    End Sub
+    Private Sub Grid_CellContentDoubleClick(ByVal sender As Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles Grid.CellContentDoubleClick
+        Dim Frame = TryCast(Grid.Rows(e.RowIndex).DataBoundItem, StackNavEntry)
+        Call JumpToFile(Frame.PathAndFile, Frame.Line)
+    End Sub
+#End Region
+
+
+
+End Class
