66f46314989db83938b5c643d7e9a38a29cc842f
rorybecker <RoryBecker@Gmail.com>
2010-05-13 20:08:39 +0000

    DX_StackNav: v1 commit

---
+++ DX_StackNav/DX_StackNav/ToolWindow1.Designer.vb
@@ -0,0 +1,135 @@
+<System.Runtime.InteropServices.Guid("0db83446-00eb-45ff-b3f2-35c920c89b70"), _
+Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ToolWindow1
+	Inherits DevExpress.CodeRush.PlugInCore.ToolWindowPlugIn
+
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Public Sub New()
+		MyBase.New()
+
+		'This call is required by the Component Designer.
+		InitializeComponent()
+
+	End Sub
+
+	'ToolWindowPlugIn overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		If disposing AndAlso components IsNot Nothing Then
+			components.Dispose()
+		End If
+		MyBase.Dispose(disposing)
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ToolWindow1))
+        Me.MyEvents = New DevExpress.DXCore.PlugInCore.DXCoreEvents(Me.components)
+        Me.Grid = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.cmdRefresh = New System.Windows.Forms.Button()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        CType(Me.MyEvents, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Grid, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Grid
+        '
+        Me.Grid.AllowUserToAddRows = False
+        Me.Grid.AllowUserToDeleteRows = False
+        Me.Grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.Grid.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Grid.Location = New System.Drawing.Point(0, 0)
+        Me.Grid.Name = "Grid"
+        Me.Grid.ReadOnly = True
+        Me.Grid.Size = New System.Drawing.Size(583, 243)
+        Me.Grid.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(3, 35)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(287, 205)
+        Me.TextBox1.TabIndex = 1
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        Me.TextBox1.WordWrap = False
+        '
+        'cmdRefresh
+        '
+        Me.cmdRefresh.Location = New System.Drawing.Point(3, 6)
+        Me.cmdRefresh.Name = "cmdRefresh"
+        Me.cmdRefresh.Size = New System.Drawing.Size(75, 23)
+        Me.cmdRefresh.TabIndex = 2
+        Me.cmdRefresh.Text = "Refresh"
+        Me.cmdRefresh.UseVisualStyleBackColor = True
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.Controls.Add(Me.TextBox1)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.cmdRefresh)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Grid)
+        Me.SplitContainer1.Size = New System.Drawing.Size(880, 243)
+        Me.SplitContainer1.SplitterDistance = 293
+        Me.SplitContainer1.TabIndex = 3
+        '
+        'ToolWindow1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.Name = "ToolWindow1"
+        Me.Size = New System.Drawing.Size(880, 243)
+        CType(Me.MyEvents, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Grid, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents MyEvents As DevExpress.DXCore.PlugInCore.DXCoreEvents
+
+#Region " ShowWindow "
+    Public Shared Function ShowWindow() As EnvDTE.Window
+        Return DevExpress.CodeRush.Core.CodeRush.ToolWindows.Show(GetType(ToolWindow1).GUID)
+    End Function
+#End Region
+#Region " HideWindow "
+    Public Shared Function HideWindow() As EnvDTE.Window
+        Return DevExpress.CodeRush.Core.CodeRush.ToolWindows.Hide(GetType(ToolWindow1).GUID)
+    End Function
+    Friend WithEvents Grid As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents cmdRefresh As System.Windows.Forms.Button
+    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+#End Region
+
+End Class
