dd4510d1001ce73e2ad748705c0e4e9cb20fcb18
krzysztof.blacha <krzysztof.blacha@gmail.com>
2010-08-26 14:39:26 +0000

    I've added support for delayed properties.

--- Refactor_ConvertToXPOProperty/Refactor_ConvertToXPOProperty/ConvertToXPOPropertyPlugin.cs
+++ Refactor_ConvertToXPOProperty/Refactor_ConvertToXPOProperty/ConvertToXPOPropertyPlugin.cs
@@ -11,6 +11,7 @@ using DevExpress.CodeRush.PlugInCore;
 using DevExpress.CodeRush.StructuralParser;
 using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
@@ -35,7 +36,7 @@ namespace Refactor_ConvertToXPOProperty
             //
             // TODO: Add your initialization code here.
             //
-        } // InitializePlugIn
+        } // InitializePlugIn()
         #endregion
         #region FinalizePlugIn
         /// <summary>
@@ -48,13 +49,95 @@ namespace Refactor_ConvertToXPOProperty
             //
 
             base.FinalizePlugIn();
-        } // FinalizePlugIn
+        } // FinalizePlugIn()
         #endregion
+
+        private const string MenuItem_ConvertTo_XPOProperty = "ConvertToXPOProperty";
+        private const string MenuItem_ConvertTo_DelayedProperty = "ConvertToDelayedProperty";
+        private const string PersistentAttribute = "DevExpress.Xpo.PersistentAttribute";
+        private const string PersistentAliasAttribute = "DevExpress.Xpo.PersistentAliasAttribute";
+        private const string NonPersistentAttribute = "DevExpress.Xpo.NonPersistentAttribute";
 
         /// <summary>
         /// _Property
         /// </summary>
         private Property _Property;
+
+        /// <summary>
+        /// _New source tree resolver
+        /// </summary>
+        private SourceTreeResolver _NewSourceTreeResolver;
+
+        /// <summary>
+        /// _Is xpo property
+        /// </summary>
+        private bool _IsXpoProperty;
+
+        /// <summary>
+        /// _Is auto implemented
+        /// </summary>
+        private bool _IsAutoImplemented;
+
+        /// <summary>
+        /// Is element of type
+        /// </summary>
+        public bool IsElementOfType(ITypeElement DeclaredTypeElement, string type)
+        {
+            if (DeclaredTypeElement == null)
+                return false;
+            if (DeclaredTypeElement.FullName.Replace("`1", "") == type || DeclaredTypeElement.DescendsFrom(type))
+                return true;
+            return false;
+        } // IsElementOfType(DeclaredTypeElement, type)
+
+        /// <summary>
+        /// Is element of type
+        /// </summary>
+        public bool IsElementOfType(ITypeReferenceExpression declaredTypeExpression, string type)
+        {
+            try
+            {
+                if (declaredTypeExpression == null)
+                    return false;
+                if (_NewSourceTreeResolver == null)
+                    _NewSourceTreeResolver = new SourceTreeResolver();
+                ITypeElement DeclaredTypeElement = _NewSourceTreeResolver.ResolveExpression(declaredTypeExpression) as ITypeElement;
+                return IsElementOfType(DeclaredTypeElement, type);
+            } // try
+            catch (Exception ex)
+            {
+                Trace.WriteLine("Exception in IsElementOfType Function: " + ex.Message);
+            }
+            return false;
+        } // IsElementOfType(declaredTypeExpression, type)
+
+        /// <summary>
+        /// Is xpo property
+        /// </summary>
+        private bool IsXpoProperty(Property property)
+        {
+            // Don;t know if this is a valid assumption but I do not have
+            // better right now.
+            // Some ideas borrowed from EasyFields plugin - Thank you
+            IHasAttributes hasAttrs = property as IHasAttributes;
+            if (hasAttrs != null)
+            {
+                foreach (IAttributeElement attr in hasAttrs.Attributes)
+                {
+            	    ITypeElement attrTypeElement = attr.GetDeclaration() as ITypeElement;
+                    if (attrTypeElement != null)
+                    {
+                        if (IsElementOfType(attrTypeElement, PersistentAttribute) || IsElementOfType(attrTypeElement, PersistentAliasAttribute))
+                            return true;
+                        if (IsElementOfType(attrTypeElement, NonPersistentAttribute))
+                            return false;
+                    }
+                } // foreach
+            } // if
+            if (property.Visibility == MemberVisibility.Public)
+                return true;
+            return false;
+        } // IsXpoProperty(property)
 
         /// <summary>
         /// Refactoring provider check availability
@@ -68,23 +151,83 @@ namespace Refactor_ConvertToXPOProperty
             if (element.InsideClass && element is Property)
             {
                 _Property = (Property)element;
-                if (_Property.IsAutoImplemented)
+                _IsXpoProperty = IsXpoProperty(_Property);
+                _IsAutoImplemented = _Property.IsAutoImplemented;
+                if (_IsAutoImplemented || _IsXpoProperty)
+                {
                     ea.Available = true;
-            }
-        } // refactoringProvider_CheckAvailability
+                    if (_IsAutoImplemented)
+                        ea.AddSubMenuItem(MenuItem_ConvertTo_XPOProperty, "XPO property");
+                    ea.AddSubMenuItem(MenuItem_ConvertTo_DelayedProperty, "Delayed property");
+                }
+            } // if
+        } // refactoringProvider_CheckAvailability(sender, ea)
 
         /// <summary>
         /// Refactoring provider apply
         /// </summary>
         private void refactoringProvider_Apply(object sender, ApplyContentEventArgs ea)
         {
-            if (_Property != null)
+            if (_Property != null && ea.SelectedSubMenuItem != null)
             {
                 ElementBuilder elementBuilder = ea.NewElementBuilder();
-                string newCode = GetNewPropertyDeclaration(elementBuilder, _Property);
-                ea.TextDocument.Replace(_Property.Range, newCode, "Converting into Xpo property", true);
+                string newCode;
+                switch (ea.SelectedSubMenuItem.Name)
+                {
+                    case MenuItem_ConvertTo_XPOProperty:
+                        newCode = GetNewPropertyDeclaration(elementBuilder, _Property);
+                        ea.TextDocument.Replace(_Property.Range, newCode, "Converting into XPO property", true);
+                        break;
+                    case MenuItem_ConvertTo_DelayedProperty:
+                        newCode = GetNewDelayedPropertyDeclaration(elementBuilder, _Property);
+                        ea.TextDocument.Replace(_Property.Range, newCode, "Converting into delayed property", true);
+                        // How detect and remove property variable?
+                        //if (_IsXpoProperty && _Property.HasGetter)
+                        //    ea.TextDocument.Replace(_Property.Getter.LastChild.FirstDetail.Range, string.Empty, "Converting into delayed property - field var removed", true);
+                        break;
+                } // switch
+            } // if
+        } // refactoringProvider_Apply(sender, ea)
+
+        /// <summary>
+        /// Get new delayed property declaration
+        /// </summary>
+        private string GetNewDelayedPropertyDeclaration(ElementBuilder elementBuilder, Property oldProperty)
+        {
+            string propName = oldProperty.Name;
+            string typeName = oldProperty.GetTypeName();
+
+            Property newProperty = elementBuilder.AddProperty(null, typeName, propName);
+            newProperty.Visibility = oldProperty.Visibility;
+            newProperty.IsStatic = oldProperty.IsStatic;
+            newProperty.IsVirtual = oldProperty.IsVirtual;
+            newProperty.IsOverride = oldProperty.IsOverride;
+            newProperty.IsExplicitInterfaceMember = oldProperty.IsExplicitInterfaceMember;
+
+            AttributeSection attrSection = elementBuilder.AddAttributeSection(newProperty);
+            elementBuilder.AddAttribute(attrSection, "Delayed");
+
+            if (oldProperty.HasGetter)
+            {
+                Get getter = elementBuilder.AddGetter(newProperty);
+                ExpressionCollection expressionCollection = new ExpressionCollection();
+                expressionCollection.Add(new PrimitiveExpression(String.Format("\"{0}\"", propName)));
+                string methodName = String.Format("GetDelayedPropertyValue<{0}>", typeName);
+                elementBuilder.AddReturn(getter, elementBuilder.BuildMethodCall(methodName, expressionCollection, null));
             }
-        } // refactoringProvider_Apply
+
+            if (oldProperty.HasSetter)
+            {
+                Set setter = elementBuilder.AddSetter(newProperty);
+                ExpressionCollection expressionCollection = new ExpressionCollection();
+                expressionCollection.Add(new PrimitiveExpression(String.Format("\"{0}\"", propName)));
+                expressionCollection.Add(new ElementReferenceExpression("value"));
+                string methodName = String.Format("SetDelayedPropertyValue<{0}>", typeName);
+                elementBuilder.AddMethodCall(setter, methodName, expressionCollection, null);
+            } // if
+
+            return elementBuilder.GenerateCode();
+        } // GetNewDelayedPropertyDeclaration(elementBuilder, oldProperty)
 
         /// <summary>
         /// Get new property declaration
@@ -121,24 +264,40 @@ namespace Refactor_ConvertToXPOProperty
                     new ElementReferenceExpression(fieldVariableName)));
                 expressionCollection.Add(new ElementReferenceExpression("value"));
                 elementBuilder.AddMethodCall(setter, "SetPropertyValue", expressionCollection, null);
-            }
+            } // if
 
             return elementBuilder.GenerateCode();
-        } // GetNewPropertyDeclaration
+        } // GetNewPropertyDeclaration(elementBuilder, oldProperty)
 
         /// <summary>
         /// Refactoring provider prepare preview
         /// </summary>
         private void refactoringProvider_PreparePreview(object sender, PrepareContentPreviewEventArgs ea)
         {
-            if (_Property != null)
+            if (_Property != null && ea.SelectedSubMenuItem != null)
             {
                 ElementBuilder elementBuilder = ea.NewElementBuilder();
-                string newCode = GetNewPropertyDeclaration(elementBuilder, _Property);
+                string newCode = string.Empty;
+                switch (ea.SelectedSubMenuItem.Name)
+                {
+                    case MenuItem_ConvertTo_XPOProperty:
+                        newCode = GetNewPropertyDeclaration(elementBuilder, _Property);
+                        break;
+                    case MenuItem_ConvertTo_DelayedProperty:
+                        newCode = GetNewDelayedPropertyDeclaration(elementBuilder, _Property);
+                        break;
+                } // switch
 
-                ea.AddCodePreview(_Property.Range.Start, newCode.Trim());
-                ea.AddStrikethrough(_Property.Range);
-            }
-        } // refactoringProvider_PreparePreview
+                if (!string.IsNullOrEmpty(newCode))
+                {
+                    ea.AddCodePreview(_Property.Range.Start, newCode.Trim());
+                    ea.AddStrikethrough(_Property.Range);
+
+                    // How remove field variable used by that property?
+                    //if (_IsXpoProperty && _Property.HasGetter)
+                    //    ea.AddStrikethrough(_Property.Getter.LastChild.FirstDetail.Range);
+                }
+            } // if
+        } // refactoringProvider_PreparePreview(sender, ea)
     } // class ConvertToXPOPropertyPlugin
 } // namespace Refactor_ConvertToXPOProperty
