80d6d12d64b56bb0c15358ad084348d5024641cd
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2010-11-24 20:02:39 +0000

    added 2 more code issues

--- trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1403_FileMayOnlyContainASingleNamespace.cs
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1403_FileMayOnlyContainASingleNamespace.cs
@@ -7,10 +7,8 @@
     using Microsoft.StyleCop;
     using Microsoft.StyleCop.CSharp;
 
-    internal class SA1401_FieldsMustBePrivate : ICodeIssue
+    internal class SA1403_FileMayOnlyContainASingleNamespace : ICodeIssue
     {
-        private string[] modifiers = new string[] { "public", "internal", "protected" };
-
         public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
         {
             string message = String.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
@@ -26,23 +24,25 @@
                                   where token.LineNumber >= violation.Line && !string.IsNullOrEmpty(token.Text.Trim())
                                   select token)
             {
-                if (modifiers.Contains(token.Text))
+                if (token.Text == "namespace")
                 {
-                    if (startPoint == null)
-                    {
-                        startPoint = new SourcePoint(token.Location.StartPoint.LineNumber, token.Location.StartPoint.IndexOnLine + 1);
-                        endPoint = new SourcePoint(token.Location.EndPoint.LineNumber, token.Location.EndPoint.IndexOnLine + 2);
-                        continue;
-                    }
-                    else
-                    {
-                        endPoint = new SourcePoint(token.Location.EndPoint.LineNumber, token.Location.EndPoint.IndexOnLine + 2);
-                        continue;
-                    }
+                    startPoint = endPoint = new SourcePoint(token.Location.StartPoint.LineNumber, token.Location.StartPoint.IndexOnLine + 1);
+                    continue;
                 }
-                SourceRange sourceRange = new SourceRange(startPoint.Value, endPoint.Value);
-                ea.AddSmell(sourceRange, message, 10);
-                return;
+                if (token.Text == "{")
+                {
+                    if (startPoint != null)
+                    {
+                        SourceRange sourceRange = new SourceRange(startPoint.Value, endPoint.Value);
+                        ea.AddSmell(sourceRange, message, 10);
+                    }
+                    return;
+                }
+                if (startPoint != null)
+                {
+                    endPoint = new SourcePoint(token.Location.EndPoint.LineNumber, token.Location.EndPoint.IndexOnLine + 2);
+                    continue;
+                }
             }
         }
     }
