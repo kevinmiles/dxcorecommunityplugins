bd243f28c729982d75d89999cf361c954b4970e6
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2010-12-19 00:40:13 +0000

    Final implementation for now.

--- trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1119_StatementMustNotUseUnnecessaryParenthesis.cs
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1119_StatementMustNotUseUnnecessaryParenthesis.cs
@@ -7,9 +7,76 @@
     using DevExpress.CodeRush.Diagnostics.General;
     using Microsoft.StyleCop;
     using Microsoft.StyleCop.CSharp;
+    using System.Collections.Generic;
 
     internal class SA1119_StatementMustNotUseUnnecessaryParenthesis : ICodeIssue
     {
+        private List<LanguageElementType> validParenthesizedContent = new List<LanguageElementType>()
+            {
+                LanguageElementType.TypeCheck,
+                LanguageElementType.ConditionalTypeCast,
+                LanguageElementType.TypeCastExpression,
+                LanguageElementType.NullCoalescingExpression,
+                LanguageElementType.LogicalInversion,
+                LanguageElementType.LogicalOperation,
+                LanguageElementType.ConditionalExpression,
+                LanguageElementType.AssignmentExpression,
+                LanguageElementType.BinaryOperatorExpression,
+                LanguageElementType.UnaryDecrement,
+                LanguageElementType.UnaryIncrement,
+                LanguageElementType.UnaryOperatorExpression,
+                LanguageElementType.ObjectCreationExpression,
+                LanguageElementType.ArrayCreateExpression,
+                LanguageElementType.LambdaExpression,
+                LanguageElementType.QueryExpression,
+                LanguageElementType.RelationalOperation
+            };
+
+        private List<LanguageElementType> invalidParenthesizedParent = new List<LanguageElementType>()
+            {
+                LanguageElementType.Assignment,
+                LanguageElementType.Block,
+                LanguageElementType.Break,
+                LanguageElementType.Case,
+                LanguageElementType.CaseClause,
+                LanguageElementType.CaseClausesList,
+                LanguageElementType.Catch,
+                LanguageElementType.Checked,
+                LanguageElementType.ConstructorInitializer,
+                LanguageElementType.Continue,
+                LanguageElementType.Do,
+                LanguageElementType.DoStatement,
+                LanguageElementType.Else,
+                LanguageElementType.EmptyStatement,
+                LanguageElementType.Finally,
+                LanguageElementType.Fixed,
+                LanguageElementType.For,
+                LanguageElementType.ForEach,
+                LanguageElementType.ForEachExpression,
+                LanguageElementType.ForExpression,
+                LanguageElementType.Goto,
+                LanguageElementType.If,
+                LanguageElementType.IfExpression,
+                LanguageElementType.ImplicitVariable,
+                LanguageElementType.InitializedVariable,
+                LanguageElementType.Label,
+                LanguageElementType.Lock,
+                LanguageElementType.Return,
+                LanguageElementType.Switch,
+                LanguageElementType.Throw,
+                LanguageElementType.Try,
+                LanguageElementType.Unchecked,
+                LanguageElementType.UncheckedExpression,
+                LanguageElementType.UnsafeStatement,
+                LanguageElementType.UsingStatement,
+                LanguageElementType.While,
+                LanguageElementType.WhileExpression,
+                LanguageElementType.YieldBreak,
+                LanguageElementType.YieldComputation,
+                LanguageElementType.YieldResult,
+                LanguageElementType.YieldReturn
+            };
+
         public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
         {
             string message = String.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
@@ -22,12 +89,8 @@
 
             foreach (IElement element in ea.GetEnumerable(ea.Scope, new ElementTypeFilter(LanguageElementType.ParenthesizedExpression)).Where(x => x.FirstNameRange.Start.Line == violation.Line))
             {
-                Log.SendMsg("CR_StyleCop: element found " + element.Name);
-                Log.SendMsg("CR_StyleCop: element parent is " + element.Parent.ElementType);
-                if (element.Parent.ElementType == LanguageElementType.Assignment
-                    || element.Parent.ElementType == LanguageElementType.ImplicitVariable
-                    || element.Parent.ElementType == LanguageElementType.InitializedVariable
-                    || element.Parent.ElementType == LanguageElementType.Return)
+                if (!this.validParenthesizedContent.Contains(element.Children[0].ElementType)
+                    || this.invalidParenthesizedParent.Contains(element.Parent.ElementType))
                 {
                     ea.AddSmell(element.FirstNameRange, message, 10);
                 }
