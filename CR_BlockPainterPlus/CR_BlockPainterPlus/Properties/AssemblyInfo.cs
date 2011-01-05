a256fa0dde6edbe667ce2cfe66fd49a49b2769dc
rorybecker <RoryBecker@Gmail.com>
2011-01-05 09:55:24 +0000

    CR_BlockPainterPlus: Changed LoadType from LoadDisabled to LoadEnabled

--- CR_BlockPainterPlus/CR_BlockPainterPlus/Properties/AssemblyInfo.cs
+++ CR_BlockPainterPlus/CR_BlockPainterPlus/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using DevExpress.CodeRush.Common;
 
-[assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "CR_BlockPainterPlus", PlugInLoadType.StartUp, LoadAbilityType.LoadDisabled)]
+[assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "CR_BlockPainterPlus", PlugInLoadType.StartUp, LoadAbilityType.LoadEnabled)]
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
