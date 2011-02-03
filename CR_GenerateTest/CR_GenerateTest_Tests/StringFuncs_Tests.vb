b3c3653174feba9389bc3ce76615c23706940070
rorybecker <RoryBecker@Gmail.com>
2011-02-03 19:51:24 +0000

    CR_GenerateTest: Complete ground-up rewrite.

---
+++ CR_GenerateTest/CR_GenerateTest_Tests/StringFuncs_Tests.vb
@@ -0,0 +1,13 @@
+﻿'Imports NUnit.Framework
+'Imports CR_GenerateTest
+'<TestFixture()> _
+'Public Class StringFuncs_Tests
+'    <Test()> _
+'    Public Sub Test1()
+'        Assert.AreEqual("MyClass", StringFuncs.ExtractClassname("NS1.NS2.MyClass"))
+'    End Sub
+'    <Test()> _
+'    Public Sub Test2()
+'        Assert.AreEqual("MyClass", StringFuncs.ExtractClassname("MyClass"))
+'    End Sub
+'End Class
