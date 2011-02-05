752d9861429749e0ebd9d05aee82c6fc1e2c5212
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-02-05 22:04:10 +0000

    now integrated with StyleCop settings

--- CR_StyleCop/CR_StyleCop/CR_StyleCopPlugIn.cs
+++ CR_StyleCop/CR_StyleCop/CR_StyleCopPlugIn.cs
@@ -2,38 +2,41 @@ namespace CR_StyleCop
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
+    using System.Xml;
     using CR_StyleCop.CodeIssues;
     using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.Diagnostics.General;
     using DevExpress.CodeRush.PlugInCore;
     using DevExpress.CodeRush.StructuralParser;
     using Microsoft.StyleCop;
 
     public partial class CR_StyleCopPlugIn : StandardPlugIn
     {
-        // DXCore-generated code...
-        #region InitializePlugIn
+        private Dictionary<string, List<Violation>> violations;
+        private CodeIssueFactory issuesFactory;
+        private ObjectBasedEnvironment environment;
+        private StyleCopObjectConsole styleCopConsole;
+
         public override void InitializePlugIn()
         {
             base.InitializePlugIn();
 
-            //
-            // TODO: Add your initialization code here.
-            //
+            this.violations = new Dictionary<string, List<Violation>>();
+            this.issuesFactory = new CodeIssueFactory();
+            this.environment = new ObjectBasedEnvironment(this.SourceCodeFactory, this.SettingsFactory);
+            this.styleCopConsole = new StyleCopObjectConsole(this.environment, null, null, true);
+            this.styleCopConsole.ViolationEncountered += this.OnViolationEncountered;
         }
-        #endregion
-        #region FinalizePlugIn
+
         public override void FinalizePlugIn()
         {
-            //
-            // TODO: Add your finalization code here.
-            //
+            this.styleCopConsole.ViolationEncountered -= this.OnViolationEncountered;
+            this.styleCopConsole.Dispose();
+            this.styleCopConsole = null;
 
             base.FinalizePlugIn();
         }
-        #endregion
-
-        private Dictionary<string, List<Violation>> violations = new Dictionary<string, List<Violation>>();
-        private CodeIssueFactory factory = new CodeIssueFactory();
 
         private void StyleCopIssueProvider_LanguageSupported(LanguageSupportedEventArgs ea)
         {
@@ -43,33 +46,36 @@ namespace CR_StyleCop
         private void StyleCopIssueProvider_CheckCodeIssues(object sender, CheckCodeIssuesEventArgs ea)
         {
             if (ea.IsSuppressed(ea.Scope))
+            {
                 return;
+            }
 
             var scope = ea.Scope as SourceFile;
             if (scope == null || scope.Document == null)
+            {
                 return;
+            }
 
             var project = scope.Project as ProjectElement;
             if (project == null)
-                return;
-
-            using (StyleCopObjectConsole styleCopConsole = new StyleCopObjectConsole(new ObjectBasedEnvironment(this.CodeFactory, this.SettingsFactory), null, null, true))
             {
-                Configuration configuration = new Configuration(new string[] { "DEBUG" });
-                CodeProject styleCopProject = new CodeProject(project.FullName.GetHashCode(), project.FullName, configuration);
-                string analyzedCode = scope.Document.GetText(scope.StartLine, scope.StartOffset, scope.EndLine, scope.EndOffset);
-                styleCopConsole.Core.Environment.AddSourceCode(styleCopProject, scope.FilePath, analyzedCode);
-                this.violations.Clear();
-                styleCopConsole.ViolationEncountered += this.OnViolationEncountered;
-                styleCopConsole.Start(new List<CodeProject> { styleCopProject });
-                styleCopConsole.ViolationEncountered -= this.OnViolationEncountered;
+                return;
             }
+
+            this.violations.Clear();
+
+            string settingsFolder = this.GetProjectFolder(project);
+            var configuration = new Configuration(new[] { "DEBUG", "TRACE" });
+            var styleCopProject = new CodeProject(project.FullName.GetHashCode(), settingsFolder, configuration);
+            string analyzedCode = scope.Document.GetText(scope.StartLine, scope.StartOffset, scope.EndLine, scope.EndOffset);
+            this.environment.AddSourceCode(styleCopProject, scope.FilePath, analyzedCode);
+            this.styleCopConsole.Start(new List<CodeProject> { styleCopProject });
 
             foreach (var violationList in this.violations.Values)
             {
                 foreach (var violation in violationList)
                 {
-                    ICodeIssue issue = this.factory.GetIssueFor(violation);
+                    ICodeIssue issue = this.issuesFactory.GetIssueFor(violation);
                     IDocument document = scope.Document;
                     if (document != null)
                     {
@@ -79,15 +85,55 @@ namespace CR_StyleCop
             }
         }
 
-        private Microsoft.StyleCop.SourceCode CodeFactory(string path, CodeProject project, SourceParser parser, object context)
+        private SourceCode SourceCodeFactory(string path, CodeProject project, SourceParser parser, object context)
         {
             string codeToAnalyze = (string)context;
             return new AnalyzedSourceCode(project, parser, path, codeToAnalyze);
         }
 
-        private Microsoft.StyleCop.Settings SettingsFactory(string path, bool readOnly)
+        private Settings SettingsFactory(string path, bool readOnly)
         {
-            return null;
+            try
+            {
+                string settingsPath = Path.Combine(path, "Settings.StyleCop");
+                if (!File.Exists(settingsPath))
+                {
+                    return null;
+                }
+
+                var document = new XmlDocument();
+                document.Load(settingsPath);
+                var writeTime = File.GetLastWriteTime(settingsPath);
+
+                if (readOnly)
+                {
+                    return new Settings(this.styleCopConsole.Core, settingsPath, document, writeTime);
+                }
+                else
+                {
+                    return new WritableSettings(this.styleCopConsole.Core, settingsPath, document, writeTime);
+                }
+            }
+            catch (IOException ioex)
+            {
+                Log.SendException(ioex);
+                return null;
+            }
+            catch (UnauthorizedAccessException uaex)
+            {
+                Log.SendException(uaex);
+                return null;
+            }
+            catch (XmlException xmlex)
+            {
+                Log.SendException(xmlex);
+                return null;
+            }
+        }
+
+        private string GetProjectFolder(ProjectElement project)
+        {
+            return Path.GetDirectoryName(project.FilePath);
         }
 
         private void OnViolationEncountered(object sender, ViolationEventArgs e)
