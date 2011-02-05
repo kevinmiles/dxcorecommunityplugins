e4d8cdba04264216398cdf6ff046393c462835a9
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-02-05 12:08:16 +0000

    added sa1202 code issue

--- trunk/CR_StyleCop/CR_StyleCop/CodeIssueFactory.cs
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssueFactory.cs
@@ -103,7 +103,9 @@
             this.handlers.Add("SA1309", new SA1309_FieldNamesMustNotBeginWithUnderscore()); // OK
             this.handlers.Add("SA1310", new SA1310_FieldNamesMustNotContainUnderscore()); // OK
 
-            this.handlers.Add("SA1200", new SA1200_UsingDirectivesMustBePlacedWithinNamespace());// OK
+            this.handlers.Add("SA1200", new SA1200_UsingDirectivesMustBePlacedWithinNamespace()); // OK
+            //this.handlers.Add("SA1201", new SA1201_ElementsMustAppearInTheCorrectOrder());
+            this.handlers.Add("SA1202", new SA1202_ElementsMustBeOrderedByAccess());
             this.handlers.Add("SA1208", new SA1208_SystemUsingDirectivesMustBePlacedBeforeOtherUsingDirectives());// OK
             this.handlers.Add("SA1209", new SA1209_UsingAliasDirectivesMustBePlacedAfterOtherUsingDirectives());// OK
             this.handlers.Add("SA1210", new SA1210_UsingDirectivesMustBeOrderedAlphabeticallyByNamespace());// OK
