caf22cda86a50cd67110c1187dea8b82be57320b
rorybecker <RoryBecker@Gmail.com>
2011-02-06 00:25:47 +0000

    CR_GenerateTest:  - Added Options page  - Suffixes and prefix now configurable.  - Formatting now working  - Field and FinalTarget also still working.
     - FinalTarget could probably do with a little more positional tweaking.

--- CR_GenerateTest/CR_GenerateTest/TestGenerators/ITestGenerator.vb
+++ CR_GenerateTest/CR_GenerateTest/TestGenerators/ITestGenerator.vb
@@ -2,13 +2,10 @@ Option Infer On
 Imports DevExpress.CodeRush.StructuralParser
 
 Public Interface ITestGenerator
+    Sub SetOptions(ByVal ProjectSuffix As String, ByVal FixtureSuffix As String, ByVal TestPrefix As String, ByVal TestPostfix As String)
     Sub GenerateTest(ByVal SourceProject As ProjectElement, _
-                     ByVal SourceType As [Class], _
-                     ByVal TestLanguage As String)
-    ReadOnly Property TestnamePrefix As String
-    ReadOnly Property TestnamePostfix As String
-    ReadOnly Property TestProjectSuffix() As String
-    ReadOnly Property TestFixtureSuffix() As String
+                         ByVal SourceType As [Class], _
+                         ByVal TestLanguage As String)
     Function IsNonTestClass(ByVal CodeActive As LanguageElement) As Boolean
     Function IsNonTestMethod(ByVal CodeActive As LanguageElement) As Boolean
 End Interface
--- CR_GenerateTest/CR_GenerateTest/TestGenerators/NUnitTestGenerator.vb
+++ CR_GenerateTest/CR_GenerateTest/TestGenerators/NUnitTestGenerator.vb
@@ -1,27 +1,7 @@
 Option Infer On
 Public Class NUnitTestGenerator
     Inherits BaseTestGenerator
-
-    Public Overrides ReadOnly Property TestAssemblyName As String
-        Get
-            Return "NUnit.Framework.dll"
-        End Get
-    End Property
-    Public Overrides ReadOnly Property TestNamespace As String
-        Get
-            Return "NUnit.Framework"
-        End Get
-    End Property
-    Public Overrides ReadOnly Property TestAttributeName As String
-        Get
-            Return "Test"
-        End Get
-    End Property
-    Public Overrides ReadOnly Property TestFixtureAttributeName As String
-        Get
-            Return "TestFixture"
-        End Get
-    End Property
-
-
+    Public Sub New()
+        MyBase.New("NUnit.Framework.dll", "NUnit.Framework", "TestFixture", "Test")
+    End Sub
 End Class
