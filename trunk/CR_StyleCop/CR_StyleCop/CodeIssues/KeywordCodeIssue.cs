03cd01c300c031946ff7193abfb4054d5187d4d1
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-02-10 23:36:23 +0000

    added sa1100 code issue

--- trunk/CR_StyleCop/CR_StyleCop/CodeIssues/KeywordCodeIssue.cs
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/KeywordCodeIssue.cs
@@ -19,8 +19,8 @@
 
         public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
         {
-            string message = String.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
-            CsElement csElement = violation.Element as CsElement;
+            var message = string.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
+            var csElement = violation.Element as CsElement;
             if (csElement == null)
             {
                 ea.AddSmell(new SourceRange(violation.Line, 1, violation.Line, document.LengthOfLine(violation.Line) + 1), message, 10);
---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1100_DoNotPrefixCallsWithBaseUnlessLocalImplementationExists.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1100_DoNotPrefixCallsWithBaseUnlessLocalImplementationExists : KeywordCodeIssue
+    {
+        public SA1100_DoNotPrefixCallsWithBaseUnlessLocalImplementationExists()
+            : base("base")
+        {
+        }
+    }
+}
