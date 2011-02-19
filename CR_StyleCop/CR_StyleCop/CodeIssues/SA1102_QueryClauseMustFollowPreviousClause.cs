2f7fdccaadd584c99cb27dc1f3c8217eac811574
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-02-19 00:12:38 +0000

    Added sa1102, sa1103, sa1104 and sa1105 code issues

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1102_QueryClauseMustFollowPreviousClause.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1102_QueryClauseMustFollowPreviousClause : QueryExpressionCodeIssue
+    {
+        public SA1102_QueryClauseMustFollowPreviousClause()
+            : base(Underline.FirstKeywordOnLine)
+        {
+        }
+    }
+}
