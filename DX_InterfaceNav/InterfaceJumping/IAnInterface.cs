47cffd580f21b60023b5d38fa4ca488733e8a947
rorybecker <RoryBecker@Gmail.com>
2011-03-01 15:57:43 +0000

    DX_InterfaceNav: Refactored example project to remove it from build.

---
+++ DX_InterfaceNav/InterfaceJumping/IAnInterface.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace InterfaceJumping
+{
+    // from here I would click on the interface and select the implementation to go to (Which I can)
+
+    public interface IAnInterface
+    {
+        void DoesSomething();
+    }
+}
---
+++ DX_InterfaceNav/InterfaceJumping/MyFirstImplementor.cs
@@ -0,0 +1,9 @@
+﻿
+namespace InterfaceJumping
+{
+    // from here I would click on the interface type and say go to declaration (which I can already), or jump to another implementation
+
+    class MyFirstImplementor : IAnInterface
+    {
+    }
+}
