81d75241e9c43844c63fe899b150909f272430e8
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-03-08 22:11:45 +0000

    added sa1106, 1108, 1109, 1110 and 1111 issues

---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1110_OpeningParenthesisMustBeOnDeclarationLine.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1110_OpeningParenthesisMustBeOnDeclarationLine : KeywordCodeIssue
+    {
+        public SA1110_OpeningParenthesisMustBeOnDeclarationLine()
+            : base(Underline.FirstKeywordOnLine, "(", "[")
+        {
+        }
+    }
+}
---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1111_ClosingParenthesisMustBeOnLineOfLastParameter.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1111_ClosingParenthesisMustBeOnLineOfLastParameter : KeywordCodeIssue
+    {
+        public SA1111_ClosingParenthesisMustBeOnLineOfLastParameter()
+            : base(Underline.FirstKeywordOnLine, ")", "]")
+        {
+        }
+    }
+}
