93464e604f6410cab2d18504d93ce84fa1f5ecdd
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-03-09 21:55:17 +0000

    added sa1112, 1113, 1114 and 1115 issues

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/FirstParameterCodeIssue.cs
@@ -0,0 +1,47 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Linq;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using Microsoft.StyleCop;
+    using Microsoft.StyleCop.CSharp;
+
+    internal class FirstParameterCodeIssue : ICodeIssue
+    {
+        public void AddViolationIssue(CheckCodeIssuesEventArgs ea, IDocument document, Violation violation)
+        {
+            var message = string.Format("{0}: {1}", violation.Rule.CheckId, violation.Message);
+            var csElement = violation.Element as CsElement;
+            if (csElement == null)
+            {
+                ea.AddSmell(new SourceRange(violation.Line, 1, violation.Line, document.LengthOfLine(violation.Line) + 1), message, 10);
+                return;
+            }
+
+            CodePoint startPoint = null;
+            CodePoint endPoint = null;
+            foreach (var token in from token in csElement.ElementTokens
+                                  where token.LineNumber == violation.Line && token.CsTokenType != CsTokenType.WhiteSpace
+                                  select token)
+            {
+                if (startPoint == null)
+                {
+                    startPoint = token.Location.StartPoint;
+                    endPoint = token.Location.EndPoint;
+                }
+
+                if (token.CsTokenType == CsTokenType.CloseSquareBracket
+                    || token.CsTokenType == CsTokenType.CloseParenthesis
+                    || token.CsTokenType == CsTokenType.Comma)
+                {
+                    var sourceRange = new SourceRange(startPoint.LineNumber, startPoint.IndexOnLine + 1, endPoint.LineNumber, endPoint.IndexOnLine + 2);
+                    ea.AddSmell(sourceRange, message, 10);
+                    return;
+                }
+
+                endPoint = token.Location.EndPoint;
+            }
+        }
+    }
+}
---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1112_ClosingParenthesisMustBeOnLineOfOpeningParenthesis.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1112_ClosingParenthesisMustBeOnLineOfOpeningParenthesis : KeywordCodeIssue
+    {
+        public SA1112_ClosingParenthesisMustBeOnLineOfOpeningParenthesis()
+            : base(Underline.FirstKeywordOnLine, ")", "]")
+        {
+        }
+    }
+}
