bae9238270a07a977a0758af46f3407a361a4729
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2011-03-10 10:48:15 +0000

    DX_ConvertToTryParse:  - Added additional trigger point of Parse method

--- trunk/DX_ConvertToTryParse/DX_ConvertToTryParse/PlugIn1.vb
+++ trunk/DX_ConvertToTryParse/DX_ConvertToTryParse/PlugIn1.vb
@@ -30,7 +30,7 @@ Public Class PlugIn1
         CType(ConvertToTryParse, ISupportInitialize).EndInit()
     End Sub
     Private Sub ConvertToTryParse_CheckAvailability(ByVal sender As Object, ByVal ea As CheckContentAvailabilityEventArgs)
-        Dim InitializedVar = TryCast(ea.CodeActive, InitializedVariable)
+        Dim InitializedVar = GetInitializedVar(ea.CodeActive)
         If InitializedVar Is Nothing Then
             Exit Sub
         End If
@@ -50,11 +50,25 @@ Public Class PlugIn1
         End If
         ea.Available = True ' Change this to return true, only when your refactoring should be available.
     End Sub
+    Private Shared Function GetInitializedVar(ByVal CodeActive As LanguageElement) As InitializedVariable
+        Dim InitializedVar As InitializedVariable = TryCast(CodeActive, InitializedVariable)
+        If InitializedVar Is Nothing Then
+            Dim MRE = TryCast(CodeActive, MethodReferenceExpression)
+            If MRE Is Nothing Then
+                Return Nothing
+            End If
+            Dim MREParent As LanguageElement = MRE.Parent
+            If MREParent Is Nothing Then
+                Return Nothing
+            End If
+            InitializedVar = TryCast(MREParent.Parent, InitializedVariable)
+        End If
+        Return InitializedVar
+    End Function
     Private Sub ConvertToTryParse_Execute(ByVal Sender As Object, ByVal ea As ApplyContentEventArgs)
-
         Dim Doc = CodeRush.Documents.ActiveTextDocument
         Using Doc.NewCompoundAction("Convert to Tryparse")
-            Dim InitializedVar = TryCast(ea.CodeActive, InitializedVariable)
+            Dim InitializedVar = GetInitializedVar(ea.CodeActive)
             Dim Builder = New ElementBuilder
             Dim Range = InitializedVar.Range
             Dim MethodCall = TryCast(InitializedVar.Expression, MethodCallExpression)
