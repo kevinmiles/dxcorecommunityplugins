c41caf6b3a2aec386750c8a84aa2194be96d546d
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-04-02 21:46:06 +0000

    updated stylecop

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/FirstToLastTokenByTypeIssueLocator.cs
@@ -0,0 +1,38 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class FirstToLastTokenByTypeIssueLocator: ICodeIssueLocator
+    {
+        private readonly IEnumerable<CsTokenType> tokenTypes;
+
+        public FirstToLastTokenByTypeIssueLocator(params CsTokenType[] tokenTypes)
+        {
+            this.tokenTypes = tokenTypes;
+        }
+
+        public IEnumerable<StyleCopCodeIssue> GetCodeIssues(
+            IDocument document,
+            Func<ElementTypeFilter, IEnumerable<IElement>> enumerate,
+            Violation violation,
+            CsElement csElement)
+        {
+            var locations = from token in csElement.ElementTokens
+                   where token.LineNumber == violation.Line && this.tokenTypes.Contains(token.CsTokenType)
+                   select token.Location;
+            var firstToken = locations.FirstOrDefault();
+            var lastToken = locations.LastOrDefault();
+
+            if (firstToken != null && lastToken != null)
+            {
+                yield return new StyleCopCodeIssue(CodeIssueType.CodeSmell, new SourceRange(firstToken.StartPoint.LineNumber, firstToken.StartPoint.IndexOnLine + 1, lastToken.EndPoint.LineNumber, lastToken.EndPoint.IndexOnLine + 2));
+            }
+        }
+    }
+}
---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1116_SplitParametersMustStartOnLineAfterDeclaration.cs
@@ -0,0 +1,12 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+
+    internal class SA1116_SplitParametersMustStartOnLineAfterDeclaration : StyleCopRule
+    {
+        public SA1116_SplitParametersMustStartOnLineAfterDeclaration()
+            : base(new FirstParameterIssueLocator())
+        {
+        }
+    }
+}
