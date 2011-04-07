389109fa706317302e2288bbebad04a8937cf007
rorybecker <RoryBecker@Gmail.com>
2011-04-07 18:28:32 +0000

    New Plugin: Refactor_NamedParameters

---
+++ Refactor_NamedParameters/Refactor_NamedParameters/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using DevExpress.CodeRush.Common;
+
+[assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "Refactor_NamedParameters", PlugInLoadType.Demand, LoadAbilityType.LoadEnabled)]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Refactor_NamedParameters")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Refactor_NamedParameters")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8527de5e-c573-4fa9-a71b-4e7d3c18662d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
