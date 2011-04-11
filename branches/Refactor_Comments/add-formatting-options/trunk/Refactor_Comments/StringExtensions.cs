472524460619501a009044be7c9d8e38a073b4af
travis.illig <travis.illig@241145c3-2f4c-0410-ae16-21396c40807f>
2011-04-11 16:29:05 +0000

    Started refactoring out methods to thin down the plugin code.

---
+++ branches/Refactor_Comments/add-formatting-options/trunk/Refactor_Comments/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using DevExpress.CodeRush.Core;
+
+namespace Refactor_Comments
+{
+	public static class StringExtensions
+	{
+		public static bool SupportsMultiLineComments(this string languageId)
+		{
+			if (languageId == null)
+			{
+				throw new ArgumentNullException("languageId");
+			}
+			return CodeRush.Language.SupportsMultiLineComments(languageId) || languageId == "JavaScript";
+		}
+	}
+}
