a8c43fd6bdc3b3f4feb305808e518118a8a9e1f7
travis.illig <travis.illig@241145c3-2f4c-0410-ae16-21396c40807f>
2011-04-11 16:42:15 +0000

    Continued refactoring.

---
+++ branches/Refactor_Comments/add-formatting-options/trunk/Refactor_Comments/LanguageElementExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevExpress.CodeRush.StructuralParser;
+
+namespace Refactor_Comments
+{
+	public static class LanguageElementExtensions
+	{
+		public static bool CanBeConvertedToMultiLineComment(this LanguageElement element)
+		{
+			var comment = element as Comment;
+
+			// Element isn't a comment OR
+			// Comment is already multiline OR
+			// It's only a one-line comment.
+			return
+				comment == null ||
+				comment.CommentType == CommentType.MultiLine ||
+				(comment.PreviousConnectedComment == null && comment.NextConnectedComment == null);
+		}
+
+		public static bool CanBeConvertedToSingleLineComment(this LanguageElement element)
+		{
+			var comment = element as Comment;
+
+			// Element isn't a comment OR
+			// Comment is already single line.
+			return
+				comment == null ||
+				comment.CommentType == CommentType.SingleLine;
+		}
+	}
+}
