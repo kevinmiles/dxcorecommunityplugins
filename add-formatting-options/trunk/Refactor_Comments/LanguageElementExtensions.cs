797e3b59574aad14f3495ac263fc1cd8d6a78a76
travis.illig <travis.illig@241145c3-2f4c-0410-ae16-21396c40807f>
2011-04-12 16:16:14 +0000

    Fixed logic issue - had multiline/singleline available reversed.

--- add-formatting-options/trunk/Refactor_Comments/LanguageElementExtensions.cs
+++ add-formatting-options/trunk/Refactor_Comments/LanguageElementExtensions.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using DevExpress.CodeRush.StructuralParser;
 
 namespace Refactor_Comments
@@ -12,24 +9,26 @@ namespace Refactor_Comments
 		{
 			var comment = element as Comment;
 
-			// Element isn't a comment OR
-			// Comment is already multiline OR
-			// It's only a one-line comment.
+			// In order to convert to a multiline comment, the element...
+			// ...must be a comment AND
+			// ...must be a single-line comment AND
+			// ...must have an adjacent comment.
 			return
-				comment == null ||
-				comment.CommentType == CommentType.MultiLine ||
-				(comment.PreviousConnectedComment == null && comment.NextConnectedComment == null);
+				comment != null &&
+				comment.CommentType == CommentType.SingleLine &&
+				(comment.PreviousConnectedComment != null || comment.NextConnectedComment != null);
 		}
 
 		public static bool CanBeConvertedToSingleLineComment(this LanguageElement element)
 		{
 			var comment = element as Comment;
 
-			// Element isn't a comment OR
-			// Comment is already single line.
+			// In order to convert to a multiline comment, the element...
+			// ...must be a comment AND
+			// ...must be a multi-line comment.
 			return
-				comment == null ||
-				comment.CommentType == CommentType.SingleLine;
+				comment != null &&
+				comment.CommentType == CommentType.MultiLine;
 		}
 	}
 }
