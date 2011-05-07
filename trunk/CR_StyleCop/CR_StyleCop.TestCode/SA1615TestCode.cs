3311334e56757fecc8b2858e6f808c1d7b1136ce
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-05-07 23:20:28 +0000

    added more test code

---
+++ trunk/CR_StyleCop/CR_StyleCop.TestCode/SA1615TestCode.cs
@@ -0,0 +1,34 @@
+﻿namespace CR_StyleCop.TestCode
+{
+    using System;
+
+    /// <summary>
+    /// Test code for SA1615 rule - return value must be documented.
+    /// </summary>
+    public class SA1615TestCode
+    {
+        /// <summary>
+        /// Some delegate.
+        /// </summary>
+        /// <param name="parameter">Some parameter.</param>
+        public delegate int MyDelegate(object parameter);
+
+        /// <summary>
+        /// Some indexer.
+        /// </summary>
+        /// <param name="parameter">Some parameter.</param>
+        public int this[int parameter]
+        {
+            get { return 42; }
+        }
+
+        /// <summary>
+        /// Some method.
+        /// </summary>
+        /// <param name="parameter">Some parameter.</param>
+        public int MethodName(string parameter)
+        {
+            return 42;
+        }
+    }
+}
