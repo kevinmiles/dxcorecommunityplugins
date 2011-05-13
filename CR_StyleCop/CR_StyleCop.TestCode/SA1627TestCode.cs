ffb8d602ec0ff8c85c62246f7503216d94104fc3
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-05-13 23:36:53 +0000

    added more test code

---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1627TestCode.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="SA1627TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+
+    // BUGBUG: SA1627 is not reported at all.
+    /// <summary>
+    /// Test code for SA1627 - documentation text must not be empty.
+    /// </summary>
+    /// <remarks></remarks>
+    /// <example></example>
+    /// <exception cref="System.NullReferenceException"></exception>
+    /// <permission cref="System.Security.PermissionSet"></permission>
+    public partial class SA1627TestCode
+    {
+        /// <summary>
+        /// Some method.
+        /// </summary>
+        /// <remarks></remarks>
+        /// <example></example>
+        /// <exception cref="System.NullReferenceException"></exception>
+        /// <permission cref="System.Security.PermissionSet"></permission>
+        public void MethodName()
+        {
+        }
+    }
+}
