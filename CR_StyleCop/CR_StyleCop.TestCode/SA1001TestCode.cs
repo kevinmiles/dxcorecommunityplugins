b348f22f8d8aa757cb7ec9cc1f725523a3234371
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-10 23:25:57 +0000

    added more test cases

---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1001TestCode.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="SA1001TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    /// <summary>
+    /// Test code for SA1001 rule - commas must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1001 rule.")]
+    public class SA1001TestCode
+    {
+        private void MethodName()
+        {
+        }
+    }
+}
