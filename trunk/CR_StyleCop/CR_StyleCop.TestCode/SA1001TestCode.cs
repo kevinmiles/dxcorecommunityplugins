3c893b6731ed0f03edca5981db5bb3f00f461bba
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-12 00:11:04 +0000

    added more test code

--- trunk/CR_StyleCop/CR_StyleCop.TestCode/SA1001TestCode.cs
+++ trunk/CR_StyleCop/CR_StyleCop.TestCode/SA1001TestCode.cs
@@ -12,11 +12,17 @@ namespace CR_StyleCop.TestCode
     /// <summary>
     /// Test code for SA1001 rule - commas must be spaced correctly.
     /// </summary>
-    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1001 rule.")]
-    public class SA1001TestCode
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules","*" ,Justification = "This is about SA1001 rule.")]
+    public class SA1001TestCode<T1,T2>
     {
-        private void MethodName()
+        private int field1,field2;
+
+        private void MethodName(int paramName , int paramName2)
         {
+            this.MethodName(1,1);
+            this.MethodName(1 ,1);
+            this.MethodName(1 , 1);
+            this.MethodName(1, 1);
         }
     }
 }
