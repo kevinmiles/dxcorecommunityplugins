fee99eb6b7f31db29a905aeb2541d296cbc60e4c
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-13 22:39:57 +0000

    aded more test cases

---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1009TestCode.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="SA1009TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    /// <summary>
+    /// Test code for SA1009 rule - closing paren must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1009 rule." )]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1017:ClosingAttributeBracketsMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.") ]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:OpeningParenthesisMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.")]
+    public class SA1009TestCode
+    {
+        private int MethodName( )
+        {
+            int x = 3 * ((1 + 2) * 5 );
+            return (x + 3 ) * (5 * (1 + 2) );
+        }
+    }
+}
