aa6a1da1eee42e8ce4a96860bfe5f020270b339f
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-13 22:55:33 +0000

    updated tests

--- CR_StyleCop/CR_StyleCop.TestCode/SA1008TestCode.cs
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1008TestCode.cs
@@ -9,17 +9,29 @@ namespace CR_StyleCop.TestCode
     using System.Diagnostics.CodeAnalysis;
     using System.Linq;
 
+#pragma warning disable 1591
+
     /// <summary>
     /// Test code for SA1008 rule - opening paren must be spaced correctly.
     /// </summary>
     [SuppressMessage( "StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1008 rule.")]
-    [SuppressMessage ("StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "This is about SA1008 rule.")]
+    [SuppressMessage ("StyleCop.CSharp.SpacingRules", "SA1000:KeywordsMustBeSpacedCorrectly", Justification = "This is about SA1008 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "This is about SA1008 rule.")]
     public class SA1008TestCode
     {
+        public event EventHandler EventName1 = delegate (object sender, EventArgs e) { };
+
+        public event EventHandler EventName2 = delegate( object sender, EventArgs e) { };
+
         private int MethodName ()
         {
-            int x = 3 * (( 1 + 2) * 5);
-            return ( x + 3) *((1 + 2) * 5);
+            int x = 3 * ( (1 + 2) * 5);
+            if(x < 5)
+            {
+            }
+
+            return ( x + 3) 
+                *((1 + 2) * 5);
         }
     }
 }
