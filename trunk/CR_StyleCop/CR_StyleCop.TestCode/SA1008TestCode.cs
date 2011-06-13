3a8365ba7cba6f6efd2e90fd9598ed7803a6bb32
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-13 22:39:57 +0000

    aded more test cases

---
+++ trunk/CR_StyleCop/CR_StyleCop.TestCode/SA1008TestCode.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="SA1008TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    /// <summary>
+    /// Test code for SA1008 rule - opening paren must be spaced correctly.
+    /// </summary>
+    [SuppressMessage( "StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1008 rule.")]
+    [SuppressMessage ("StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "This is about SA1008 rule.")]
+    public class SA1008TestCode
+    {
+        private int MethodName ()
+        {
+            int x = 3 * (( 1 + 2) * 5);
+            return ( x + 3) *((1 + 2) * 5);
+        }
+    }
+}
---
+++ trunk/CR_StyleCop/CR_StyleCop.TestCode/SA1014TestCode.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="SA1014TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+#pragma warning disable 168
+#pragma warning disable 169
+#pragma warning disable 1591
+
+    /// <summary>
+    /// Test code for SA1014 rule - opening generic bracket must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1014 rule.")]
+    public class SA1014TestCode< T>
+    {
+        private List <int> ints1;
+        private List< int> ints2;
+
+        private void MethodName <T1>(T1 x)
+        {
+            List <int> ints11;
+            this.MethodName <T1>(x);
+        }
+
+        private void MethodName2< T1>(T1 x)
+        {
+            List< int> ints22;
+            this.MethodName2< T1>(x);
+        }
+
+        public struct MyStruct <T1>
+        {
+        }
+    }
+}
