414f095021945d46abb2b4d7b1830aa79519e904
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-17 22:58:57 +0000

    added more test code

---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1018TestCode.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="SA1018TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    
+#pragma warning disable 169
+#pragma warning disable 1591
+
+    /// <summary>
+    /// Test code for SA1018 rule - nullable symbol must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1018 rule.")]
+    public class SA1018TestCode
+    {
+        private int ? property;
+        private List<int ?> ints;
+
+        public int ? Property
+        {
+            get { return this.property; }
+            set { this.property = value; }
+        }
+
+        private int ? Method<T>(T parameter)
+        {
+            long ? x = null;
+            return this.Method<long ?>(x);
+        }
+
+        private void MethodName(DateTime ? date)
+        {
+        }
+    }
+}
---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1023TestCode.cs
@@ -0,0 +1,51 @@
+﻿// <copyright file="SA1023TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+#pragma warning disable 219
+#pragma warning disable 414
+
+    /// <summary>
+    /// Test code for SA1023 rule - dereference symbol must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1023 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1003:SymbolsMustBeSpacedCorrectly", Justification = "This is about SA1023 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:OpeningParenthesisMustBeSpacedCorrectly", Justification = "This is about SA1023 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1010:OpeningSquareBracketsMustBeSpacedCorrectly", Justification = "This is about SA1023 rule.")]
+    public unsafe class SA1023TestCode
+    {
+        private int
+            * x1 = null;
+
+        private int * x2 = null;
+        private int*x3 = null;
+        private int* [] y4 = null;
+
+        private void MethodName(int p)
+        {
+            int
+                * y1 = null;
+            int z1 = *
+                y1;
+
+            int * y2 = null;
+            int z2 = * y2;
+            int*y3 = null;
+            int z3 =*y3;
+            int* [] y4 = null;
+            int[] zz = null;
+            int z4 = zz[ *y3];
+            this.MethodName( *y3);
+
+            DateTime * date = null;
+            int day = ( *date).Day;
+        }
+    }
+}
