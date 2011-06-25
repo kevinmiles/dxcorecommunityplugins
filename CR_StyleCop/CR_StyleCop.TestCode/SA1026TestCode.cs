5f3fba1fe826e8cf52109237f11e131e5a2cea67
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-25 21:43:02 +0000

    added more test code

---
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1026TestCode.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="SA1026TestCode.cs" company="ACME">
+//     Copyright (c) 2011. All rights reserved.
+// </copyright>
+// <summary>Summary for the file</summary>
+
+namespace CR_StyleCop.TestCode
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    /// <summary>
+    /// Test code for SA1026 rule - new keyword in array initializer must be spaced correctly.
+    /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1026 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1010:OpeningSquareBracketsMustBeSpacedCorrectly", Justification = "This is about SA1026 rule.")]
+    public class SA1026TestCode
+    {
+        private int[] ints = new [] { 1, 10, 100, 1000 };
+
+        private void MethodName()
+        {
+            var a = new [] { 1, 10, 100, 1000 };
+        }
+    }
+}
