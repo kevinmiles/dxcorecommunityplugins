7cdcfb33bd9ecffc565abbd111eb3a7be6784e76
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2011-06-29 11:02:47 +0000

    CR_GenerateTest: Misc Refactoring

--- trunk/CR_GenerateTest/CR_GenerateTest/Extensions/LanguageElementExt.vb
+++ trunk/CR_GenerateTest/CR_GenerateTest/Extensions/LanguageElementExt.vb
@@ -9,10 +9,19 @@ Public Module LanguageElementExt
         Dim CodeElement = TryCast(Source, CodeElement)
         If HasAttributeSections(CodeElement) Then
             Dim Section As AttributeSection = TryCast(CodeElement.AttributeSections(0), AttributeSection)
-            Result &= CodeRush.Language.GenerateElement(Section, Language)
+            'Result &= CodeRush.Language.GenerateElement(Section, Language) & CodeRush.Language.ActiveExtension.LineContinuationCharacter
+            Section.SetTarget(Source)
+            Result &= CodeRush.CodeMod.GenerateCode(Section, True) '& CodeRush.Language.ActiveExtension.LineContinuationCharacter
+            'ShowCode(Section)
         End If
         Result &= CodeRush.Language.GenerateElement(Source, Language)
         Return Result
+    End Function
+    Private Function ShowCode(ByVal Section As AttributeSection) As String
+        Dim Gen As New VB.VBCodeGen()
+        Dim SupportGen = New VB.VBSupportElementCodeGen(Gen)
+        SupportGen.GenerateAttributes(Section)
+        Return Gen.Code.Code
     End Function
     Private Function HasAttributeSections(ByVal CodeElement As CodeElement) As Boolean
         Return CodeElement IsNot Nothing AndAlso CodeElement.AttributeSectionsCount > 0
--- trunk/CR_GenerateTest/CR_GenerateTest/TestGenerators/CustomTestGenerator.vb
+++ trunk/CR_GenerateTest/CR_GenerateTest/TestGenerators/CustomTestGenerator.vb
@@ -6,9 +6,9 @@ Imports SP = DevExpress.CodeRush.StructuralParser
 Public Class CustomTestGenerator
     Inherits BaseTestGenerator
     Public Sub New(ByVal FrameworkAssemblyName As String, _
-                      ByVal FrameworkNamespace As String, _
-                      ByVal FrameworkFixtureAttributeName As String, _
-                      ByVal FrameworkTestAttributeName As String)
+                   ByVal FrameworkNamespace As String, _
+                   ByVal FrameworkFixtureAttributeName As String, _
+                   ByVal FrameworkTestAttributeName As String)
         MyBase.New(FrameworkAssemblyName, FrameworkNamespace, FrameworkFixtureAttributeName, FrameworkTestAttributeName)
     End Sub
 End Class
