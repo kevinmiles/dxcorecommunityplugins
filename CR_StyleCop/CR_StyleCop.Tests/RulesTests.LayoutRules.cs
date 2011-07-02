38127be92edccd5808c5a4aaef1802797da8c410
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-07-02 00:31:08 +0000

    framework for unit testing of code issues - delivered. added unit tests for first 2 rules

---
+++ CR_StyleCop/CR_StyleCop.Tests/RulesTests.LayoutRules.cs
@@ -0,0 +1,27 @@
+﻿namespace CR_StyleCop.Tests
+{
+    using System;
+    using System.Linq;
+    using MbUnit.Framework;
+
+    public partial class RulesTests
+    {
+        [Test]
+        [Row(15, 33, 15, 34)]
+        [Row(18, 35, 18, 36)]
+        [Row(19, 33, 19, 34)]
+        [Row(25, 27, 25, 28)]
+        [Row(27, 33, 27, 34)]
+        [Row(28, 27, 28, 28)]
+        public void SA1500_should_be_reported(int startLine, int startOffset, int endLine, int endOffset)
+        {
+            this.AssertSpecificCodeIssueExists("SA1500", startLine, startOffset, endLine, endOffset);
+        }
+
+        [Test]
+        public void All_SA1500_should_be_tested()
+        {
+            this.AssertAllSpecificCodeIssuesAreTested("SA1500");
+        }
+    }
+}
---
+++ CR_StyleCop/CR_StyleCop.Tests/RulesTests.cs
@@ -0,0 +1,71 @@
+﻿namespace CR_StyleCop.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using CR_StyleCop.Tests.Helpers;
+    using DevExpress.CodeRush.StructuralParser;
+    using MbUnit.Framework;
+    using System.Reflection;
+
+    [TestFixture]
+    public partial class RulesTests
+    {
+        private IEnumerable<SourceFile> files;
+        private CR_StyleCopPlugIn plugin;
+
+        [FixtureSetUp]
+        public void SetUp()
+        {
+            ParserHelper.RegisterParserServices();
+            var solutionParser = new SolutionParser(Path.GetFullPath("..\\..\\..\\CR_StyleCop.TestCode.sln"));
+            var solution = solutionParser.GetParsedSolution();
+            var project = solution.AllProjects.Cast<ProjectElement>().First();
+            this.files = project.AllFiles.Cast<SourceFile>();
+            this.plugin = new CR_StyleCopPlugIn();
+        }
+
+        [FixtureTearDown]
+        public void TearDown()
+        {
+            ParserHelper.UnRegisterParserServices();
+            this.plugin.FinalizePlugIn();
+            this.plugin = null;
+            this.files = null;
+        }
+
+        private void AssertSpecificCodeIssueExists(string ruleCheck, int startLine, int startOffset, int endLine, int endOffset)
+        {
+            foreach (SourceFile file in files.Where(x => x.Name.EndsWith(string.Format("{0}TestCode.cs", ruleCheck))))
+            {
+                var codeIssues = plugin.GetCodeIssuesFor(file);
+                Assert.Exists(
+                    codeIssues,
+                    x => x.Message.StartsWith(ruleCheck)
+                        && x.Range.Start.Line == startLine
+                        && x.Range.Start.Offset == startOffset
+                        && x.Range.End.Line == endLine
+                        && x.Range.End.Offset == endOffset,
+                    "{0} is not reported from line {1}, col {2} to line {3}, col {4}",
+                    new object[] { ruleCheck, startLine, startOffset, endLine, endOffset });
+            }
+        }
+
+        private void AssertAllSpecificCodeIssuesAreTested(string ruleCheck)
+        {
+            var method = typeof(RulesTests).GetMethod(ruleCheck + "_should_be_reported");
+            var coveredCodeIssues = method.GetCustomAttributes(typeof(RowAttribute), false);
+            foreach (SourceFile file in files.Where(x => x.Name.EndsWith(string.Format("{0}TestCode.cs", ruleCheck))))
+            {
+                var codeIssues = plugin.GetCodeIssuesFor(file);
+                Assert.AreEqual(
+                    coveredCodeIssues.Length,
+                    codeIssues.Count(),
+                    "Not all occurances of {0} are covered with tests",
+                    ruleCheck);
+            }
+
+        }
+    }
+}
