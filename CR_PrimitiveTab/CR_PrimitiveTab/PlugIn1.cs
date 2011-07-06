32995ddd1c969b4bd8cff5cdad943fe85377df02
rorybecker <RoryBecker@Gmail.com>
2011-07-06 10:16:56 +0000

    CR_PrimitiveTab: Updated target framework to 2.0 so plugin will run in context of VS2005

--- CR_PrimitiveTab/CR_PrimitiveTab/PlugIn1.cs
+++ CR_PrimitiveTab/CR_PrimitiveTab/PlugIn1.cs
@@ -2,7 +2,7 @@ using System;
 using System.ComponentModel;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
+//using System.Linq;
 using System.Windows.Forms;
 using DevExpress.CodeRush.Core;
 using DevExpress.CodeRush.PlugInCore;
