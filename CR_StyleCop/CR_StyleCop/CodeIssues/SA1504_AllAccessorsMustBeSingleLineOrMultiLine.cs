92368ca89643b424255c3826156976630d915fa4
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-07-07 22:54:56 +0000

    fixed bugs in code issues

--- CR_StyleCop/CR_StyleCop/CodeIssues/SA1504_AllAccessorsMustBeSingleLineOrMultiLine.cs
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1504_AllAccessorsMustBeSingleLineOrMultiLine.cs
@@ -1,11 +1,12 @@
 ﻿namespace CR_StyleCop.CodeIssues
 {
     using System;
+    using StyleCop.CSharp;
 
     internal class SA1504_AllAccessorsMustBeSingleLineOrMultiLine : StyleCopRule
     {
         public SA1504_AllAccessorsMustBeSingleLineOrMultiLine()
-            : base(new ElementNameIssueLocator())
+            : base(new FirstTokenByTypeIssueLocator(CsTokenType.Get, CsTokenType.Set, CsTokenType.Add, CsTokenType.Remove))
         {
         }
     }
