6739832230a991bb70673eef06dd4c84cac060c1
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-07-22 22:47:46 +0000

    suppressed unrelated violations

--- CR_StyleCop/CR_StyleCop.TestCode/SA1309TestCode.cs
+++ CR_StyleCop/CR_StyleCop.TestCode/SA1309TestCode.cs
@@ -6,12 +6,14 @@
 namespace CR_StyleCop.TestCode
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
 
 #pragma warning disable 169
 
     /// <summary>
     /// Test code for SA1309 rule - field name must not start with _.
     /// </summary>
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1309 rule.")]
     public class SA1309TestCode
     {
         private int _fieldName;
