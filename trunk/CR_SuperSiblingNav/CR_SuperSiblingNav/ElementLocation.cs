5035b8ce57e80036a8ce72d476d7588f04dc265f
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2011-08-11 09:42:36 +0000

    CR_SuperSiblingNav: Re-targeted at .Net 2.0

--- trunk/CR_SuperSiblingNav/CR_SuperSiblingNav/ElementLocation.cs
+++ trunk/CR_SuperSiblingNav/CR_SuperSiblingNav/ElementLocation.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using DevExpress.CodeRush.StructuralParser;
 using DevExpress.CodeRush.Core;
 using System.Text.RegularExpressions;
