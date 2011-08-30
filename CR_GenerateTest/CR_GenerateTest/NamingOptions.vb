24060e2bd23634f56a265bff8d40e38aa94cd199
rorybecker <RoryBecker@Gmail.com>
2011-08-30 22:43:10 +0000

    CR_GenerateTest: minor refactorings

--- CR_GenerateTest/CR_GenerateTest/NamingOptions.vb
+++ CR_GenerateTest/CR_GenerateTest/NamingOptions.vb
@@ -1,21 +1,19 @@
 Option Infer On
-Imports DevExpress.CodeRush.Core
-Imports DevExpress.CodeRush.StructuralParser
-Imports SP = DevExpress.CodeRush.StructuralParser
-Imports EnvDTE80
-Imports System.IO
-Imports System.Runtime.CompilerServices
-Imports System.Reflection
 
 Public Class NamingOptions
+#Region "Constants"
     Public Const DEFAULT_ProjectSuffix As String = "_Tests"
     Public Const DEFAULT_FixtureSuffix As String = "_Tests"
     Public Const DEFAULT_TestPrefix As String = ""
     Public Const DEFAULT_TestSuffix As String = "_Test"
+#End Region
+#Region "Fields"
     Public ProjectSuffix As String = DEFAULT_ProjectSuffix
     Public FixtureSuffix As String = DEFAULT_FixtureSuffix
     Public TestPrefix As String = DEFAULT_TestPrefix
     Public TestSuffix As String = DEFAULT_TestSuffix
+#End Region
+#Region "Construction"
     Public Sub New()
     End Sub
     Public Sub New(ByVal ProjectSuffix As String, _
@@ -27,4 +25,5 @@ Public Class NamingOptions
         Me.TestPrefix = TestPrefix
         Me.TestSuffix = TestSuffix
     End Sub
+#End Region
 End Class
