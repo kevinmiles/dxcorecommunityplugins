f99e5e116333d155745bd0afcbea401313edb089
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-12-15 00:25:01 +0000

    added partial support for SA1001

---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1001_CommasMustBeSpacedCorrectly.cs
@@ -0,0 +1,19 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StyleCop.CSharp;
+
+    internal class SA1001_CommasMustBeSpacedCorrectly : StyleCopRule
+    {
+        public SA1001_CommasMustBeSpacedCorrectly()
+            : base(new AggregatedIssueLocator(new ICodeIssueLocator[] 
+                { 
+                    new AllTokensByTypePrecededByWhitespaceIssueLocator(CsTokenType.Comma),
+                    new AllTokensByTypeNotFollowedByWhitespaceIssueLocator(CsTokenType.Comma),
+                }))
+        {
+        }
+    }
+}
