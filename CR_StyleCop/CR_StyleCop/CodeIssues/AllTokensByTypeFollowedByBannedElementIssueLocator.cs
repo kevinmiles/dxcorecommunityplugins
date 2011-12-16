de618856d05c198614c9bc70f410b8f61720c3ed
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-12-16 00:10:29 +0000

    Added support for SA1002

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/AllTokensByTypeFollowedByBannedElementIssueLocator.cs
@@ -0,0 +1,65 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class AllTokensByTypeFollowedByBannedElementIssueLocator : ICodeIssueLocator
+    {
+        private readonly IEnumerable<CsTokenType> bannedFollowers;
+        private readonly IEnumerable<CsTokenType> tokenTypesToInspect;
+
+        public AllTokensByTypeFollowedByBannedElementIssueLocator(CsTokenType bannedFollower, params CsTokenType[] tokenTypesToInspect)
+        {
+            this.bannedFollowers = new[] { bannedFollower };
+            this.tokenTypesToInspect = tokenTypesToInspect;
+        }
+
+        public AllTokensByTypeFollowedByBannedElementIssueLocator(CsTokenType bannedFollower, IEnumerable<CsTokenType> tokenTypesToInspect)
+        {
+            this.bannedFollowers = new[] { bannedFollower };
+            this.tokenTypesToInspect = tokenTypesToInspect;
+        }
+
+        public AllTokensByTypeFollowedByBannedElementIssueLocator(IEnumerable<CsTokenType> bannedFollowers, params CsTokenType[] tokenTypesToInspect)
+        {
+            this.bannedFollowers = bannedFollowers;
+            this.tokenTypesToInspect = tokenTypesToInspect;
+        }
+
+        public AllTokensByTypeFollowedByBannedElementIssueLocator(IEnumerable<CsTokenType> bannedFollowers, IEnumerable<CsTokenType> tokenTypesToInspect)
+        {
+            this.bannedFollowers = bannedFollowers;
+            this.tokenTypesToInspect = tokenTypesToInspect;
+        }
+
+        public IEnumerable<StyleCopCodeIssue> GetCodeIssues(
+            ISourceCode sourceCode,
+            Func<ElementTypeFilter, IEnumerable<IElement>> enumerate,
+            Violation violation,
+            CsElement csElement)
+        {
+            CodeLocation issueLocation = null;
+            foreach (var token in csElement.ElementTokens.Where(x => x.LineNumber == violation.Line))
+            {
+                if (this.tokenTypesToInspect.Contains(token.CsTokenType))
+                {
+                    issueLocation = token.Location;
+                }
+                else if (this.bannedFollowers.Contains(token.CsTokenType) && issueLocation != null)
+                {
+                    yield return new StyleCopCodeIssue(CodeIssueType.CodeSmell, new SourceRange(issueLocation.StartPoint.LineNumber, issueLocation.StartPoint.IndexOnLine + 1, issueLocation.EndPoint.LineNumber, issueLocation.EndPoint.IndexOnLine + 2));
+                    issueLocation = null;
+                }
+                else
+                {
+                    issueLocation = null;
+                }
+            }
+        }
+    }
+}
