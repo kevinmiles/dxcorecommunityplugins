545fd77cfeb48d08ed72f2abd83ee6274415a223
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-12-23 17:44:52 +0000

    added full support for SA1003

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/AllTokensPrecededByBannedElementAndWhitespaceIssueLocator.cs
@@ -0,0 +1,60 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class AllTokensPrecededByBannedElementAndWhitespaceIssueLocator : ICodeIssueLocator
+    {
+        private readonly Func<CsElement, IEnumerable<CsToken>> getTokens;
+        private readonly IEnumerable<CsTokenType> bannedPredecessors;
+        private readonly Func<CsToken, Violation, bool> reportIssueFor;
+
+        public AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(Func<CsElement, IEnumerable<CsToken>> getTokens, Func<CsToken, Violation, bool> predicate, params CsTokenType[] bannedPredecessors)
+        {
+            this.getTokens = getTokens;
+            this.bannedPredecessors = bannedPredecessors;
+            this.reportIssueFor = predicate;
+        }
+
+        public AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(Func<CsElement, IEnumerable<CsToken>> getTokens, Func<CsToken, Violation, bool> predicate, IEnumerable<CsTokenType> bannedPredecessors)
+        {
+            this.getTokens = getTokens;
+            this.bannedPredecessors = bannedPredecessors;
+            this.reportIssueFor = predicate;
+        }
+
+        public IEnumerable<StyleCopCodeIssue> GetCodeIssues(
+            ISourceCode sourceCode,
+            Func<ElementTypeFilter, IEnumerable<IElement>> enumerate,
+            Violation violation,
+            CsElement csElement)
+        {
+            int predecessorFound = 0;
+            foreach (var token in this.getTokens(csElement).Where(x => x.LineNumber == violation.Line).Flatten())
+            {
+                if (predecessorFound == 2 && this.reportIssueFor(token, violation))
+                {
+                    predecessorFound = 0;
+                    yield return new StyleCopCodeIssue(CodeIssueType.CodeSmell, new SourceRange(token.Location.StartPoint.LineNumber, token.Location.StartPoint.IndexOnLine + 1, token.Location.EndPoint.LineNumber, token.Location.EndPoint.IndexOnLine + 2));
+                }
+                else if (this.bannedPredecessors.Contains(token.CsTokenType))
+                {
+                    predecessorFound = 1;
+                }
+                else if (predecessorFound == 1 && token.CsTokenType == CsTokenType.WhiteSpace)
+                {
+                    predecessorFound = 2;
+                }
+                else
+                {
+                    predecessorFound = 0;
+                }
+            }
+        }
+    }
+}
