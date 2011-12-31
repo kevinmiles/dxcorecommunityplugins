97c244c87854d1b5dbc5e59225bf61004f7633a1
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2011-12-31 00:36:14 +0000

    added support for SA1018 rule

---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/SA1018_NullableTypeSymbolsMustNotBePrecededBySpace.cs
@@ -0,0 +1,19 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StyleCop.CSharp;
+
+    internal class SA1018_NullableTypeSymbolsMustNotBePrecededBySpace : StyleCopRule
+    {
+        public SA1018_NullableTypeSymbolsMustNotBePrecededBySpace()
+            : base(new AggregatedIssueLocator(new ICodeIssueLocator[]
+                {
+                    new AllTokensByTypePrecededByBannedElementIssueLocator(element => element.ElementTokens, CsTokenType.NullableTypeSymbol, CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
+                    new AllTokensByTypePrecededByBannedElementIssueLocator(element => element.Attributes.SelectMany(attribute => attribute.ChildTokens), CsTokenType.NullableTypeSymbol, CsTokenType.WhiteSpace, CsTokenType.EndOfLine)
+                }))
+        {
+        }
+    }
+}
