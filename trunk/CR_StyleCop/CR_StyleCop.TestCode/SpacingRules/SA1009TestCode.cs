5d64bd29b9509f4d0c6b33b5176f3d1bde6211d1
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2012-01-10 00:38:29 +0000

    added full support for SA1009, simplified implementation of SA1008

--- trunk/CR_StyleCop/CR_StyleCop.TestCode/SpacingRules/SA1009TestCode.cs
+++ trunk/CR_StyleCop/CR_StyleCop.TestCode/SpacingRules/SA1009TestCode.cs
@@ -14,13 +14,23 @@ namespace CR_StyleCop.TestCode
     /// </summary>
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "*", Justification = "This is about SA1009 rule." )]
     [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1017:ClosingAttributeBracketsMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.") ]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1019:MemberAccessSymbolsMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.")]
     [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1008:OpeningParenthesisMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.")]
+    [SuppressMessage("StyleCop.CSharp.SpacingRules", "SA1002:SemicolonsMustBeSpacedCorrectly", Justification = "This is about SA1009 rule.")]
     public class SA1009TestCode
     {
         private int MethodName( )
         {
             int x = 3 * ((1 + 2) * 5 );
             return (x + 3 ) * (5 * (1 + 2) );
+        }
+
+        private long MethodName2()
+        {
+            int x = this.MethodName() .CompareTo(11) ;
+            int y = this.MethodName( ).CompareTo(11 );
+            long z = (long) x;
+            return (long) (z + y);
         }
     }
 }
---
+++ trunk/CR_StyleCop/CR_StyleCop/CodeIssues/AllTokensFollowedByWhitespaceAndBannedElementIssueLocator.cs
@@ -0,0 +1,58 @@
+namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using DevExpress.CodeRush.Core;
+    using DevExpress.CodeRush.StructuralParser;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class AllTokensFollowedByWhitespaceAndBannedElementIssueLocator : ICodeIssueLocator
+    {
+        private readonly Func<CsElement, IEnumerable<CsToken>> getTokens;
+        private readonly Func<CsToken, bool> isBannedFollower;
+        private readonly Func<CsToken, Violation, bool> reportIssueFor;
+
+        public AllTokensFollowedByWhitespaceAndBannedElementIssueLocator(Func<CsElement, IEnumerable<CsToken>> getTokens, Func<CsToken, Violation, bool> predicate, Func<CsToken, bool> isBannedFollower)
+        {
+            this.getTokens = getTokens;
+            this.isBannedFollower = isBannedFollower;
+            this.reportIssueFor = predicate;
+        }
+
+        public IEnumerable<StyleCopCodeIssue> GetCodeIssues(
+            ISourceCode sourceCode,
+            Func<ElementTypeFilter, IEnumerable<IElement>> enumerate,
+            Violation violation,
+            CsElement csElement)
+        {
+            CsToken potentialViolation = null;
+            bool whitespaceFound = false;
+            foreach (var token in this.getTokens(csElement).Where(x => x.LineNumber == violation.Line).Flatten())
+            {
+                if (potentialViolation != null && token.CsTokenType == CsTokenType.WhiteSpace)
+                {
+                    whitespaceFound = true;
+                }
+                else if (potentialViolation != null && whitespaceFound && isBannedFollower(token))
+                {
+                    yield return new StyleCopCodeIssue(CodeIssueType.CodeSmell, new SourceRange(potentialViolation.Location.StartPoint.LineNumber, potentialViolation.Location.StartPoint.IndexOnLine + 1, potentialViolation.Location.EndPoint.LineNumber, potentialViolation.Location.EndPoint.IndexOnLine + 2));
+                    whitespaceFound = false;
+                    potentialViolation = null;
+                }
+                else if (this.reportIssueFor(token, violation))
+                {
+                    potentialViolation = token;
+                    whitespaceFound = false;
+                }
+                else 
+                {
+                    whitespaceFound = false;
+                    potentialViolation = null;
+                }
+            }
+        }
+    }
+}
+
