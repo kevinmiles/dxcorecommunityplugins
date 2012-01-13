86eced96478ecaeb3f777cc304ca69c6ca561ebb
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2012-01-13 23:39:35 +0000

    Added support for SA1021 and 1022 rules

---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1021_NegativeSignsMustBeSpacedCorrectly.cs
@@ -0,0 +1,27 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class SA1021_NegativeSignsMustBeSpacedCorrectly : StyleCopRule
+    {
+        private static Func<CsToken, Violation, bool> isNegativeSign = (token, violation) =>
+            token.CsTokenType == CsTokenType.OperatorSymbol && token.Text == "-" && token.Parent is UnaryExpression;
+
+        public SA1021_NegativeSignsMustBeSpacedCorrectly()
+            : base(new AggregatedIssueLocator(new ICodeIssueLocator[]
+                {
+                    new AllTokensNotPrecededByRequiredElementIssueLocator(ElementTokens, isNegativeSign, CsTokenType.WhiteSpace, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensNotPrecededByRequiredElementIssueLocator(AttributesTokens, isNegativeSign, CsTokenType.WhiteSpace, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(ElementTokens, isNegativeSign, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(AttributesTokens, isNegativeSign, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensFollowedByBannedElementIssueLocator(ElementTokens, isNegativeSign, CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
+                    new AllTokensFollowedByBannedElementIssueLocator(AttributesTokens, isNegativeSign, CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
+                }))
+        {
+        }
+    }
+}
---
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1022_PositiveSignsMustBeSpacedCorrectly.cs
@@ -0,0 +1,27 @@
+﻿namespace CR_StyleCop.CodeIssues
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using StyleCop;
+    using StyleCop.CSharp;
+
+    internal class SA1022_PositiveSignsMustBeSpacedCorrectly : StyleCopRule
+    {
+        private static Func<CsToken, Violation, bool> isPositiveSign = (token, violation) =>
+            token.CsTokenType == CsTokenType.OperatorSymbol && token.Text == "+" && token.Parent is UnaryExpression;
+
+        public SA1022_PositiveSignsMustBeSpacedCorrectly()
+            : base(new AggregatedIssueLocator(new ICodeIssueLocator[]
+                {
+                    new AllTokensNotPrecededByRequiredElementIssueLocator(ElementTokens, isPositiveSign, CsTokenType.WhiteSpace, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensNotPrecededByRequiredElementIssueLocator(AttributesTokens, isPositiveSign, CsTokenType.WhiteSpace, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(ElementTokens, isPositiveSign, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensPrecededByBannedElementAndWhitespaceIssueLocator(AttributesTokens, isPositiveSign, CsTokenType.OpenSquareBracket, CsTokenType.OpenParenthesis),
+                    new AllTokensFollowedByBannedElementIssueLocator(ElementTokens, isPositiveSign, CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
+                    new AllTokensFollowedByBannedElementIssueLocator(AttributesTokens, isPositiveSign, CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
+                }))
+        {
+        }
+    }
+}
