fc087208bc7b7061077272ef2175c9c64a965249
wlodarczak.przemyslaw <wlodarczak.przemyslaw@241145c3-2f4c-0410-ae16-21396c40807f>
2012-01-29 23:22:15 +0000

    added more tests, fixed few bugs. All spacing tests are green - yey

--- CR_StyleCop/CR_StyleCop/CodeIssues/SA1019_MemberAccessSymbolsMustBeSpacedCorrectly.cs
+++ CR_StyleCop/CR_StyleCop/CodeIssues/SA1019_MemberAccessSymbolsMustBeSpacedCorrectly.cs
@@ -12,8 +12,8 @@
         public SA1019_MemberAccessSymbolsMustBeSpacedCorrectly()
             : base(new AggregatedIssueLocator(new ICodeIssueLocator[]
                 {
-                    new AllTokensPrecededByBannedElementIssueLocator(ElementTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), CsTokenType.WhiteSpace),
-                    new AllTokensPrecededByBannedElementIssueLocator(AttributesTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), CsTokenType.WhiteSpace),
+                    new AllTokensPrecededByBannedElementIssueLocator(ElementTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), token => token.CsTokenType == CsTokenType.WhiteSpace && token.Location.StartPoint.IndexOnLine != 0),
+                    new AllTokensPrecededByBannedElementIssueLocator(AttributesTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), token => token.CsTokenType == CsTokenType.WhiteSpace && token.Location.StartPoint.IndexOnLine != 0),
                     new AllTokensFollowedByBannedElementIssueLocator(ElementTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
                     new AllTokensFollowedByBannedElementIssueLocator(AttributesTokens, (token, violation) => token.CsTokenType == CsTokenType.OperatorSymbol && memberAccessSymbols.Contains(token.Text), CsTokenType.WhiteSpace, CsTokenType.EndOfLine),
                 }))
