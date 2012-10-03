eacbd902cab54a5b7a731963451133e751b59d23
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2012-10-03 20:00:40 +0000

    New Plugin: CR_ForceProjectConverter

---
+++ trunk/CR_ForceProjectConverter/CR_ForceProjectConverter/PlugIn1.cs
@@ -0,0 +1,62 @@
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.CodeRush.Core;
+using DevExpress.CodeRush.PlugInCore;
+using DevExpress.CodeRush.StructuralParser;
+using System.IO;
+
+namespace CR_ForceProjectConverter
+{
+  public partial class PlugIn1 : StandardPlugIn
+  {
+    // DXCore-generated code...
+    #region InitializePlugIn
+    public override void InitializePlugIn()
+    {
+      base.InitializePlugIn();
+
+      //
+      // TODO: Add your initialization code here.
+      //
+    }
+    #endregion
+    #region FinalizePlugIn
+    public override void FinalizePlugIn()
+    {
+      //
+      // TODO: Add your finalization code here.
+      //
+
+      base.FinalizePlugIn();
+    }
+    #endregion
+
+    private void PlugIn1_SolutionOpened()
+    {
+      SolutionElement activeSolution = CodeRush.Source.ActiveSolution;
+      if (activeSolution == null)
+      	return;
+
+      List<string> unresolvedAssemblies = new List<string>();
+      foreach (ProjectElement project in activeSolution.ProjectElements)
+      {
+        foreach (AssemblyReference reference in project.AssemblyReferences)
+        {
+          if (reference.Name.StartsWith("DevExpress"))
+           // if (!File.Exists(reference.FilePath))
+              unresolvedAssemblies.Add(reference.Name);
+        }
+      }
+
+      if (unresolvedAssemblies.Count > 0)
+      {
+        UnresolvedReferences form = new UnresolvedReferences(unresolvedAssemblies);
+        form.ShowDialog(CodeRush.IDE);
+      }
+    }
+  }
+}
---
+++ trunk/CR_ForceProjectConverter/CR_ForceProjectConverter/UnresolvedReferences.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+using DevExpress.CodeRush.Core;
+
+namespace CR_ForceProjectConverter
+{
+  public partial class UnresolvedReferences : Form
+  {
+    public UnresolvedReferences()
+    {
+      InitializeComponent();
+    }
+
+    public UnresolvedReferences(List<string> unresolvedAssemblies)
+    {
+      InitializeComponent();
+
+      listBoxControl1.Items.Clear();
+      listBoxControl1.Items.AddRange(unresolvedAssemblies.ToArray());
+    }
+
+    private void simpleButton1_Click(object sender, EventArgs e)
+    {
+      string rootDirectory;
+
+      if (Environment.Is64BitOperatingSystem)
+      {
+        rootDirectory = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\DevExpress\DXperience\v12.1\", "RootDirectory", null) as String;
+      }
+      else
+      {
+        rootDirectory = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\DevExpress\DXperience\v12.1\", "RootDirectory", null) as String;
+      }
+
+      if (String.IsNullOrEmpty(rootDirectory))
+      {
+        MessageBox.Show("Could not retreive the DXperience root folder.");
+        return;
+      }
+
+      string projectConverterPath = Path.Combine(rootDirectory, @"Tools\DXperience\ProjectConverter.exe");
+
+      if (!File.Exists(projectConverterPath))
+      {
+        MessageBox.Show("Could not find the ProjectConverter tool.");
+        return;
+      }
+
+      string targetDirectoryForConversion = Path.GetDirectoryName(CodeRush.Source.ActiveSolution.FilePath);
+      Process.Start(projectConverterPath, "\"" + targetDirectoryForConversion + "\"");
+    }
+  }
+}
