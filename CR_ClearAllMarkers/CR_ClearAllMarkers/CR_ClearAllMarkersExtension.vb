5892fba80371f85db370f0ff877a117fab01c0df
rorybecker <RoryBecker@Gmail.com>
2012-11-14 20:26:19 +0000

    Multiple projects upgraded to VSIX. Also some previous VB.Net VSIX projects fixed .

--- CR_ClearAllMarkers/CR_ClearAllMarkers/CR_ClearAllMarkersExtension.vb
+++ CR_ClearAllMarkers/CR_ClearAllMarkers/CR_ClearAllMarkersExtension.vb
@@ -1,9 +1,7 @@
 Imports System.ComponentModel.Composition
 Imports DevExpress.CodeRush.Common
 
-Namespace CR_ClearAllMarkers
-  <Export(GetType(IVsixPluginExtension))> _
+<Export(GetType(IVsixPluginExtension))> _
   Public Class CR_ClearAllMarkersExtension
-	  Implements IVsixPluginExtension
-  End Class
-End Namespace
+    Implements IVsixPluginExtension
+End Class
--- CR_DeclareClassInProject/CR_DeclareClassInProject/CR_DeclareClassInProjectExtension.vb
+++ CR_DeclareClassInProject/CR_DeclareClassInProject/CR_DeclareClassInProjectExtension.vb
@@ -1,9 +1,7 @@
 Imports System.ComponentModel.Composition
 Imports DevExpress.CodeRush.Common
 
-Namespace CR_DeclareClassInProject
-  <Export(GetType(IVsixPluginExtension))> _
+<Export(GetType(IVsixPluginExtension))> _
   Public Class CR_DeclareClassInProjectExtension
-	  Implements IVsixPluginExtension
-  End Class
-End Namespace
+    Implements IVsixPluginExtension
+End Class
