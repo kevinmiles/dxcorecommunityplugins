131491725f664fb5756baee933a760955592f133
rorybecker <RoryBecker@Gmail.com>
2013-11-19 11:52:25 +0000

    Upgraded project to VS2013

---
+++ DX_ConvertToTryParse/DX_ConvertToTryParse/DX_ConvertToTryParseExtension.vb
@@ -0,0 +1,7 @@
+Imports System.ComponentModel.Composition
+Imports DevExpress.CodeRush.Common
+
+<Export(GetType(IVsixPluginExtension))> _
+  Public Class DX_ConvertToTryParseExtension
+    Implements IVsixPluginExtension
+End Class
