c27bae59a5ca0e893560f2dab677ce6fc696934a
travis.illig <travis.illig@gmail.com>
2014-06-10 17:01:46 +0000

    Fixed security attributes for .NET 4.0 and DXCore 14 compatibility.

--- CR_JoinLines/CR_JoinLines/AssemblyInfo.cs
+++ CR_JoinLines/CR_JoinLines/AssemblyInfo.cs
@@ -1,16 +1,13 @@
 using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Security;
 using DevExpress.CodeRush.Common;
 
 [assembly: DXCoreAssembly(DXCoreAssemblyType.PlugIn, "CR_JoinLines")]
 [assembly: ComVisible(false)]
-[assembly: SecurityTransparent]
-[assembly: SecurityRules(SecurityRuleSet.Level2)]
 
 [assembly: AssemblyTitle("CR_JoinLines")]
 [assembly: AssemblyDescription("DXCore plug-in providing a command to join text lines in the editor")]
 [assembly: AssemblyProduct("DXCore for Visual Studio .NET")]
 [assembly: AssemblyCopyright("Copyright © 2005 Travis Illig")]
-[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyVersion("2.1.1.0")]
