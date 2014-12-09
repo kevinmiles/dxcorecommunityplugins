8f153dd7088aec113e7c6860ff8cff0ad852ac87
rorybecker <RoryBecker@Gmail.com>
2014-12-09 11:09:08 +0000

    Moved DX_ConvertToTryParse to CR_ConvertToTryParse on Github

--- DX_ConvertToTryParse/DX_ConvertToTryParse/DX_ConvertToTryParseExtension.vb
+++
@@ -1,7 +0,0 @@
-Imports System.ComponentModel.Composition
-Imports DevExpress.CodeRush.Common
-
-<Export(GetType(IVsixPluginExtension))> _
-  Public Class DX_ConvertToTryParseExtension
-    Implements IVsixPluginExtension
-End Class
