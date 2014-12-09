8177896d0736dd93ec9346a47b56d02ee8709554
rorybecker <RoryBecker@Gmail.com>
2014-12-09 12:41:38 +0000

    Moved CR_DeclareParameter to GitHub

--- CR_DeclareParameter/CR_DeclareParameter/CR_DeclareParameterExtension.vb
+++
@@ -1,9 +0,0 @@
-Imports System.ComponentModel.Composition
-Imports DevExpress.CodeRush.Common
-
-Namespace CR_DeclareParameter
-  <Export(GetType(IVsixPluginExtension))> _
-  Public Class CR_DeclareParameterExtension
-	  Implements IVsixPluginExtension
-  End Class
-End Namespace
