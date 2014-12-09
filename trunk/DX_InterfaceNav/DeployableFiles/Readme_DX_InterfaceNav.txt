a97e069e137ad89d3280712d7624911812cb6963
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2014-12-09 08:54:07 +0000

    Moved DX_InterfaceNav to CR_InterfaceNav on Github

--- trunk/DX_InterfaceNav/DeployableFiles/Readme_DX_InterfaceNav.txt
+++
@@ -1,2 +0,0 @@
-There is no need to copy the ExampleVB and EXampleCS files along with the plugin. 
-These files will be removed from this build process at a later date.
--- trunk/DX_InterfaceNav/InterfaceJumping/MySecondImplementor.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace InterfaceJumping
-{
-    // from here I would click on the interface type and say go to declaration (which I can already), or jump to another implementation
-
-    class MySecondImplementor : IAnInterface
-    {
-    }
-}
