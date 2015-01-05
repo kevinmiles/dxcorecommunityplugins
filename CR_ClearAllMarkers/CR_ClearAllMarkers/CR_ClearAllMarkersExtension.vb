54099d86954b74bb25d093bfdc9040aca8227173
rorybecker <RoryBecker@Gmail.com>
2015-01-05 21:43:41 +0000

    Moved CR_ClearAllMarkers to GitHub

--- CR_ClearAllMarkers/CR_ClearAllMarkers/CR_ClearAllMarkersExtension.vb
+++
@@ -1,7 +0,0 @@
-Imports System.ComponentModel.Composition
-Imports DevExpress.CodeRush.Common
-
-<Export(GetType(IVsixPluginExtension))> _
-  Public Class CR_ClearAllMarkersExtension
-    Implements IVsixPluginExtension
-End Class
