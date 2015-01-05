b5033dd6d30a3f8b2c57d747462a853994a2d5d9
rorybecker <RoryBecker@Gmail.com>
2015-01-05 20:52:54 +0000

    Moved CR_JumpToType to GitHub

--- CR_JumpToType/CR_JumpToType/CR_JumpToTypeExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.Composition;
-using DevExpress.CodeRush.Common;
-
-namespace CR_JumpToType
-{
-  [Export(typeof(IVsixPluginExtension))]
-  public class CR_JumpToTypeExtension : IVsixPluginExtension { }
-}
