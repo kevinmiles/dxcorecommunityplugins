de56860c57d0dde9094eb56bf79278475ce12765
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2015-01-20 22:56:41 +0000

    Moved CR_PrimitiveTab to GitHub

--- trunk/CR_PrimitiveTab/CR_PrimitiveTab/CR_PrimitiveTabExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.Composition;
-using DevExpress.CodeRush.Common;
-
-namespace CR_PrimitiveTab
-{
-  [Export(typeof(IVsixPluginExtension))]
-  public class CR_PrimitiveTabExtension : IVsixPluginExtension { }
-}
