beae28092d5ab851cdaea6999c19cce72e60ed33
rorybecker <RoryBecker@Gmail.com>
2015-01-21 22:44:36 +0000

    Moved CR_ImportNamespace to GitHub

--- CR_ImportNamespace/CR_ImportNamespace/CR_ImportNamespaceExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.Composition;
-using DevExpress.CodeRush.Common;
-
-namespace CR_ImportNamespace
-{
-  [Export(typeof(IVsixPluginExtension))]
-  public class CR_ImportNamespaceExtension : IVsixPluginExtension { }
-}
