8b0ba91a944b20a689fd27842f30d7a33f3fddda
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2015-01-21 22:15:58 +0000

    Moved CR_StringFormatter to GitHub

--- trunk/CR_StringFormatter/CR_StringFormatter/CR_StringFormatterExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.Composition;
-using DevExpress.CodeRush.Common;
-
-namespace CR_StringFormatter
-{
-  [Export(typeof(IVsixPluginExtension))]
-  public class CR_StringFormatterExtension : IVsixPluginExtension { }
-}
