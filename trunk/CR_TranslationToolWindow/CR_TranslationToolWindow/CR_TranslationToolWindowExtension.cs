4259c5200e27a6d9bdf1e426a89d90487b60e412
rorybecker@gmail.com <rorybecker@gmail.com@241145c3-2f4c-0410-ae16-21396c40807f>
2015-01-22 10:42:46 +0000

    Moved CR_TranslationToolWindow to GitHub

--- trunk/CR_TranslationToolWindow/CR_TranslationToolWindow/CR_TranslationToolWindowExtension.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.Composition;
-using DevExpress.CodeRush.Common;
-
-namespace CR_TranslationToolWindow
-{
-  [Export(typeof(IVsixPluginExtension))]
-  public class CR_TranslationToolWindowExtension : IVsixPluginExtension { }
-}
