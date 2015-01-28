89b8023977c8c3da5f1ba2d3db02fb60e294d05a
rorybecker <RoryBecker@Gmail.com>
2015-01-28 09:55:33 +0000

    Obsoleted by CR_FavoriteViews

--- CR_LinkBar/CR_LinkBar/Extensions/StringExt.vb
+++
@@ -1,7 +0,0 @@
-Imports System.Runtime.CompilerServices
-Public Module StringExt
-    <Extension()> _
-    Public Function DefaultTo(ByVal Source As String, ByVal Defaultvalue As String) As String
-        Return If(Source = String.Empty, Defaultvalue, Source)
-    End Function
-End Module
